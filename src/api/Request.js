4d0ad2b568d0fa370cb7407423bb14df62d964d7
Stephen Inglis <si5@sanger.ac.uk>
2018-12-04 16:01:27 +0000

    Added request.

---
+++ src/api/Request.js
@@ -0,0 +1,10 @@
+
+import axios from 'axios'
+
+const Request = {
+  baseUrl: 'http://example.com',
+  apiNamespace: 'api/v1',
+  headers: {}
+}
+
+export default Request
---
+++ tests/unit/api/Request.spec.js
@@ -0,0 +1,28 @@
+import Request from '@/api/Request'
+import axios from 'axios'
+
+jest.mock('axios')
+
+describe('Request', () => {
+
+  it('can have a basuUrl', () => {
+    Request.baseUrl = 'http://sequencescape.com'
+    expect(Request.baseUrl).toEqual('http://sequencescape.com')
+  })
+
+  it('can have an apiNamespace', () => {
+    Request.apiNamespace = 'api/v2'
+    expect(Request.apiNamespace).toEqual('api/v2')
+  })
+
+  it('can have some headers', () => {
+    let headers = {'Content-Type': 'application/vnd.api+json', 'Accept': 'application/vnd.api+json'}
+    Request.headers = headers
+    console.log(Request.config)
+    expect(Request.headers).toEqual(headers)
+  })
+
+  it('performs a get request', () => {
+    axios.get.mockResolvedValue({data: { status: 200, data: {id: 1, attributes: {name: 'sample1', species: 'dog'}}}})
+  })
+})
