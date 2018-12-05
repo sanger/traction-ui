7b4992f3c0926fdec85b688c4a781e8bb2e2b620
Stephen Inglis <si5@sanger.ac.uk>
2018-12-05 15:44:25 +0000

    Added request.

--- src/api/Request.js
+++ src/api/Request.js
@@ -1,10 +1,40 @@
 
 import axios from 'axios'
 
-const Request = {
+let defaultConfig = {
   baseUrl: 'http://example.com',
   apiNamespace: 'api/v1',
   headers: {}
 }
 
-export default Request
+class Request {
+  constructor(config) {
+    this.config = config
+  }
+
+  get baseUrl () {
+    return this.config.baseUrl
+  }
+
+  get apiNamespace () {
+    return this.config.apiNamespace
+  }
+
+  get headers () {
+    return this.config.headers
+  }
+
+  set baseUrl (_baseUrl) {
+    this.config.baseUrl = _baseUrl
+  }
+
+  set apiNamespace (_apiNamespace) {
+    this.config.apiNamespace = _apiNamespace
+  }
+
+  set headers (_headers) {
+    this.config.headers = _headers
+  }
+}
+
+export default new Request(defaultConfig)
--- tests/unit/api/Request.spec.js
+++ tests/unit/api/Request.spec.js
@@ -18,11 +18,12 @@ describe('Request', () => {
   it('can have some headers', () => {
     let headers = {'Content-Type': 'application/vnd.api+json', 'Accept': 'application/vnd.api+json'}
     Request.headers = headers
-    console.log(Request.config)
+    console.log(Request)
     expect(Request.headers).toEqual(headers)
   })
 
   it('performs a get request', () => {
     axios.get.mockResolvedValue({data: { status: 200, data: {id: 1, attributes: {name: 'sample1', species: 'dog'}}}})
+    
   })
 })
