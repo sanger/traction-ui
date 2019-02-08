3c54f46424133996462730101cffeee0bedbe380
Stephen Inglis <si5@sanger.ac.uk>
2019-02-08 13:09:32 +0000

    Added includes for response.

--- src/api/Response.js
+++ src/api/Response.js
@@ -1,16 +1,26 @@
+/*
+  TODO: separate out into reusable functional methods - currying and recursion.
+*/
+
 class Response {
-  constructor(response) {
+  constructor(response, resource, included = '') {
     this.status = response.status
     this.statusText = response.statusText
-    this._body = response.data.data
+    this._body = response.data
+    this.resource = resource
     this._errors = response.data.errors
+    this.included = included
   }
 
-  get body() {
-    if (this._body === undefined) return {}
-    return this._body.map(attrs =>
-      Object.assign({ id: attrs.id }, attrs.attributes)
-    )
+  get successful () {
+    return (this.status >= 200 && this.status <= 400)
+  }
+
+  get data () {
+    if (this._body.data === undefined) return {}
+    return { [this.resource]:
+      this._body.data.map(attrs => ({ id: attrs.id, ...attrs.attributes }) )
+    }
   }
 
   get errors() {
@@ -20,12 +30,43 @@ class Response {
 
     return Object.assign({
       message:  Object.keys(self._errors).map(key => {
-                  return self._errors[key].map(error => key.concat(" ", error))
+                  return self._errors[key].map(error => key.concat(' ', error))
                 })
                 .reduce(function(a, b) { return(a.concat(b))}, [])
-                .join(", ")
+                .join(', ')
     })
   }
+
+  get body() {
+    if (this.data === {}) return this.data
+
+    let data = this.data
+
+    if (this._body.included === undefined) return  this.data
+
+    let relationships = this.included.split('.')
+
+    data[this.resource].map(item => {
+
+      let relationship = this._body.data.find(i => i.id === item.id)['relationships'][relationships[0]]
+      let relationshipId = relationship['data'][0]['id']
+      let included = this._body.included.find(item => item.id === relationshipId)
+      item[relationships[0]] = [{ id: relationshipId, ...included['attributes']}]
+
+      if (relationships[1] !== undefined) {
+
+        relationship = included['relationships'][relationships[1]]
+        relationshipId = relationship['data']['id']
+        included = this._body.included.find(item => item.id === relationshipId)
+        item[relationships[0]][0][relationships[1]] = { id: relationshipId, ...included['attributes'] }
+
+      }
+    })
+
+    return data
+   
+  }
+
 }
 
 export default Response
--- tests/unit/api/Response.spec.js
+++ tests/unit/api/Response.spec.js
@@ -1,64 +1,58 @@
 import Response from '@/api/Response'
+import Requests from '../../data/requests'
 
 describe('Response', () => {
 
-  describe('Success', () => {
-    let response
+  let mockResponse, response
 
-    beforeEach(() => {
-      response = {
-        data: {
-          data: [
-            {
-              id: 1,
-              attributes: {
-                name: "testname1",
-                species: "testspecies1"
-              }
-            },
-            {
-              id: 2,
-              attributes: {
-                name: "testname2",
-                species: "testspecies2"
-              }
-            },
-            {
-              id: 3,
-              attributes: {
-                name: "testname3",
-                species: "testspecies3"
-              }
-            }
-          ]
+  describe('basic', () => {
+
+    describe('Success', () => {
+      beforeEach(() => {
+        mockResponse =  { 
+          data: { 
+            data: [
+               { id: 1, attributes: { name: "testname1", species: "testspecies1" }},
+               { id: 2, attributes: { name: "testname2", species: "testspecies2" }}
+            ]
+          },
+          status: 200,
+          statusText: "OK"
         },
-        status: 200,
-        statusText: "OK"
-      }
+        response = new Response(mockResponse, 'requests')
+      })
+
+      it('has a status', () => {
+        expect(response.status).toEqual(200)
+      })
+
+      it('has a status text', () => {
+        expect(response.statusText).toEqual('OK')
+      })
+
+      it('has some attributes', () => {
+        let attributes = response.data
+        expect(attributes.requests.length).toEqual(2)
+        let request = attributes.requests[0]
+        expect(request.name).toEqual('testname1')
+        expect(request.species).toEqual('testspecies1')
+      })
+
+      it('flagged as successful', () => {
+        expect(response.successful).toBeTruthy()
+      })
+
+      it('has no errors', () => {
+        expect(response.errors).toEqual({})
+      })
     })
 
-    it('is initialized with a response object', () => {
-      let init = new Response(response)
-      expect(init.status).toEqual(response.status)
-      expect(init.errors).toEqual({})
-    })
-
-    it('#get body', () => {
-      let init = new Response(response)
-      let expectedResponse = [
-        { id: 1, name: "testname1", species: "testspecies1" },
-        { id: 2, name: "testname2", species: "testspecies2" },
-        { id: 3, name: "testname3", species: "testspecies3" },
-      ]
-      expect(init.body).toEqual(expectedResponse)
-    })
   })
 
   describe('Failure', () => {
-    let response
 
     beforeEach(() => {
-      response = {
+      mockResponse = {
         data: {
           errors: {
             name: ['name error message 1'],
@@ -67,22 +61,48 @@ describe('Response', () => {
         },
         status: 422,
         statusText: "Unprocessible entity"
-      }
+      },
+      response = new Response(mockResponse, 'requests')
     })
 
-    it('is initialized with a response object', () => {
-      let init = new Response(response)
-      expect(init.status).toEqual(response.status)
-      expect(init.body).toEqual({})
+    it('has a status', () => {
+      expect(response.status).toEqual(422)
     })
 
-    it('#get errors', () => {
-      let init = new Response(response)
-      let msg = 'name name error message 1, species species error message 2.1, species species error message 2.2'
-      let expectedResponse = { message: msg }
-      expect(init.errors).toEqual(expectedResponse)
+    it('has some errors', () => {
+      let message = 'name name error message 1, species species error message 2.1, species species error message 2.2'
+      expect(response.errors).toEqual({ message: message})
+    })
+
+    it('not flagged as successful', () => {
+      expect(response.successful).toBeFalsy()
+    })
+
+    it('has no data', () => {
+      expect(response.data).toEqual({})
     })
 
   })
+
+  describe('with associated data', () => {
+
+    let requests, request, expectedResponse
+
+    beforeEach(() => {
+      requests = Requests.request
+      response = new Response(requests, 'requests', 'samples.sample_metadata')
+    })
+
+    it('has the correct number of items', () => {
+      expect(response.body.requests.length).toEqual(requests.data.data.length)
+    })
+
+    it('includes the correct relationship data', () => {
+      expect(response.body.requests[0]).toEqual(Requests.response.requests[0])
+    })
+
+  })
+
+  
 
 })
