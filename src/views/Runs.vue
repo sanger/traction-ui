5191e6c26125d4d02038902ddfd42a0a9a442dad
Harriet Craven <hc6@sanger.ac.uk>
2019-02-20 15:55:23 +0000

    Added b-table, Request and Response to Runs view

--- src/views/Runs.vue
+++ src/views/Runs.vue
@@ -3,28 +3,72 @@
       <router-link :to="{name: 'NewRun'}">
         <b-button id="newRun" class="float-right">Create New Run</b-button>
       </router-link>
+
+      <b-table
+         show-empty
+         :items="getRuns"
+         :fields="fields"
+      >
+        <template slot="selected" slot-scope="row">
+          <b-checkbox @change="toggleSelectedRow(row.item)"></b-checkbox>
+        </template>
+      </b-table>
+      {{ selected }}
     </div>
 </template>
 
 <script>
+import ApiConfig from '@/api/Config'
+import ConfigItem from '@/api/ConfigItem'
+import ComponentFactory from '@/mixins/ComponentFactory'
+import Request from '@/mixins/Request'
+import Response from '@/api/Response'
 
 export default {
   name: 'Runs',
+  mixins: [ComponentFactory],
   props: {
   },
   data () {
     return {
+      fields: [
+        { key: 'selected', label: '' },
+        { key: 'id', label: 'Run ID' },
+        { key: 'state', label: 'State' },
+        { key: 'chip-barcode', label: 'Chips Barcode' }
+      ],
+      selected: []
     }
   },
   created: function () {
   },
   methods: {
+    toggleSelectedRow(item) {
+      if (this.selected.indexOf(item) === -1) {
+        this.selected.push(item)
+      } else {
+        this.selected.splice(this.selected.indexOf(item), 1 );
+      }
+    },
+    async getRuns () {
+      try {
+        let rawRuns = await this.runRequest.get()
+        return new Response(rawRuns).deserialize.runs
+      } catch(error) {
+        return error
+      }
+    },
   },
   components: {
 
   },
   computed: {
-
+    runRequest () {
+      return this.build(Request, this.tractionConfig.resource('runs'))
+    },
+    tractionConfig () {
+      return this.build(ConfigItem, ApiConfig.traction)
+    },
   }
 }
 </script>
--- tests/unit/views/Runs.spec.js
+++ tests/unit/views/Runs.spec.js
@@ -1,18 +1,39 @@
 import Runs from '@/views/Runs'
 import { mount, localVue } from '../testHelper'
 import VueRouter from 'vue-router'
+import RunsJson from '../../data/runs'
+import Response from '@/api/Response'
 
 describe('Runs.vue', () => {
 
-  let wrapper, data
+  let wrapper, data, runs
 
   beforeEach(() => {
     const router = new VueRouter()
     wrapper = mount(Runs, { localVue, router })
+    runs = wrapper.vm
   })
 
   it('contains a create new run button', () => {
     expect(wrapper.contains('button')).toBe(true)
+  })
+
+  it('will create a run request', () => {
+    let request = runs.runRequest
+    expect(request.resource).toBeDefined()
+  })
+
+  it('will get a list of samples', async () => {
+    runs.runRequest.execute = jest.fn()
+    runs.runRequest.execute.mockResolvedValue(RunsJson)
+
+    let JsonApiResponse = await runs.getRuns()
+    let expected = new Response(RunsJson)
+    expect(JsonApiResponse).toEqual(expected.deserialize.runs)
+  })
+
+  it('contains a table', () => {
+    expect(wrapper.contains('table')).toBe(true)
   })
 
 })
