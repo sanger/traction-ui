fb17d231cf1b19be9c7d270a1b061f8d05a1471e
Harriet Craven <hc6@sanger.ac.uk>
2019-02-20 15:06:01 +0000

    Added Request and Repsonse to Reception view

--- tests/unit/views/Reception.spec.js
+++ tests/unit/views/Reception.spec.js
@@ -1,153 +1,225 @@
 import Reception from '@/views/Reception'
 import Alert from '@/components/Alert'
 import { mount, localVue } from '../testHelper'
-import DataList from '@/api/DataList'
 import flushPromises from 'flush-promises'
+import SSRequestsJson from '../../data/ssRequests'
+import Response from '@/api/Response'
 
 describe('Reception.vue', () => {
 
-  let wrapper, reception, data
+  let wrapper, reception
 
   beforeEach(() => {
-
-    // TODO: is this sustainable?
-    DataList.created = jest.fn()
-
-    data = { body: [
-      { "id": 1, "name": "DN11111", "species": "cat" },
-      { "id": 2, "name": "DN11112", "species": "cat" },
-      { "id": 3, "name": "DN11113", "species": "dog" },
-      { "id": 4, "name": "DN11114", "species": "dog" },
-      { "id": 5, "name": "DN11115", "species": "cat" }
-    ]}
     wrapper = mount(Reception, { localVue })
-    wrapper.find(DataList).vm.data = data
     reception = wrapper.vm
   })
 
-  it('has a data list', () => {
-    expect(wrapper.contains(DataList)).toBe(true)
+  it('will have a name', () => {
+    expect(wrapper.name()).toEqual('Reception')
   })
 
   it('has a alert', () => {
     expect(wrapper.contains(Alert)).toBe(true)
   })
 
+  it('will create a sample request', () => {
+    let request = reception.sampleRequest
+    expect(request.resource).toBe('samples')
+  })
+
+  it('will create a ss request request', () => {
+    let request = reception.ssRequestRequest
+    expect(request.resource).toBe('requests')
+  })
+
+  it('#getSSRequests will get a list of ss requests', async () => {
+    reception.ssRequestRequest.execute = jest.fn()
+    reception.ssRequestRequest.execute.mockResolvedValue(SSRequestsJson)
+
+    let JsonApiResponse = await reception.getSSRequests()
+    let expected = new Response(SSRequestsJson)
+    expect(JsonApiResponse).toEqual(expected.deserialize.requests)
+  })
+
   it('contains a table', () => {
     expect(wrapper.contains('table')).toBe(true)
   })
 
-  it('contains the correct data', () => {
+  it.skip('contains the correct data', () => {
     expect(wrapper.find('tbody').findAll('tr').length).toEqual(data.body.length)
   })
 
-  describe('selected', () => {
-
-    let checkboxes
+  describe('#exportRequestsIntoTraction', () => {
+    let response
 
     beforeEach(() => {
-      checkboxes = wrapper.findAll(".selected")
-      checkboxes.at(0).trigger('click')
-      checkboxes.at(1).trigger('click')
-      checkboxes.at(2).trigger('click')
-    })
+      reception.sampleRequest.execute = jest.fn()
 
-    it('will find the requests which have been selected', () => {
-      expect(reception.selected.length).toEqual(3)
-    })
-
-    describe('#exportRequestsIntoTraction', () => {
-      let response
-
-      beforeEach(() => {
-        reception.tractionApi.create = jest.fn()
-      })
-
-      it('success', async () => {
-        response = {status: 201}
-        reception.tractionApi.data = response
-        reception.tractionApi.create.mockReturnValue(response)
-        reception.exportRequestsIntoTraction()
-        await flushPromises()
-        let body = { data: { attributes: { samples: reception.selected }}}
-        expect(reception.tractionApi.create).toBeCalledWith(body)
-        expect(reception.message).toEqual("Samples imported into Traction")
-      })
-
-      it('failure', async () => {
-        response = {message: 'Something went wrong'}
-        reception.tractionApi.errors = response
-        reception.tractionApi.create.mockReturnValue(response)
-        let fn = reception.exportRequestsIntoTraction()
-        await expect(fn).rejects.toBe("Something went wrong")
-        await flushPromises()
-        let body = { data: { attributes: { samples: reception.selected }}}
-        expect(reception.tractionApi.create).toBeCalledWith(body)
-        expect(reception.message).toEqual("Something went wrong")
-      })
-    })
-
-    describe('#updateSequencescapeRequests', () => {
-      let response
-
-      beforeEach(() => {
-        reception.sequencescapeApi.update = jest.fn()
-      })
-
-      it('success', async () => {
-        response = {status: 201}
-        reception.sequencescapeApi.data = response
-        reception.sequencescapeApi.update.mockReturnValue(response)
-        reception.updateSequencescapeRequests()
-        await flushPromises()
-        expect(reception.message).toEqual('Samples updated in SS')
-        expect(reception.sequencescapeApi.update).toHaveBeenCalledTimes(reception.selected.length)
-      })
-
-      it('failure', async () => {
-        response = {message: 'Something went wrong'}
-        reception.sequencescapeApi.errors = response
-        reception.sequencescapeApi.update.mockReturnValue(response)
-        let fn = reception.updateSequencescapeRequests()
-        await expect(fn).rejects.toBe("Something went wrong")
-        await flushPromises()
-        expect(reception.message).toEqual('Something went wrong')
-        expect(reception.sequencescapeApi.update).toHaveBeenCalledTimes(reception.selected.length)
-      })
+      reception.selected = [
+        { id: "4", samples: [{ name: "sample_d", sample_metadata: { sample_common_name: "human" }}]}
+      ]
 
     })
 
-    describe('#exportRequests', () =>{
+    it('success', async () => {
+      let mockResponse =  {
+        data: {
+          data: [
+             { id: 1, type: "samples", attributes: { name: "testname1" }},
+             { id: 2, type: "samples", attributes: { name: "testname2" }}
+          ]
+        },
+        status: 200,
+        statusText: "OK"
+      }
 
-      beforeEach(() => {
-        reception.exportRequestsIntoTraction = jest.fn()
-        reception.updateSequencescapeRequests = jest.fn()
-      })
+      reception.sampleRequest.execute.mockResolvedValue(mockResponse)
 
-      describe('when exportRequestsIntoTraction is successful', () => {
-        it('calls updateSequencescapeRequests', async () => {
-          let response = {status: 201}
-          reception.exportRequestsIntoTraction.mockResolvedValue(response)
-          reception.updateSequencescapeRequests.mockResolvedValue(response)
-          wrapper.find('#exportRequests').trigger('click')
-          expect(reception.exportRequestsIntoTraction).toBeCalled()
-          await flushPromises()
-          expect(reception.updateSequencescapeRequests).toBeCalled()
-        })
-      })
-
-      describe('when exportRequestsIntoTraction fails', () => {
-        it('does not call updateSequencescapeRequests', () => {
-          let response = {message: 'Something went wrong'}
-          reception.exportRequestsIntoTraction.mockReturnValue(response)
-          wrapper.find('#exportRequests').trigger('click')
-          expect(reception.exportRequestsIntoTraction).toBeCalled()
-          expect(reception.updateSequencescapeRequests).not.toBeCalled()
-        })
-      })
-
+      await reception.exportRequestsIntoTraction()
+      expect(reception.message).toEqual("Samples imported into Traction")
     })
 
+    it('failure', async () => {
+      let mockResponse = {
+        data: {
+          errors: {
+            name: ['name error message 1']
+          }
+        },
+        status: 422,
+        statusText: "Unprocessible entity"
+      }
+
+      reception.sampleRequest.execute.mockReturnValue(mockResponse)
+
+      let fn = reception.exportRequestsIntoTraction()
+      await expect(fn).rejects.toBe("name name error message 1")
+      await flushPromises()
+      expect(reception.message).toEqual("name name error message 1")
+    })
+  })
+
+  describe('#updateSequencescapeRequests', () => {
+    let response
+
+    beforeEach(() => {
+      reception.ssRequestRequest.execute = jest.fn()
+    })
+
+    it('success', async () => {
+      let mockResponse =  {
+        data: {
+          data: [
+             { id: 1, type: "samples", attributes: { name: "testname1" }},
+             { id: 2, type: "samples", attributes: { name: "testname2" }}
+          ]
+        },
+        status: 200,
+        statusText: "OK"
+      }
+
+      reception.ssRequestRequest.execute.mockResolvedValue(mockResponse)
+
+      reception.selected = [{sequencescape_request_id: 1}]
+      await reception.updateSequencescapeRequests()
+      expect(reception.message).toEqual('Samples updated in SS')
+    })
+
+    it('failure', async () => {
+
+      let mockResponse = {
+        data: {
+          errors: {
+            name: ['name error message 1'],
+            species: ['species error message 1'],
+          }
+        },
+        status: 422,
+        statusText: "Unprocessible entity"
+      }
+
+      reception.ssRequestRequest.execute.mockReturnValue(mockResponse)
+      reception.selected = [{sequencescape_request_id: 1}]
+      let fn = reception.updateSequencescapeRequests()
+      await expect(fn).rejects.toEqual(["name name error message 1, species species error message 1"])
+      await flushPromises()
+      expect(reception.message).toEqual(["name name error message 1, species species error message 1"])
+    })
+
+  })
+
+  describe('#exportRequests', () =>{
+
+    beforeEach(() => {
+      reception.sampleRequest.execute = jest.fn()
+      reception.ssRequestRequest.execute = jest.fn()
+
+      reception.selected = [
+        { id: "4", samples: [{ name: "sample_d", sample_metadata: { sample_common_name: "human" }}]}
+      ]
+    })
+
+    describe('when exportRequestsIntoTraction is successful', () => {
+      it('calls updateSequencescapeRequests', async () => {
+
+        let mockResponse =  {
+          data: {
+            data: [
+               { id: 1, type: "samples", attributes: { name: "testname1" }},
+               { id: 2, type: "samples", attributes: { name: "testname2" }}
+            ]
+          },
+          status: 200,
+          statusText: "OK"
+        }
+
+        reception.sampleRequest.execute.mockResolvedValue(mockResponse)
+
+        await reception.exportRequests()
+
+        expect(reception.sampleRequest.execute).toBeCalled()
+        expect(reception.ssRequestRequest.execute).toBeCalled()
+
+      })
+    })
+
+    describe('when exportRequestsIntoTraction fails', () => {
+      it('does not call updateSequencescapeRequests', () => {
+        let mockResponse = {
+          data: {
+            errors: {
+              name: ['name error message 1']
+            }
+          },
+          status: 422,
+          statusText: "Unprocessible entity"
+        }
+
+        reception.sampleRequest.execute.mockReturnValue(mockResponse)
+
+        let fn = reception.exportRequests()
+        expect(reception.sampleRequest.execute).toBeCalled()
+        expect(reception.ssRequestRequest.execute).not.toBeCalled()
+      })
+    })
+
+  })
+
+  describe('#selectedJSON', () => {
+    it('returns the json formatted request', () => {
+      let selected = [{
+       id: "4",
+       samples: [
+         {  name: "sample_d",
+            sample_metadata:
+              { sample_common_name: "human" }
+          }
+        ]
+      }]
+
+      let expected = [{ sequencescape_request_id: '4', name: 'sample_d', species: 'human' }]
+      expect(reception.selectedJSON(selected)).toEqual(expected)
+    })
   })
 
 })
