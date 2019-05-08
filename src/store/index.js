5499d18b5db4d8e078a56f29402745e3eb996ef8
Harriet Craven <hc6@sanger.ac.uk>
2019-05-08 11:05:13 +0100

    Add printer list config

--- src/store/index.js
+++ src/store/index.js
@@ -2,13 +2,14 @@ import Vuex from 'vuex'
 import Vue from 'vue'
 import Api from '@/api'
 import build from '@/api/ApiBuilder'
+import PrinterList from '@/config/PrinterList'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
     api: build(Api.Config, process.env),
-    printers: ['printer1', 'printer2', 'printer3']
+    printers: PrinterList
   },
   mutations: {
   },
