40ecbc6fa42fdfc18875f2ad8276fb2341c08232
Harriet Craven <hc6@sanger.ac.uk>
2019-05-20 15:30:08 +0100

    Add printer

--- src/api/PromiseHelper.js
+++ src/api/PromiseHelper.js
@@ -4,7 +4,6 @@ import Api from '@/api'
 // return - a Api.Response object
 const handlePromise = async (promise) => {
   let rawResponse
-  debugger
   try {
     rawResponse = await promise
   } catch(resp) {
