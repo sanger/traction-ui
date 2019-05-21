7974cdd9b64d5118d600bcfa38ed528cb4331861
Stephen Inglis <si5@sanger.ac.uk>
2019-05-21 07:50:50 +0100

    Added validation for create.

--- src/api/Run.js
+++ src/api/Run.js
@@ -1,15 +1,26 @@
+import getTubesForBarcodes from './TubeRequests'
+
 const build = () => {
   return {
     chip: {
       flowcells: [
-        { position: 1 },
-        { position: 2 }
+        { position: 1, library: {} },
+        { position: 2, library: {} }
       ]     
     }
   }
 }
 
-const validate = (run) => {
+const getLibrary = async (barcode, request) => {
+  let response = await getTubesForBarcodes(barcode, request)
+  if (response.successful && !response.empty) {
+    let material = response.deserialize.tubes[0].material
+    if (material.type === 'libraries') { return material }
+  }
+  return
+}
+
+const validate = async (run, request) => {
   let errors = {}
   if (run.chip.barcode === undefined) {
     errors['chip'] = 'barcode not present'
@@ -17,10 +28,24 @@ const validate = (run) => {
   if (run.chip.barcode && run.chip.barcode.length < 16) {
     errors['chip'] = 'barcode not in correct format'
   }
+  for (const flowcell of run.chip.flowcells) {
+    if (flowcell.library.barcode === undefined) {
+      if (errors['flowcells'] === undefined) { errors['flowcells'] = {} }
+      errors.flowcells[flowcell.position] = 'library not present'
+    }
+    else {
+      let library = await getLibrary(flowcell.library.barcode, request)
+      if (library === undefined) {
+        if (errors['flowcells'] === undefined) { errors['flowcells'] = {} }
+        errors.flowcells[flowcell.position] = 'library does not exist'
+      }
+    }
+  }
   return errors
 }
 
 export {
   build,
-  validate
+  validate,
+  getLibrary
 }
--- tests/unit/api/Run.spec.js
+++ tests/unit/api/Run.spec.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import { mount } from '../testHelper'
+import Request from '@/api/Request'
+import LibraryTubeJson from '../../data/tubeWithLibrary'
+import SampleTubeJson from '../../data/tractionTubesWithSample'
+import Response from '@/api/Response'
 import * as Run from '@/api/Run'
 
 describe('Run', () => {
@@ -7,7 +11,6 @@ describe('Run', () => {
   let run
 
   describe('build', () => {
-
 
     beforeEach(() => {
       run = Run.build()
@@ -21,6 +24,10 @@ describe('Run', () => {
       expect(run.chip.flowcells.length).toEqual(2)
     })
 
+    it('each flowcell will have a library', () => {
+      expect(run.chip.flowcells[0].library).toBeDefined()
+    })
+
     it('each flowcell will have a position', () => {
       let flowcells = run.chip.flowcells
       expect(flowcells[0].position).toEqual(1)
@@ -29,32 +36,141 @@ describe('Run', () => {
 
   })
 
-  describe('validate', () => {
+  describe('creating a run', () => {
+
+    let cmp, props, wrapper, request, mockResponse, response, failedResponse, chipBarcode, barcode1, barcode2
 
     beforeEach(() => {
-      run = Run.build()
+      cmp = Vue.extend({
+        mixins: [Request],
+        render () { return ''}
+      })
+
+      props = { baseURL: 'http://sequencescape.com',
+                apiNamespace: 'api/v2',
+                resource: 'requests' }
+      wrapper = mount(cmp, { propsData: props })
+
+      request = wrapper.vm.api
+      request.get = jest.fn()
+
+      failedResponse = { status: 404, statusText: 'Record not found', data: { errors: { title: ['The record identified by 100 could not be found.'] }} }
+
+      chipBarcode = 'XYZ1234567891012'
+      barcode1 = 'DN123'
+      barcode2 = 'DN234'
     })
-    
-    describe('chip', () => {
 
-      it('will raise an error if the barcode is not present', () => {
-        expect(Run.validate(run).chip).toEqual("barcode not present")
-      }) 
+    describe('getLibrary', () => {
+      it('when it returns a library', async () => {
+        mockResponse = new Response(LibraryTubeJson).deserialize.tubes[0].material
+        request.get.mockResolvedValue(LibraryTubeJson)
+        response = await Run.getLibrary('DN123', request)
+        expect(response).toEqual(mockResponse)
+      })
 
-      it('will raise an error if the barcode is not in the correct format', () => {
-        run.chip['barcode'] = 'XYZ1234'
-        expect(Run.validate(run).chip).toEqual("barcode not in correct format")
+      it('when it returns a sample', async () => {
+        request.get.mockResolvedValue(SampleTubeJson)
+        response = await Run.getLibrary('DN123', request)
+        expect(response).not.toBeDefined()
+      })
+
+      it('when it returns nothing', async() => {
+        request.get.mockResolvedValue(failedResponse)
+        response = await Run.getLibrary('DN123', request)
+        expect(response).not.toBeDefined()
       })
     })
 
-    describe('flowcell', () => {
+    describe('validate', () => {
+      let result
 
-      it('will raise an error if the library is not present', () => {
-
+      beforeEach(() => {
+        run = Run.build()
       })
 
-      it('will raise an error if the library is not valid', () => {
+      describe('chip', () => {
 
+        it('will raise an error if the barcode is not present', async () => {
+          result = await Run.validate(run)
+          expect(result.chip).toEqual("barcode not present")
+        }) 
+
+        it('will raise an error if the barcode is not in the correct format', async () => {
+          run.chip['barcode'] = 'XYZ1234'
+          result = await Run.validate(run)
+          expect(result.chip).toEqual("barcode not in correct format")
+        })
+      })
+
+      describe('flowcell', () => {
+
+        beforeEach(() => {
+          run.chip['barcode'] = chipBarcode
+          run.chip.flowcells[0].library.barcode = barcode1
+        })
+
+        it('will raise an error if the library is not present', async () => {
+          mockResponse = new Response(LibraryTubeJson).deserialize.tubes[0].material
+          request.get.mockResolvedValue(LibraryTubeJson)
+          result = await Run.validate(run, request)
+          expect(result.flowcells['2']).toEqual('library not present')
+        })
+
+        it('will raise an error if the library does not exist', async () => {
+          request.get.mockResolvedValue(failedResponse)
+          result = await Run.validate(run, request)
+          expect(result.flowcells['1']).toEqual('library does not exist')
+
+          request.get.mockResolvedValue(SampleTubeJson)
+          result = await Run.validate(run, request)
+          expect(result.flowcells['1']).toEqual('library does not exist')
+        })
+      })
+    })
+
+    describe('create', () => {
+
+      describe('success', () => {
+
+        beforeEach(() => {
+          run = Run.build()
+          run['name'] = 'run1'
+          run.chip['barcode'] = chipBarcode
+          run.chip.flowcells[0].barcode = barcode1
+          run.chip.flowcells[1].barcode = barcode2
+          request.post = jest.fn()
+        })
+
+        it('will create a run', () => {
+          mockResponse = 
+
+        })
+
+        it('will create a chip', () => {
+
+        })
+
+        it('will create some flowcells', () => {
+
+        })
+      })
+
+      describe('when the run is invalid', () => {
+        it('will not create a run', () => {
+
+        })
+      })
+
+      describe('if any call is unsuccessful', () => {
+
+        it('will generate an error message', () => {
+
+        })
+
+        it('will rollback', () => {
+
+        })
       })
     })
   })
