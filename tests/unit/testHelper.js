1bb6b20baf8db533ac2b0a3d5d8f3bd52032267c
Harriet Craven <hc6@sanger.ac.uk>
2019-09-04 12:44:40 +0100

    Add back store helper in tests/unit/testHelper.js

--- tests/unit/testHelper.js
+++ tests/unit/testHelper.js
@@ -2,10 +2,11 @@ import BootstrapVue from 'bootstrap-vue'
 import { mount, createLocalVue } from '@vue/test-utils'
 import VueRouter from 'vue-router'
 import Vuex from 'vuex'
+import store from '@/store'
 
 const localVue = createLocalVue()
 localVue.use(BootstrapVue)
 localVue.use(VueRouter)
 localVue.use(Vuex)
 
-export { mount, localVue, Vuex }
+export { mount, localVue, Vuex, store }
