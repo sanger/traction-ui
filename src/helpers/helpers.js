31495dfe3faa4c86d2db0ac77e3b2c51ef9db776
Philip Jansen van Vuuren <pj5@sanger.ac.uk>
2019-09-24 15:45:22 +0100

    Remove unused file

--- src/helpers/helpers.js
+++
@@ -1,9 +0,0 @@
-/**
- * Send message to the console - only when not in production
- * @param {*} message the message to log
- */
-log(message) {
-  if (process.env.VUE_APP_LOG === 'true') { // https://stackoverflow.com/a/264037
-    console.log(message)
-  }
-},
