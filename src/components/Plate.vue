da9177ad1ca6d9d6ffafe0d17a303e619b7f351e
Harriet Craven <hc6@sanger.ac.uk>
2019-10-24 17:18:11 +0100

    Trailing SVG

--- src/components/Plate.vue
+++ src/components/Plate.vue
@@ -1,35 +1,17 @@
 <template>
   <div>
-    <thead>
-      <th>&nbsp;</th>
-      <th v-for="column in columns" v-bind:key="column">{{ column}}</th>
-    </thead>
-    <row v-for="row in rows" v-bind:key="row" v-bind:id="row" v-bind:columns="columns"></row>
+    <Test />
   </div>
 </template>
 
 <script>
 
-import Row from '@/components/Row'
+import Test from '@/components/Test'
 
 export default {
   name: 'Plate',
-  props: {
-    rows: {
-      type: Array,
-      default: () => {
-        return ['A','B','C','D','E','F','G','H']
-      }
-    },
-    columns: {
-      type: Array,
-      default: () => {
-        return [1,2,3,4,5,6,7,8,9,10,11,12]
-      }
-    }
-  },
   components: {
-    Row
+    Test
   },
 }
 </script>
