2db6f1098344537a8b4f9dc706f2798f96e830e0
Harriet Craven <hc6@sanger.ac.uk>
2019-10-31 14:38:40 +0000

    Added well getter

--- src/store/traction/pacbio/runs/getters.js
+++ src/store/traction/pacbio/runs/getters.js
@@ -3,6 +3,9 @@ const getters = {
     runRequest: (state, getters, rootState) => rootState.api.traction.pacbio.runs,
     currentRun: state => state.currentRun,
     pacbioRequests: (state, getters, rootState) => rootState.api.traction.pacbio,
+    well: state => (position) => {
+        return state.currentRun.plate.wells.find(well => well.position === position)
+    },
 }
 
 export default getters
