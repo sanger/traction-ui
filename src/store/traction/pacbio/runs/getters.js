7e0c6485695db5027a5cf82ac592a509ffdbf220
Harriet Craven <hc6@sanger.ac.uk>
2019-11-04 16:43:45 +0000

    Add code lost on merge - probably should add getter tests!

--- src/store/traction/pacbio/runs/getters.js
+++ src/store/traction/pacbio/runs/getters.js
@@ -2,6 +2,7 @@ const getters = {
     runs: state => state.runs,
     runRequest: (state, getters, rootState) => rootState.api.traction.pacbio.runs,
     currentRun: state => state.currentRun,
+    pacbioRequests: (state, getters, rootState) => rootState.api.traction.pacbio,
     well: state => (position) => {
         return state.currentRun.plate.wells.find(well => well.position === position)
     }
