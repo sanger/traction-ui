44e31b9c5da339be98d4abf272aae1a8450a2d9a
Harriet Craven <hc6@sanger.ac.uk>
2019-11-14 15:19:50 +0000

    Refactored run mutations

--- src/store/traction/pacbio/runs/mutations.js
+++ src/store/traction/pacbio/runs/mutations.js
@@ -1,46 +1,35 @@
+import * as PacbioRun from '@/api/PacbioRun'
+
+const mutate = key => (state, val) => {
+    state[key] = val
+}
+
+const mutateRun = key => (state, val) => {
+    state.currentRun[key] = val
+}
+
 const mutations = {
-    setRuns(state, runs) {
-        state.runs = runs
-    },
-    setCurrentRun(state, run) {
-        state.currentRun = run
-    },
-    setName(state, name) {
-        state.currentRun.name = name
-    },
-    setTemplatePrepKitBoxBarcode(state, barcode) {
-        state.currentRun.template_prep_kit_box_barcode = barcode
-    },
-    setBindingKitBoxBarcode(state, barcode) {
-        state.currentRun.binding_kit_box_barcode = barcode
-    },
-    setSequencingKitBoxBarcode(state, barcode) {
-        state.currentRun.sequencing_kit_box_barcode = barcode
-    },
-    setDNAControlComplexBoxBarcode(state, barcode) {
-        state.currentRun.dna_control_complex_box_barcode = barcode
-    },
-    setComments(state, comments) {
-        state.currentRun.comments = comments
-    },
-    setSystemName(state, systemName) {
-        state.currentRun.system_name = systemName
-    },
-    setLibraryBarcode(state, payload) {
-        // Assuming there is only one library in a well
-        state.currentRun.plate.wells.filter(well => well.position === payload.position)[0].libraries[0] = payload.library
-    },
-    setInsertSize(state, payload) {
-        state.currentRun.plate.wells.filter(well => well.position === payload.position)[0].insert_size = payload.insertSize
-    },
-    setOnPlateLoadingConc(state, payload) {
-        state.currentRun.plate.wells.filter(well => well.position === payload.position)[0].on_plate_loading_concentration = payload.onPlateLoadingConc
-    },
-    setMovieTime(state, payload) {
-        state.currentRun.plate.wells.filter(well => well.position === payload.position)[0].movie_time = payload.movieTime
-    },
-    setSequencingMode(state, payload) {
-        state.currentRun.plate.wells.filter(well => well.position === payload.position)[0].sequencing_mode = payload.sequencingMode
+    setRuns: mutate('runs'),
+    setCurrentRun: mutate('currentRun'),
+    setName: mutateRun('name'),
+    setTemplatePrepKitBoxBarcode: mutateRun('template_prep_kit_box_barcode'),
+    setBindingKitBoxBarcode: mutateRun('binding_kit_box_barcode'),
+    setSequencingKitBoxBarcode: mutateRun('sequencing_kit_box_barcode'),
+    setDNAControlComplexBoxBarcode: mutateRun('dna_control_complex_box_barcode'),
+    setComments: mutateRun('comments'),
+    setSystemName: mutateRun('system_name'),
+    mutateWell(state, payload) {
+        let currentWell = state.currentRun.plate.wells.filter(well => well.position === payload.position)[0]
+
+        if (!currentWell) {
+            // If well does not exist - Build a new well
+            let row = payload.position.split("")[0] // e.g. A
+            let column = payload.position.split("")[1] // e.g. 1
+            currentWell = PacbioRun.buildWell(row, column)
+
+            state.currentRun.plate.wells.push(currentWell)
+        }
+        currentWell[payload.property] = payload.with
     }
 }
 
--- tests/unit/store/traction/pacbio/runs/mutations.spec.js
+++ tests/unit/store/traction/pacbio/runs/mutations.spec.js
@@ -1,37 +1,76 @@
 import * as Mutations from '@/store/traction/pacbio/runs/mutations'
 import * as Run from '@/api/PacbioRun'
+import { Data } from '../../../../testHelper'
+import Response from '@/api/Response'
 
-describe.skip('#updateWell', () => {
+describe('mutate', () => {
+  let runs, state
 
-  let run, payload, well, state
+  beforeEach(() => {
+    runs = new Response(Data.PacbioRuns).deserialize.runs
+    state = { runs: [] }
+  })
+
+  it('can update the mutate the state, e.g setRuns', () => {
+    Mutations.default.setRuns(state, runs)
+    expect(state.runs.length).toEqual(runs.length)
+  })
+})
+
+describe('mutateRun', () => {
+  let run, state
 
   beforeEach(() => {
     run = Run.build()
-    state = {
-      currentRun: run
-    }
+    state = { currentRun: run }
   })
 
-  it('successfully', () => {
-    payload = { 
-      position: 'A1',
-      movie_time: 20,
-      insert_size: 30,
-      on_plate_loading_concentration: 40,
-      library: {
-        id: 1,
-        barcode: 'DN1234567'
-      }
-    }
-
-    Mutations.default.updateWell(state, payload)
-
-    well = state.currentRun.plate.wells.filter(well => well.position === 'A1')[0]
-
-    expect(well.position).toEqual('A1')
-    expect(well.movie_time).toEqual(20)
-    expect(well.insert_size).toEqual(30)
-    expect(well.on_plate_loading_concentration).toEqual(40)
-    expect(well.library).toEqual({id: 1, barcode: 'DN1234567'})
+  it('can update the mutate the state, e.g setName', () => {
+    let newName = 'a new name'
+    Mutations.default.setName(state, newName)
+    expect(state.currentRun.name).toEqual(newName)
   })
+})
+
+describe('mutateWell', () => {
+  let run, position, state
+
+  beforeEach(() => {
+    run = Run.build()
+    state = { currentRun: run }
+  })
+
+  describe('when well exists', () => {
+    it('mutates the well, e.g. sequencing_mode', () => {
+      position = 'A1'
+      let payload = { position: position, property: 'sequencing_mode', with: 'seqMode' }
+      Mutations.default.mutateWell(state, payload)
+
+      let well = state.currentRun.plate.wells.filter(well => well.position === position)[0]
+      expect(well.sequencing_mode).toEqual('seqMode')
+    })
+
+    it('mutates the well, e.g. libraries', () => {
+      position = 'A1'
+      let payload = { position: position, property: 'libraries', with: [{ id: 123, barcode: 'TRAC-1' }] }
+      Mutations.default.mutateWell(state, payload)
+
+      let well = state.currentRun.plate.wells.filter(well => well.position === position)[0]
+      expect(well.libraries.length).toEqual(payload.with.length)
+      expect(well.libraries[0].id).toEqual(123)
+      expect(well.libraries[0].barcode).toEqual('TRAC-1')
+    })
+  })
+
+  describe('when well does not exist', () => {
+    it('creates the well, then mutates the given property of the well, with the give prop', () => {
+      position = 'X1'
+      let payload = { position: position, property: 'sequencing_mode', with: 'seqMode' }
+      Mutations.default.mutateWell(state, payload)
+
+      let well = state.currentRun.plate.wells.filter(well => well.position === position)[0]
+      expect(well.sequencing_mode).toEqual('seqMode')
+    })
+  })
+
 })
