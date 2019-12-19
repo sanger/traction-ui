37fa58da4483c361689f24d65b0d36e8e199814b
Ben Topping <bt8@mib113631i.internal.sanger.ac.uk>
2019-12-19 09:06:32 +0000

    Added tests
    Created pagenotfound spec

--- tests/unit/views/PageNotFound.spec.js
+++ tests/unit/views/PageNotFound.spec.js
@@ -1,54 +1,35 @@
 import PageNotFound from '@/views/PageNotFound'
-import Pacbio from '@/views/Pacbio'
-import Saphyr from '@/views/Saphyr'
-import SaphyrRuns from '@/views/saphyr/SaphyrRuns'
-import PacbioRuns from '@/views/pacbio/PacbioRuns'
-import Dashboard from '@/views/Dashboard'
-import { mount, localVue, store } from '../testHelper'
+import { mount, localVue } from '../testHelper'
 import VueRouter from 'vue-router'
 
 describe('PageNotFound.vue', () => {
 
-  let wrapper, router, box
+  let wrapper, router
 
   beforeEach(() => {
-   router = new VueRouter({ routes: [
-    {
-        path: '/dashboard',
-        name: 'Dashboard',
-        component: Dashboard
-    },
-    {
-        path: '/saphyr',
-        component: Saphyr,
-        children: 
-            { path: 'runs', name: 'SaphyrRuns', component: SaphyrRuns },
-    },
-    {
-        path: '/pacbio',
-        component: Pacbio,
-        children:
-            { path: 'runs', name: 'PacbioRuns', component: PacbioRuns },
-    },
-    ]
+    router = new VueRouter({ routes: [
+      { path: '/dashboard', name: 'Dashboard' },
+      { path: '/saphyr/runs', name: 'SaphyrRuns'},
+      { path: '/pacbio/runs', name: 'PacbioRuns'},
+      ]
     })
-    wrapper = mount(PageNotFound, { router, localVue,
-      store }
-    )
-    box = wrapper.find('PageNotFound')
-    console.log(box)
-
+    wrapper = mount(PageNotFound, { router, localVue })
   })
 
 
-
-  it('will have page not found text', () => {
-    box.find('#backToDashboard').trigger('click')
-    expect(wrapper.vm.$route.path).toBe('/')
-  })
-  it('will have page not found text', () => {
-    box.find('#backToPacbioRuns').trigger('click')
-    expect(wrapper.vm.$route.path).toBe('/pacbio/runs')
+  describe('Links on page ', () => {
+    it('will redirect to dashboard', () => {
+      wrapper.find('#backToDashboard').trigger('click')
+      expect(wrapper.vm.$route.path).toBe('/dashboard')
+    })
+    it('will redirect to pacbio runs', () => {
+      wrapper.find('#backToPacbioRuns').trigger('click')
+      expect(wrapper.vm.$route.path).toBe('/pacbio/runs')
+    })
+    it('will redirect to saphyr runs', () => {
+      wrapper.find('#backToSaphyrRuns').trigger('click')
+      expect(wrapper.vm.$route.path).toBe('/saphyr/runs')
+    })
   })
   
 })
