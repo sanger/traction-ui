d3b3f6829531829ccad5478bd0d06114a3c3fd93
Harriet Craven <hc6@sanger.ac.uk>
2020-01-09 12:02:07 +0000

    Fix update Run state - reload page
    Use the vuex pattern:
    component calls an action, which does the async call,
    then calls a mutator, which updates the state immutably

--- tests/unit/store/traction/mutations.spec.js
+++ tests/unit/store/traction/mutations.spec.js
@@ -4,7 +4,7 @@ import mutations from '@/store/traction/mutations'
 
 let tractionTags
 
-describe('mutations', () => {
+describe('setTags', () => {
     beforeEach(() => {
         tractionTags = new Response(Data.TractionTags).deserialize.tractionTags
     })
@@ -15,5 +15,29 @@ describe('mutations', () => {
         }
         mutations.setTags(state, tractionTags)
         expect(state.tractionTags).toEqual(tractionTags)
+    })
+})
+
+describe('updateRun', () => {
+    let runs, state, updatedRun
+
+    beforeEach(() => {
+        runs = new Response(Data.Runs).deserialize.runs
+
+        state = { pacbio: { runs: { runs: runs } } }
+
+        updatedRun = {
+            id: '1',
+            type: 'runs',
+            state: 'started',
+            chip_barcode: 'TRAC-123',
+            created_at: '03/21/2019 01:01'
+        }
+    })
+
+    it('updates the given run in the states runs', () => {
+        mutations.updateRun(state, updatedRun)
+        let recievedRun = state.pacbio.runs.runs.filter(r => r.id === updatedRun.id)[0]
+        expect(recievedRun.state).toEqual(updatedRun.state)
     })
 })
