3e632486f1d41e175f3bfd832329b82d447def4e
Ben Topping <ben.topping@outlook.com>
2020-04-20 16:18:19 +0100

    added global footer and footer component

---
+++ src/components/InfoFooter.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <br>
+        <b-icon-server></b-icon-server>
+        <b-form>
+            <b-form-group :label="info.environment">
+            </b-form-group>
+            <b-form-group :label="info.host_name">
+            </b-form-group>
+        </b-form>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'infoFooter',
+  data() {
+    return {
+      info: {
+        environment: process.env.NODE_ENV,
+        host_name: location.hostname
+      }
+    }
+  }
+}
+</script>
