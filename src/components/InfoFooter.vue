1221c75cd3f93d87870853c7ca0c856b3b05aeb6
Ben Topping <ben.topping@outlook.com>
2020-04-22 09:35:47 +0100

    updated footer

--- src/components/InfoFooter.vue
+++ src/components/InfoFooter.vue
@@ -1,27 +1,33 @@
 <template>
-    <div>
+    <div id="footer">
         <br>
-        <b-icon-server></b-icon-server>
-        <b-form>
-            <b-form-group :label="info.environment">
-            </b-form-group>
-            <b-form-group :label="info.host_name">
-            </b-form-group>
-        </b-form>
+        <b-row class="justify-content-md-center">
+          <b-col col lg="3">
+            <b-row>
+              <b-link href="https://www.sanger.ac.uk/">Wellcome Sanger Institute</b-link>
+            </b-row>
+            <b-row> 
+              <b-link href="https://www.sanger.ac.uk/science/groups/production-software-development">
+                A tool from LIMS and Informatics
+              </b-link>
+            </b-row>
+          </b-col>
+          <b-col col lg="3">
+            <b-row>Traction [{{ $data.environment }}]</b-row>
+            <b-row>{{ $data.host_name }}</b-row>
+          </b-col>
+        </b-row>
     </div>
 </template>
 
 <script>
-
 export default {
   name: 'infoFooter',
   data() {
     return {
-      info: {
         environment: process.env.NODE_ENV,
         host_name: location.hostname
       }
-    }
   }
 }
 </script>
