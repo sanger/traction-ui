e9153a56324aae3f852e84e89fbf788bc1b57114
Harriet Craven <hc6@sanger.ac.uk>
2020-04-29 10:54:37 +0100

    Created GridION SVG

---
+++ src/components/svg/GridionSVG.vue
@@ -0,0 +1,41 @@
+<template> 
+  <div class="plate">
+
+    <svg xmlns="http://www.w3.org/2000/svg" width="1158" height="568" >
+      <g id="6170b4b58694418f9b51248b46e1ecff" transform="matrix(1,0,0,1,176,95)">
+        <rect width="509" height="307" rx="0" ry="0" x="0" y="0" style="stroke-width: 2; fill: rgb(255, 255, 255); fill-opacity: 1; stroke: rgb(27, 50, 128); stroke-opacity: 1;"  id="d8ae15d089f24463aad905b1310f7860"/>
+      </g>
+
+      <g transform="matrix(1,0,0,1,214,134)">
+        <rect width="61" height="227" rx="0" ry="0" x="0" y="0" style="stroke-width: 2; fill: rgb(67, 136, 204); fill-opacity: 0.309804; stroke: rgb(27, 50, 128); stroke-opacity: 1;" id="bb35d7182fd84dc2a25c8ef056719d5a"/>
+      </g>
+
+      <g transform="matrix(1,0,0,1,302,135)">
+        <rect width="61" height="227" rx="0" ry="0" x="0" y="0" style="stroke-width: 2; fill: rgb(67, 136, 204); fill-opacity: 0.309804; stroke: rgb(27, 50, 128); stroke-opacity: 1;" p:name="rrRect" id="a440ff096176476a8159f406cfe605e1" transform="translate(1,1)"/>
+      </g>
+      
+      <g transform="matrix(1,0,0,1,400,135)">
+        <rect width="61" height="227" rx="0" ry="0" x="0" y="0" style="stroke-width: 2; fill: rgb(67, 136, 204); fill-opacity: 0.309804; stroke: rgb(27, 50, 128); stroke-opacity: 1;" p:name="rrRect" id="419d808098d2438186042033751ae852" transform="translate(1,1)"/>
+      </g>
+      
+      <g transform="matrix(1,0,0,1,497,135)">
+        <rect width="61" height="227" rx="0" ry="0" x="0" y="0" style="stroke-width: 2; fill: rgb(67, 136, 204); fill-opacity: 0.309804; stroke: rgb(27, 50, 128); stroke-opacity: 1;" p:name="rrRect" id="324d1b3e78dd4212a9f0e149a8e67af3" transform="translate(1,1)"/>
+      </g>
+      
+      <g transform="matrix(1,0,0,1,589,135)">
+        <rect width="61" height="227" rx="0" ry="0" x="0" y="0" style="stroke-width: 2; fill: rgb(67, 136, 204); fill-opacity: 0.309804; stroke: rgb(27, 50, 128); stroke-opacity: 1;" p:name="rrRect" id="9c96f79f72174f80ac04ac6f7a54486b" transform="translate(1,1)"/>
+      </g>
+    </svg>
+  </div>
+  
+</template>
+
+<script>
+
+export default {
+  name: 'GridionSVG',
+}
+</script>
+
+<style scoped lang="scss">
+</style>
