e021e931ba16f09a57e0ee1e313de4bfc871c4da
Harriet Craven <hc6@sanger.ac.uk>
2020-05-07 13:16:11 +0100

    Saphyr requests was going to pacbio, not saphyr

--- src/store/traction/saphyr/requests/getters.js
+++ src/store/traction/saphyr/requests/getters.js
@@ -1,6 +1,6 @@
 const getters = {
     requests: state => state.requests,
-    requestsRequest: (state, getters, rootState) => rootState.api.traction.pacbio.requests,
+    requestsRequest: (state, getters, rootState) => rootState.api.traction.saphyr.requests,
 }
 
 export default getters
