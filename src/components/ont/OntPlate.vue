2ba68636a02b453ec5606cf7b323b9d2a462bf1d
Harriet Craven <hc6@sanger.ac.uk>
2020-05-11 12:52:31 +0100

    Updated pooling samples on library create to not require a tag set or direction

--- src/components/ont/OntPlate.vue
+++ src/components/ont/OntPlate.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <PoolSamplesModal class="float-right" v-bind:plate_barcode="plate.barcode"></PoolSamplesModal>
+    <b-button size="sm" variant="outline-success" :id="'pool-btn-'+plate.barcode" @click="poolSamples">
+      Pool Samples
+    </b-button>
+
     <Plate96SVG v-if="wells" v-bind:height="'30%'" v-bind:width="'30%'">
       <OntWell v-for="(well, position) in plateMap.wells" v-bind="well" v-bind:key="position" v-bind:well_info="getWellAt(position)">
       </OntWell>
@@ -13,8 +16,8 @@
 import Plate96SVG from '@/components/svg/Plate96SVG'
 import PlateMap from '@/config/PlateMap'
 import OntWell from '@/components/ont/OntWell'
-import PoolSamplesModal from '@/components/ont/PoolSamplesModal'
 import WELLS_ALL_FOR_PLATE_QUERY from '@/graphql/queries/WellsAllForPlate.query.gql'
+import POOL_SAMPLES from '@/graphql/queries/PoolSamples.mutation.gql'
 
 export default {
   name: 'OntPlate',
@@ -22,7 +25,6 @@ export default {
   components: {
     Plate96SVG,
     OntWell,
-    PoolSamplesModal
   },
   apollo: {
     wells: {
@@ -38,6 +40,22 @@ export default {
     getWellAt(position) {
       let well = this.wells.filter(well => well.position == position)[0]
       return well ? well : { position: position, materials: [] }
+    },
+    poolSamples () {
+      this.$apollo.mutate({
+        mutation: POOL_SAMPLES,
+        variables: {
+          plate_barcode: this.plate.barcode
+        }
+      }).then(data => {
+        let response = data.data.createOntLibraries
+        if (response.errors.length > 0) {
+          this.$emit('alert', 'Failure: ' + data.data.createOntLibraries.errors.join(', '), 'danger')
+        } else {
+          let libraryBarcodes = response.tubes.map(t => t.barcode).join(', ')
+          this.$emit('alert', 'Library(s) were created with barcodes: ' + libraryBarcodes, 'success')
+        }
+      })
     }
   },
   computed: {
--- tests/unit/components/ont/OntPlate.spec.js
+++ tests/unit/components/ont/OntPlate.spec.js
@@ -1,12 +1,11 @@
 import Plate96SVG from '@/components/svg/Plate96SVG'
 import OntPlate from '@/components/ont/OntPlate'
 import OntWell from '@/components/ont/OntWell'
-import PoolSamplesModal from '@/components/ont/PoolSamplesModal'
 import PlateMap from '@/config/PlateMap'
 import { localVue, mount } from '../../testHelper'
 
 describe('OntPlate.vue', () => {
-  let wrapper, plate, wellsData
+  let wrapper, plate, wellsData, mutate
 
   beforeEach(() => {
     wellsData = [
@@ -29,6 +28,8 @@ describe('OntPlate.vue', () => {
       }
     ]
 
+    mutate = jest.fn()
+
     wrapper = mount(OntPlate, {
       localVue,
       propsData: { plate: { id: 1, barcode: 'TRAC-1-1' }},
@@ -39,6 +40,11 @@ describe('OntPlate.vue', () => {
       data() {
         return {
           wells: wellsData
+        }
+      },
+      mocks: {
+        $apollo: {
+          mutate: mutate
         }
       }
     })
@@ -77,17 +83,55 @@ describe('OntPlate.vue', () => {
     it('has a Plate96SVG component', () => {
       expect(wrapper.contains(Plate96SVG)).toBe(true)
     })  
-
-    it('has a Plate96SVG component', () => {
-      expect(wrapper.contains(PoolSamplesModal
-      )).toBe(true)
-    })
   })
 
   describe('SVG wells', () => {
     it('has the correct number of wells', () => {
       let ellipses = wrapper.findAll(OntWell)
       expect(ellipses.length).toEqual(Object.keys(PlateMap.wells).length)
+    })
+  })
+
+  describe('Pool Samples button', () => {
+    it('is shows button', () => {
+      let button = wrapper.find('#pool-btn-TRAC-1-1')
+      expect(button.text()).toEqual('Pool Samples')
+    })
+
+    it('shows an alert on success', async () => {
+      let mockResponse = { data: { createOntLibraries: { tubes: [{ barcode: 'TRAC-1-1' }, { barcode: 'TRAC-1-2' } ], errors: []} } }
+
+      let promise = new Promise((resolve) => {
+        resolve(mockResponse)
+      })
+
+      mutate.mockReturnValue(promise)
+
+      let button = wrapper.find('#pool-btn-TRAC-1-1')
+      await button.trigger('click')
+
+      expect(mutate).toBeCalled()
+      expect(wrapper.emitted().alert).toBeTruthy()
+      expect(wrapper.emitted().alert[0][0]).toEqual('Library(s) were created with barcodes: TRAC-1-1, TRAC-1-2')
+      expect(wrapper.emitted().alert[0][1]).toEqual('success')
+    })
+
+    it('shows an alert on failure', async () => {
+      let mockResponse = { data: { createOntLibraries: { tubes: [], errors: ['this is an error'] } } }
+
+      let promise = new Promise((resolve) => {
+        resolve(mockResponse)
+      })
+
+      mutate.mockReturnValue(promise)
+
+      let button = wrapper.find('#pool-btn-TRAC-1-1')
+      await button.trigger('click')
+
+      expect(mutate).toBeCalled()
+      expect(wrapper.emitted().alert).toBeTruthy()
+      expect(wrapper.emitted().alert[0][0]).toEqual('Failure: this is an error')
+      expect(wrapper.emitted().alert[0][1]).toEqual('danger')
     })
   })
 })
