216e8aa1b87f687d0649b1ff9badef9eb064f06d
Ben Topping <ben.topping@outlook.com>
2020-05-14 17:37:28 +0100

    added alert

--- src/views/ont/OntLibraries.vue
+++ src/views/ont/OntLibraries.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="ont-libraries">
+    <alert ref='alert'></alert>
     <b-table 
       id="libraries-table"
       hover 
@@ -28,6 +29,7 @@ import LIBRARIES_ALL_QUERY from '@/graphql/queries/LibrariesAll.query.gql'
 import PrinterModal from '@/components/PrinterModal'
 import Helper from '@/mixins/Helper'
 import TableHelper from '@/mixins/TableHelper'
+import Alert from '@/components/Alert'
 
 export default {
   name: 'OntLibraries',
