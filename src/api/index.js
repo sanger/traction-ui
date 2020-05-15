3d5d4e602cf2982849882f5e2156cccf95c8d9bb
Stephen Inglis <si5@sanger.ac.uk>
2020-05-15 08:56:07 +0100

    merged develop.

--- src/api/index.js
+++ src/api/index.js
@@ -3,13 +3,11 @@ import JsonApi from './JsonApi'
 import Request from './Request'
 import Response from './Response'
 import ApiBuilder from './ApiBuilder'
-import SequencescapePlates from './SequencescapePlates'
 
 export default {
   Config,
   JsonApi,
   Request,
   Response,
-  ApiBuilder,
-  SequencescapePlates
+  ApiBuilder
 }
--- src/graphql/client.js
+++ src/graphql/client.js
@@ -1,14 +1,19 @@
 import Vue from 'vue'
 import VueApollo from 'vue-apollo'
 import ApolloClient from 'apollo-boost'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import resolvers from '@/graphql/resolvers'
+
+const cache = new InMemoryCache()
 
 Vue.use(VueApollo)
 
 const apolloClient = new ApolloClient({
-  uri: process.env.VUE_APP_TRACTION_GRAPHQL_URL
+  uri: process.env.VUE_APP_TRACTION_GRAPHQL_URL,
+  cache,
+  resolvers,
 })
 
 export const apolloProvider = new VueApollo({
   defaultClient: apolloClient,
 })
-
