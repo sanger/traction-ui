288622c598af553342dfceb3711beb641d00f7a4
Ben Topping <ben.topping@outlook.com>
2020-05-19 11:38:21 +0100

    cleaned up footer

--- src/components/InfoFooter.vue
+++ src/components/InfoFooter.vue
@@ -14,10 +14,9 @@
           </b-col>
           <b-col col lg="3">
             <b-row>Traction [{{ this.environment }}]</b-row>
-            <b-row>{{ this.release }}</b-row>
             <b-row>
               <b-link :href="this.repo">
-                {{ this.repo.slice(38, this.repo.length) }}
+                {{ getRelease() }}
               </b-link>
             </b-row>
           </b-col>
@@ -31,22 +30,25 @@ export default {
   data() {
     return {
         environment: process.env.NODE_ENV,
-        release: "",
         repo: ""
       }
   },
   methods: {
+    getRelease() {
+      if (this.environment != 'development') {
+        return this.repo.slice(51, this.repo.length)
+      } else {
+        return this.repo.slice(38, this.repo.length)
+      }
+    },
     provider() {
       if (this.environment != 'development') {
         Promise.all([
-          fetch('RELEASE').then(response => response.text()),
           fetch('REPO').then(response => response.text())
-        ]).then(([response1,response2]) => {
-          this.release = response1;
-          this.repo = response2;
-        }).catch(err => console.error(err));
+        ]).then(([response1]) => {
+          this.repo = response1
+        }).catch(err => console.error(err))
       } else {
-        this.release = "Local Version"
         this.repo = "https://github.com/sanger/traction-ui/releases"
       }
     }
