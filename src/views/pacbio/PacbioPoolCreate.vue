c46366631a8c28dc10953c4c7288d1eca4914ac2
Stephen Inglis <519327+stevieing@users.noreply.github.com>
2021-06-25 08:57:21 +0100

    Added the basic tag components.

--- src/views/pacbio/PacbioPoolCreate.vue
+++ src/views/pacbio/PacbioPoolCreate.vue
@@ -1,13 +1,22 @@
 <template>
   <div>
     Pool
+    <PacbioTagSetList></PacbioTagSetList>
+    <PacbioTagSetShow></PacbioTagSetShow>
   </div>
 </template>
 
 <script>
 
+import PacbioTagSetList from '@/components/pacbio/PacbioTagSetList'
+import PacbioTagSetShow from '@/components/pacbio/PacbioTagSetShow'
+
 export default {
   name: 'PacbioPoolCreate',
+  components: {
+    PacbioTagSetList,
+    PacbioTagSetShow
+  },
   data() {
     return {
     }
