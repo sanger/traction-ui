ec7a60d19ab3aa92b6b878c7b422bc8fd98c45c7
Stephen Inglis <519327+stevieing@users.noreply.github.com>
2021-06-25 13:40:47 +0100

    Added outline for pool create view.

--- src/views/pacbio/PacbioPoolCreate.vue
+++ src/views/pacbio/PacbioPoolCreate.vue
@@ -1,6 +1,34 @@
 <template>
   <div>
+
     Pool
+    <b-container fluid id="pool">
+      <b-row id="row1">
+        <b-col data-type="plate-find" class="border">
+          Find plates
+        </b-col>
+         <b-col >
+           <b-row>
+             <b-col data-type="tag-group-list" class="border tag-group-list">
+               Find tag groups
+             </b-col>
+           </b-row>
+           <b-row>
+             <b-col data-type="tag-group" class="border tag-group">
+               Tag group
+             </b-col>
+           </b-row>
+        </b-col>
+      </b-row>
+      <b-row id="row2">
+        <b-col data-type="selected-plate-list" class="border">
+          Selected plates
+        </b-col>
+        <b-col data-type="library-list" class="border">
+          Libraries
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
@@ -14,3 +42,18 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+  #row1 {
+    height: 250px;
+  }
+  #row2 {
+    height: 750px;
+  }
+  .tag-group-list {
+    height: 75px;
+  }
+  .tag-group {
+    height: 175px;
+  }
+</style>
