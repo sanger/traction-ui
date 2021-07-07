5132b1094dcdfbf3bf3a609d07b0a655e38dbe0b
Stephen Inglis <519327+stevieing@users.noreply.github.com>
2021-07-07 14:19:51 +0100

    Removed test to update tag selected.

--- tests/unit/components/pacbio/PacbioPoolLibraryEdit.spec.js
+++ tests/unit/components/pacbio/PacbioPoolLibraryEdit.spec.js
@@ -3,23 +3,25 @@ import { mount, store, localVue } from 'testHelper'
 
 const request = {
   id: '1',
-  sample_name: 'Sample1',
+  sample_name: "Sample1",
 }
 
 const tagSet = {
   id: '1',
   name: 'TagSet1',
   tags: [
-    { id: '1', name: 'tag1' },
-    { id: '2', name: 'tag2' },
-    { id: '3', name: 'tag3' },
-  ],
+    { id: '1', name: 'tag1'},
+    { id: '2', name: 'tag2'},
+    { id: '3', name: 'tag3'},
+  ]
 }
 
 describe('PacbioPoolLibraryEdit.vue', () => {
+
   let wrapper
 
   beforeEach(() => {
+
     // TODO: This would probably better done using a separate component and an emit
     // but that is a bigger job
     store.state.traction.pacbio.poolCreate.selected.tagSet = tagSet
@@ -28,8 +30,8 @@ describe('PacbioPoolLibraryEdit.vue', () => {
       store,
       localVue,
       propsData: {
-        request,
-      },
+        request
+      }
     })
   })
 
@@ -49,9 +51,15 @@ describe('PacbioPoolLibraryEdit.vue', () => {
     )
   })
 
-  it('will allow the user to select a tag', async () => {
-    const options = wrapper.find('[data-type=tag-list]').findAll('option')
-    await options.at(1).setSelected()
-    expect(wrapper.vm.tag_id).toEqual('1')
+  describe('when the user selects a tag', () => {
+
+
+    it('will updated the tag_id', async () => {
+      const options = wrapper.find('[data-type=tag-list]').findAll('option')
+      await options.at(1).setSelected()
+      expect(wrapper.vm.tag_id).toEqual('1')
+    })
+
   })
+
 })
