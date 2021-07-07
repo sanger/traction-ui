18e2fb3f1dc198c2fe5760355441510b127e98da
Stephen Inglis <519327+stevieing@users.noreply.github.com>
2021-07-07 14:19:23 +0100

    Added tag selection to the PacbioPoolLibraryEdit component.

--- src/components/pacbio/PacbioPoolLibraryEdit.vue
+++ src/components/pacbio/PacbioPoolLibraryEdit.vue
@@ -1,8 +1,14 @@
 <template>
   <div data-type="pool-library-edit">
     <div data-attribute="sample-name">
-      {{ request.sample_name}}
+      {{ request.sample_name }}
     </div>
+    <b-form-select
+      v-if="tagList.length > 0"
+      v-model="tag_id"
+      data-type="tag-list"
+      :options="options"
+    ></b-form-select>
   </div>
 </template>
 
@@ -16,6 +22,21 @@ export default {
         return ''
       },
     },
-  }
+  },
+  data() {
+    return {
+      tag_id: null,
+    }
+  },
+  computed: {
+    tagList() {
+      return this.$store.getters[
+        'traction/pacbio/poolCreate/selectedTagSet'
+      ].tags.map(({ id: value, name: text }) => ({ value, text }))
+    },
+    options() {
+      return [{ value: null, text: 'Please select a tag' }, ...this.tagList]
+    },
+  },
 }
 </script>
--- tests/unit/components/pacbio/PacbioPoolLibraryEdit.spec.js
+++ tests/unit/components/pacbio/PacbioPoolLibraryEdit.spec.js
@@ -1,24 +1,57 @@
 import PacbioPoolLibraryEdit from '@/components/pacbio/PacbioPoolLibraryEdit'
-import { mount, store } from 'testHelper'
+import { mount, store, localVue } from 'testHelper'
 
 const request = {
   id: '1',
-  sample_name: "Sample1",
+  sample_name: 'Sample1',
+}
+
+const tagSet = {
+  id: '1',
+  name: 'TagSet1',
+  tags: [
+    { id: '1', name: 'tag1' },
+    { id: '2', name: 'tag2' },
+    { id: '3', name: 'tag3' },
+  ],
 }
 
 describe('PacbioPoolLibraryEdit.vue', () => {
-
   let wrapper
 
   beforeEach(() => {
+    // TODO: This would probably better done using a separate component and an emit
+    // but that is a bigger job
+    store.state.traction.pacbio.poolCreate.selected.tagSet = tagSet
+
     wrapper = mount(PacbioPoolLibraryEdit, {
+      store,
+      localVue,
       propsData: {
-        request
-      }
+        request,
+      },
     })
+  })
+
+  it('will have an id', () => {
+    expect(wrapper.vm.request).toEqual(request)
   })
 
   it('will have a sample name', () => {
     expect(wrapper.find('[data-attribute=sample-name]').text()).toEqual('Sample1')
+  })
+
+  it('will have a list of tags', () => {
+    console.log(wrapper.vm.options)
+    expect(wrapper.find('[data-type=tag-list]').findAll('option').length).toEqual(
+      // will also include null value
+      tagSet.tags.length + 1,
+    )
+  })
+
+  it('will allow the user to select a tag', async () => {
+    const options = wrapper.find('[data-type=tag-list]').findAll('option')
+    await options.at(1).setSelected()
+    expect(wrapper.vm.tag_id).toEqual('1')
   })
 })
