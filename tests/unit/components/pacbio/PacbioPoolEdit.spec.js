7e250d54460da94b60889aeccd0b41ed6df1ed9a
James Glover <james.glover@sanger.ac.uk>
2021-08-03 16:37:13 +0100

    Add tests for pool edit

--- tests/unit/components/pacbio/PacbioPoolEdit.spec.js
+++ tests/unit/components/pacbio/PacbioPoolEdit.spec.js
@@ -1,24 +1,22 @@
 import { mount, localVue, store } from 'testHelper'
 import PacbioPoolEdit from '@/components/pacbio/PacbioPoolEdit'
 
-const pool = {
-  id: null,
-  template_prep_kit_box_barcode: null,
-  volume: null,
-  concentration: null,
-  fragment_size: null,
-}
+describe('pacbioPoolEdit#new', () => {
+  const pool = {
+    id: null,
+    template_prep_kit_box_barcode: null,
+    volume: null,
+    concentration: null,
+    fragment_size: null,
+  }
 
-store.state.traction.pacbio.poolCreate.pool = pool
-
-describe('pacbioPoolEdit', () => {
   let wrapper
-
   beforeEach(() => {
     wrapper = mount(PacbioPoolEdit, {
       localVue,
       store,
     })
+    store.state.traction.pacbio.poolCreate.pool = pool
   })
 
   describe('input', () => {
@@ -46,6 +44,62 @@ describe('pacbioPoolEdit', () => {
       const input = wrapper.find('[data-attribute=fragment-size]')
       await input.setValue('100')
       expect(store.state.traction.pacbio.poolCreate.pool.fragment_size).toEqual('100')
+    })
+  })
+
+  describe('submit button', () => {
+    it('says Create pool', () => {
+      const button = wrapper.find('[data-action=create-pool]')
+      expect(button.text()).toContain('Create Pool')
+    })
+  })
+})
+
+describe('pacbioPoolEdit#edit', () => {
+  const pool = {
+    id: '1',
+    template_prep_kit_box_barcode: '017865101789500022821',
+    volume: 10,
+    concentration: 2.4,
+    fragment_size: 100,
+  }
+
+  let wrapper
+
+  beforeEach(() => {
+    wrapper = mount(PacbioPoolEdit, {
+      localVue,
+      store,
+    })
+    store.state.traction.pacbio.poolCreate.pool = pool
+  })
+
+  describe('input', () => {
+    it('template prep kit box barcode', async () => {
+      const input = wrapper.find('[data-attribute=template-prep-kit-box-barcode]')
+      expect(input.element.value).toEqual('017865101789500022821')
+    })
+
+    it('volume', async () => {
+      const input = wrapper.find('[data-attribute=volume]')
+      expect(input.element.value).toEqual('10')
+    })
+
+    it('concentration', async () => {
+      const input = wrapper.find('[data-attribute=concentration]')
+      expect(input.element.value).toEqual('2.4')
+    })
+
+    it('fragment size', async () => {
+      const input = wrapper.find('[data-attribute=fragment-size]')
+      expect(input.element.value).toEqual('100')
+    })
+  })
+
+  describe('submit button', () => {
+    it('says Update pool', () => {
+      const button = wrapper.find('[data-action=create-pool]')
+      expect(button.text()).toContain('Create Pool')
     })
   })
 })
