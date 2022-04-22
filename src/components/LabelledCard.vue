d86576ca4a8d0dc6b765caad5c6295544caf4c8e
Seena Nair <55585488+seenanair@users.noreply.github.com>
2022-04-22 16:04:44 +0100

    Dashboard styling
    Improvements on dashboard page styling

---
+++ src/components/LabelledCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div
+    class="mt-4 border-t-4 border-sp rounded-md space-y-1 bg-sdb-400 text-gray-100"
+  >
+    <div class="text-center text-2xl">{{ title }}</div>
+    <div class="px-4 sm:px-2 lg:px-4">
+      <div class="text-center text-xs italic">{{ description }}</div>
+      <div class="py-2">
+        <h1 class="border-b-2 border-sp" />
+      </div>
+    </div>
+    <div class="flex flex-row flex-wrap  gap-y-8 gap-x-3 mt-2 px-3 py-4 ">
+      <slot/>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'LabelledCard',
+  components: {},
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
---
+++ src/components/Link.vue
@@ -0,0 +1,42 @@
+<template>
+  <div :class="getClass()" @click="$router.push(`${link}`)">
+    <router-link :to="`${link}`">
+      {{ name }}
+    </router-link>
+  </div>
+</template>
+
+<script>
+export const ViewType = {
+  Button: 1, //Display as dashborad button
+  MenuItem: 2, // Display as menu item
+}
+
+export default {
+  name: 'Link',
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    link: {
+      type: String,
+      required: true,
+    },
+    viewType: {
+      type: ViewType,
+      default: ViewType.Button,
+    },
+  },
+  methods: {
+    getClass() {
+      return {
+        'flex w-32 h-24 whitespace-normal justify-center items-center rounded-md border border-transparent text-base font-medium focus:outline-none focus:ring-2 focus:ring-offset-2 sm:mt-0 text-white bg-sp hover:bg-sp-600 shadow-sm focus:border-sp-700 focus:shadow-outline-sp active:bg-sp-700':
+          this.viewType == ViewType.Button,
+        'px-3 py-2 rounded-md text-sm font-medium focus:outline-none focus:text-white focus:bg-gray-700 text-gray-100 hover:text-white hover:bg-gray-700':
+          this.viewType == ViewType.MenuItem,
+      }
+    },
+  },
+}
+</script>
