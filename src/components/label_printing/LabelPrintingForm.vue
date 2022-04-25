c5078a3b3941e492b35f01ff0788f0ac050f7fbe
Andrew Sparkes <as28@sanger.ac.uk>
2022-04-25 17:33:23 +0100

    Updated form

--- src/components/label_printing/LabelPrintingForm.vue
+++ src/components/label_printing/LabelPrintingForm.vue
@@ -1,35 +1,47 @@
 <template>
   <div>
     <b-form @submit="onSubmit">
-      <b-container fluid>
-        <b-row class="form-group form-row">
-          <b-form-group
-            id="barcode_input_group"
-            label="Barcode:"
-            label-for="barcode_input"
-            description="Scan the barcode you wish to create labels for."
-          >
-            <b-form-input
-              id="barcode_input"
-              placeholder="Scan barcode"
-              required
-            ></b-form-input>
-          </b-form-group>
-        </b-row>
-        <b-row class="form-group form-row">
-          <b-form-select v-model="selected_suffix" :options="suffix_options" required></b-form-select>
-        </b-row>
-        <b-row class="form-group form-row">
-          <b-form-select v-model="selected_number_of_labels_to_print" :options="number_of_suffixes_options" required></b-form-select>
-        </b-row>
-        <b-row class="form-group form-row">
-          <b-form-select v-model="selected_printer" :options="printer_options" required></b-form-select>
-        </b-row>
+      <b-form-group
+          id="barcode_input_group"
+          label="Barcode:"
+          label-for="barcode_input"
+          description="Please scan the barcode you wish to create labels for."
+        >
+        <b-form-input
+          id="barcode_input"
+          placeholder="Scan barcode"
+          required
+        ></b-form-input>
+      </b-form-group>
 
-        <b-row class="form-group form-row">
-          <b-button type="submit" variant="primary">Print</b-button>
-        </b-row>
-      </b-container>
+      <b-form-group
+          id="suffix_selection_group"
+          label="Suffix:"
+          label-for="suffix_selection"
+          description="Please select the suffix you want to use from the list."
+        >
+        <b-form-select id="suffix_selection" v-model="selected_suffix" :options="suffix_options" required></b-form-select>
+      </b-form-group>
+
+      <b-form-group
+          id="number_of_labels_group"
+          label="Number of labels:"
+          label-for="number_of_labels"
+          description="Please enter the number of labels to make (with incrementing suffixes)."
+        >
+        <b-form-input v-model="selected_number_of_labels_to_print" type="number" min="1" max="9" required></b-form-input>
+      </b-form-group>
+
+      <b-form-group
+          id="printer_choice_group"
+          label="Choice of Printer:"
+          label-for="printer_choice"
+          description="Please select which printer you wish to use to print the labels."
+        >
+        <b-form-select v-model="selected_printer" :options="printer_options" required></b-form-select>
+      </b-form-group>
+
+      <b-button type="submit" variant="primary">Print</b-button>
     </b-form>
   </div>
 </template>
@@ -46,7 +58,6 @@
           { value: 'BB', text: 'BB suffix desc' },
         ],
         selected_number_of_labels_to_print: null,
-        number_of_suffixes_options: [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ],
         selected_printer: null,
         printer_options: [
           { value: null, text: 'Please select a printer' },
