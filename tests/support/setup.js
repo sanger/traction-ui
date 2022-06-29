ded80e2fe18c427278407eded9a597fd0ebbfc56
James Glover <james.glover@sanger.ac.uk>
2022-06-29 12:16:49 +0100

    Globally mock swrv
    We expect ot use flagged-features
    extensively, yet the currently
    cause test failures when not mocked.
    
    We add this global mock so we only need
    to worry about them when explicitly
    required.

--- tests/support/setup.js
+++ tests/support/setup.js
@@ -1,6 +1,12 @@
 // Global setup to disable the vue console messages
 
 import Vue from 'vue'
+import { vi, afterEach } from 'vitest'
 
 Vue.config.productionTip = false
 Vue.config.devtools = false
+
+vi.mock('swrv')
+afterEach(() => {
+  vi.clearAllMocks()
+})
