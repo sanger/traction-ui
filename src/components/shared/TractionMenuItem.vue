05b300396ec9c9a3f09b44b9e30c3b45e97d25aa
James Glover <james.glover@sanger.ac.uk>
2022-07-01 13:24:18 +0100

    Add custom components

---
+++ src/components/shared/TractionMenuItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <li
+    :class="[
+      'pl-2 pr-2 p-0 m-0', // No margins, padded left and right
+      'hover:bg-sp hover:text-white transition-colors duration-200 ease-in-out',
+      'cursor-pointer',
+      active ? 'text-white bg-sp' : 'text-sp',
+    ]"
+  >
+    <slot />
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'TractionMenu',
+  props: {
+    active: {
+      type: Boolean,
+      default: false,
+    },
+  },
+}
+</script>
--- src/components/shared/index.js
+++ src/components/shared/index.js
@@ -1,11 +1,17 @@
 import FlaggedFeature from '@/components/shared/FlaggedFeature'
 import LoadingFullScreenModal from '@/components/shared/LoadingFullScreenModal'
+import TractionSection from '@/components/shared/TractionSection'
 import TractionHeading from '@/components/TractionHeading'
+import TractionMenu from '@/components/shared/TractionMenu'
+import TractionMenuItem from '@/components/shared/TractionMenuItem'
 
 const registerGlobal = (vue) => {
   vue.component('FlaggedFeature', FlaggedFeature)
   vue.component('LoadingFullScreenModal', LoadingFullScreenModal)
+  vue.component('TractionSection', TractionSection)
   vue.component('TractionHeading', TractionHeading)
+  vue.component('TractionMenu', TractionMenu)
+  vue.component('TractionMenuItem', TractionMenuItem)
 }
 
 export { registerGlobal }
