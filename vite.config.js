e2297ca974405ee952315c21f2b73f3dffc78c63
James Glover <james.glover@sanger.ac.uk>
2022-07-15 13:00:01 +0100

    Fix path resolving on CI

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue2'
-import path from 'path'
 // Help us keep track of what we import, and the size effect it has on the bundle
 import { visualizer } from 'rollup-plugin-visualizer'
+import { fileURLToPath, URL } from 'url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,13 +12,15 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': path.resolve('./src'),
-      '@tests': path.resolve('./tests'),
-      '@support': path.resolve('./tests/support'),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@tests': fileURLToPath(new URL('./tests', import.meta.url)),
+      '@support': fileURLToPath(new URL('./tests/support', import.meta.url)),
       // This is a dummy wrapper to ensure swrv continues to operate with vue 2.7
       // which includes the composition API directly. It can probably be removed when
       // swrv updates
-      '@vue/composition-api': path.resolve('./src/lib/composition_wrapper'),
+      '@vue/composition-api': fileURLToPath(
+        new URL('./src/lib/composition_wrapper', import.meta.url),
+      ),
     },
     extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
   },
