93621d72fdcff4818574f57b1919f63b99a6281c
Katy Taylor <kt17@sanger.ac.uk>
2022-09-15 16:11:31 +0100

    Make SapyhrSamples component able to print to PMB V1 (Toshiba) API and also PMB V2 (Squix) API

--- src/mixins/PrintHelper.js
+++ src/mixins/PrintHelper.js
@@ -1,25 +1,46 @@
 /**
  * A helper mixin to store commonly used functionality
  */
-import { printJob } from '@/api/PrintJobRequests'
+ import { printJob } from '@/api/PrintJobRequests'
 
-const MESSAGE_SUCCESS_PRINTER = 'Printed successfully'
+ const MESSAGE_SUCCESS_PRINTER = 'Printed successfully'
+ const IS_SQUIX = true // TODO: replace with better thing - e.g. load from config
 
-export default {
-  name: 'PrintHelper',
-  methods: {
-    async handlePrintLabel(pipeline, printerName) {
-      let { successful, errors: { message } = {} } = await printJob(
-        printerName,
-        this.selected,
-        pipeline,
-      )
+ export default {
+   name: 'PrintHelper',
+   methods: {
+     async handlePrintLabel(pipeline, printerName) {
+       let successful, errors, message
 
-      if (successful) {
-        this.showAlert(MESSAGE_SUCCESS_PRINTER, 'success')
-      } else {
-        this.showAlert(message, 'danger')
-      }
-    },
-  },
-}
+       if(IS_SQUIX) { // Squix printer
+
+         const params = {
+           printerName: printerName,
+           barcodesList: this.selected.map(v => (v["barcode"])),
+           copies: "1",
+         }
+         const printJobV2Response = await this.printJobV2(params) // this.printJobV2 is not a function
+
+         successful = printJobV2Response.success
+         message = printJobV2Response.data.message
+
+       } else { // Toshiba printer
+
+         // Is this equivalent to the following, without the let?
+         //  let { successful, errors: { message } = {} } = await printJob(
+         successful, { errors: { message } = {} } = await printJob(
+           printerName,
+           this.selected,
+           pipeline,
+         )
+
+       }
+
+       if (successful) {
+         this.showAlert(MESSAGE_SUCCESS_PRINTER, 'success')
+       } else {
+         this.showAlert(message, 'danger')
+       }
+     },
+   },
+ }
