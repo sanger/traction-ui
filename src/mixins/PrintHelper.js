45998878c5e480c91fe65626cd3172f1d727e431
Katy Taylor <kt17@sanger.ac.uk>
2022-09-21 13:28:37 +0100

    get rid of commented out code

--- src/mixins/PrintHelper.js
+++ src/mixins/PrintHelper.js
@@ -1,7 +1,6 @@
 /**
  * A helper mixin to store commonly used functionality
  */
-//  import { printJob } from '@/api/PrintJobRequests'
 
  const MESSAGE_SUCCESS_PRINTER = 'Printed successfully'
 
@@ -19,15 +18,6 @@
 
         const successful = printJobV2Response.success
         const message = printJobV2Response.data.message
-
-        // TODO: figure out how pipeline is being used in this previous code,
-        // and make sure we're including the same info in the V2 request.
-        //
-        // let { successful, errors: { message } = {} } = await printJob(
-        //   printerName,
-        //   this.selected,
-        //   pipeline,
-        // )
 
        if (successful) {
          this.showAlert(MESSAGE_SUCCESS_PRINTER, 'success')
