75567343acc319b911f8fdff59b526cdb89cae24
Katy Taylor <kt17@sanger.ac.uk>
2022-09-21 15:11:39 +0100

    prettier

--- src/mixins/PrintHelper.js
+++ src/mixins/PrintHelper.js
@@ -2,28 +2,27 @@
  * A helper mixin to store commonly used functionality
  */
 
- const MESSAGE_SUCCESS_PRINTER = 'Printed successfully'
+const MESSAGE_SUCCESS_PRINTER = 'Printed successfully'
 
- export default {
-   name: 'PrintHelper',
-   methods: {
-     async handlePrintLabel(pipeline, printerName) {
+export default {
+  name: 'PrintHelper',
+  methods: {
+    async handlePrintLabel(pipeline, printerName) {
+      const params = {
+        printerName: printerName,
+        barcodesList: this.selected.map((v) => v['barcode']),
+        copies: '1',
+      }
+      const printJobV2Response = await this.printJobV2(params)
 
-        const params = {
-          printerName: printerName,
-          barcodesList: this.selected.map(v => (v["barcode"])),
-          copies: "1",
-        }
-        const printJobV2Response = await this.printJobV2(params)
+      const successful = printJobV2Response.success
+      const message = printJobV2Response.data.message
 
-        const successful = printJobV2Response.success
-        const message = printJobV2Response.data.message
-
-       if (successful) {
-         this.showAlert(MESSAGE_SUCCESS_PRINTER, 'success')
-       } else {
-         this.showAlert(message, 'danger')
-       }
-     },
-   },
- }
+      if (successful) {
+        this.showAlert(MESSAGE_SUCCESS_PRINTER, 'success')
+      } else {
+        this.showAlert(message, 'danger')
+      }
+    },
+  },
+}
