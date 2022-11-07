07b852a76dcc35755479587ea9ee998dec303a37
Seena Nair <55585488+seenanair@users.noreply.github.com>
2022-11-07 14:55:09 +0000

    Revert "Update TractionPagination.vue"
    This reverts commit fba7e54ef2bdf6c31c380419e5646e84b35798f7.

--- src/components/shared/TractionPagination.vue
+++ src/components/shared/TractionPagination.vue
@@ -50,7 +50,7 @@ export default {
    * Displays a pagination component
    */
   name: 'TractionPagination',
-
+ 
   props: {
     //value field  which will be bind automatically with 'v-model' prop passed into the component
     value: {
