dbf8a66ccc1bdf131e636199f8463cca6275a9e7
Stephen <519327+stevieing@users.noreply.github.com>
2022-11-25 16:25:29 +0000

    Added skeleton tests for print helpers.

--- tests/unit/lib/LabelPrintingHelpers.spec.js
+++ tests/unit/lib/LabelPrintingHelpers.spec.js
@@ -1,9 +1,27 @@
 import { getCurrentDate } from '@/lib/DateHelpers'
-import { expect } from 'vitest'
+import { describe, expect, it } from 'vitest'
 
 describe('LabelPrintingHelpers.js', () => {
   it('works', () => {
     const date = getCurrentDate()
     expect(date).toBeDefined()
+  })
+
+  describe('suffixDropdownOptions', () => {
+    it('works', () => {
+      expect(true).toBeTruthy()
+    })
+  })
+
+  describe('suffixItems', () => {
+    it('works', () => {
+      expect(true).toBeTruthy()
+    })
+  })
+
+  describe('barcodeLabelItem', () => {
+    it('works', () => {
+      expect(true).toBeTruthy()
+    })
   })
 })
