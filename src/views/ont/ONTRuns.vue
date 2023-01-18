4f20f7c6bf545449c3347bdc065c1aa4166af029
Harriet Craven <hc6@sanger.ac.uk>
2023-01-18 12:34:56 +0000

    Try fix git case sensitive for ont folder

---
+++ src/views/ont/ONTRuns.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    <traction-table
+      id="run-index"
+      hover
+      responsive
+      show-empty
+      small
+      :items="runs"
+      :fields="fields"
+      tbody-tr-class="run"
+    >
+      <template #cell(actions)="row">
+        <traction-button
+          :id="generateId('editRun', row.item.id)"
+          theme="edit"
+          size="sm"
+          class="mr-1"
+          @click="redirectToRun(row.item.id)"
+        >
+          Edit
+        </traction-button>
+      </template>
+    </traction-table>
+
+    <span class="font-weight-bold">Total records: {{ runs.length }}</span>
+
+    <div class="clearfix">
+      <traction-button id="newRun" class="float-left" theme="create" @click="redirectToRun()"
+        >New Run</traction-button
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+import TableHelper from '@/mixins/TableHelper'
+import { mapActions, mapGetters } from 'vuex'
+
+export default {
+  name: 'ONTRuns',
+  mixins: [TableHelper],
+  data() {
+    return {
+      fields: [
+        { key: 'id', label: 'Run ID', sortable: true },
+        { key: 'experiment_name', label: 'Name', sortable: true },
+        { key: 'state', label: 'State', sortable: true },
+        {
+          key: 'instrument_name',
+          label: 'Instrument Name',
+          sortable: true,
+        },
+        { key: 'created_at', label: 'Created at', sortable: true },
+        { key: 'actions', label: 'Actions' },
+      ],
+    }
+  },
+  computed: {
+    ...mapGetters('traction/ont/runs', ['runs']),
+  },
+  created() {
+    this.provider()
+  },
+  methods: {
+    async provider() {
+      try {
+        await this.setRuns()
+      } catch (error) {
+        this.showAlert('Failed to get runs: ' + error.message, 'danger')
+      }
+    },
+    generateId(text, id) {
+      return `${text}-${id}`
+    },
+    redirectToRun(runId) {
+      this.$router.push({ path: `/ont/run/${runId || 'new'}` })
+    },
+    ...mapActions('traction/ont/runs', ['setRuns']),
+  },
+}
+</script>
+
+<style></style>
