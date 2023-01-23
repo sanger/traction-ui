9e1ccf54197acce9660750a7e8cc3194ffb684c0
Harriet Craven <hc6@sanger.ac.uk>
2023-01-23 12:02:33 +0000

    e2e tests

--- tests/e2e/specs/ont/ont_run_view.cy.js
+++ tests/e2e/specs/ont/ont_run_view.cy.js
@@ -2,15 +2,19 @@
 
 describe('ONT Run page', () => {
   beforeEach(() => {
+    cy.intercept('/v1/ont/instruments', {
+      fixture: 'tractionOntInstruments.json',
+    })
     cy.visit('#/ont/run/new')
-
-    cy.get('#instrument-selection').select('Minion')
-    cy.get('#state-selection').select('Pending')
   })
 
   it('Shows the correct information', () => {
+    cy.get('#instrument-selection').select('GXB02004')
+    cy.get('#flowcell-id-1')
+    cy.get('#state-selection').select('Pending')
+
+    cy.contains('ONT Run')
     cy.contains('1. Run Information')
-    cy.contains('2. Add Pools')
-    cy.contains('3. Run Instrument Flowcells')
+    cy.contains('2. Run Instrument Flowcells')
   })
 })
