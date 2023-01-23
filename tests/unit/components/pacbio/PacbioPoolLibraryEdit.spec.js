ba8c9f61cf0d167bd399eed40c6adb62318ea99b
Harriet Craven <hc6@sanger.ac.uk>
2023-01-23 17:07:16 +0000

    prettier

--- tests/unit/components/pacbio/PacbioPoolLibraryEdit.spec.js
+++ tests/unit/components/pacbio/PacbioPoolLibraryEdit.spec.js
@@ -44,7 +44,7 @@ describe('PacbioPoolLibraryEdit.vue', () => {
         propsData: {
           id: 1,
           request,
-          notify
+          notify,
         },
       })
     })
@@ -117,7 +117,7 @@ describe('PacbioPoolLibraryEdit.vue', () => {
     const propsData = {
       id: 1,
       request,
-      notify
+      notify,
     }
 
     it('tag id', () => {
