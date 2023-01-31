b04a47ce941e8112af09796eba768186258ae4d7
Harriet Craven <hc6@sanger.ac.uk>
2023-01-31 14:15:27 +0000

    Add feature flag to ont sequencing run

--- src/views/ont/ONTRuns.vue
+++ src/views/ont/ONTRuns.vue
@@ -1,53 +1,55 @@
 <template>
-  <DataFetcher :fetcher="fetchOntRuns">
-    <div class="clearfix">
-      <traction-pagination
-        v-model="currentPage"
-        class="float-right"
-        :total-rows="runs.length"
+  <flagged-feature name="dpl_281_ont_create_sequencing_runs">
+    <DataFetcher :fetcher="fetchOntRuns">
+      <div class="clearfix">
+        <traction-pagination
+          v-model="currentPage"
+          class="float-right"
+          :total-rows="runs.length"
+          :per-page="perPage"
+          aria-controls="run-index"
+        ></traction-pagination>
+
+        <traction-form-group label-cols-lg="1" label="Per Page" label-for="input-per-page">
+          <traction-input id="input-per-page" v-model="perPage" trim class="w-25"></traction-input>
+        </traction-form-group>
+      </div>
+
+      <traction-table
+        id="run-index"
+        hover
+        responsive
+        show-empty
+        small
+        :items="runs"
+        :fields="fields"
+        tbody-tr-class="run"
         :per-page="perPage"
-        aria-controls="run-index"
-      ></traction-pagination>
-
-      <traction-form-group label-cols-lg="1" label="Per Page" label-for="input-per-page">
-        <traction-input id="input-per-page" v-model="perPage" trim class="w-25"></traction-input>
-      </traction-form-group>
-    </div>
-
-    <traction-table
-      id="run-index"
-      hover
-      responsive
-      show-empty
-      small
-      :items="runs"
-      :fields="fields"
-      tbody-tr-class="run"
-      :per-page="perPage"
-      :current-page="currentPage"
-      :sort-by.sync="sortBy"
-      :sort-desc.sync="sortDesc"
-    >
-      <template #cell(actions)="row">
-        <traction-button
-          :id="generateId('editRun', row.item.id)"
-          theme="edit"
-          size="sm"
-          class="mr-1"
-          @click="redirectToRun(row.item.id)"
-          >Edit</traction-button
-        >
-      </template>
-    </traction-table>
-
-    <span class="font-weight-bold">Total records: {{ runs.length }}</span>
-
-    <div class="clearfix">
-      <traction-button id="newRun" class="float-left" theme="create" @click="redirectToRun()"
-        >New Run</traction-button
+        :current-page="currentPage"
+        :sort-by.sync="sortBy"
+        :sort-desc.sync="sortDesc"
       >
-    </div>
-  </DataFetcher>
+        <template #cell(actions)="row">
+          <traction-button
+            :id="generateId('editRun', row.item.id)"
+            theme="edit"
+            size="sm"
+            class="mr-1"
+            @click="redirectToRun(row.item.id)"
+            >Edit</traction-button
+          >
+        </template>
+      </traction-table>
+
+      <span class="font-weight-bold">Total records: {{ runs.length }}</span>
+
+      <div class="clearfix">
+        <traction-button id="newRun" class="float-left" theme="create" @click="redirectToRun()"
+          >New Run</traction-button
+        >
+      </div>
+    </DataFetcher>
+  </flagged-feature>
 </template>
 
 <script>
--- tests/e2e/specs/ont/ont_runs_view.cy.js
+++ tests/e2e/specs/ont/ont_runs_view.cy.js
@@ -1,4 +1,15 @@
+import { beforeEach } from 'vitest'
+
 describe('ONT Runs view', () => {
+  beforeEach(() => {
+    cy.intercept('flipper/api/actors/User', {
+      flipper_id: 'User',
+      features: {
+        dpl_281_ont_create_sequencing_runs: { enabled: true },
+      },
+    })
+  })
+  
   it('Visits the ont runs url', () => {
     cy.intercept('/v1/ont/runs?include=instrument', {
       fixture: 'tractionOntRuns.json',
