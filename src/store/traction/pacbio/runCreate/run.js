81518e2632d990c5a0c02a71a089d3f23f941b6a
Stephen <519327+stevieing@users.noreply.github.com>
2023-02-21 15:36:39 +0000

    Added comments for building and validating a run.

--- src/store/traction/pacbio/runCreate/run.js
+++ src/store/traction/pacbio/runCreate/run.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 
+/*
+ * Returns the default attributes for a run
+ * id new inidcates a new record
+ */
 const runAttributes = {
   id: 'new',
   system_name: 'Sequel IIe',
@@ -12,8 +16,16 @@ const runAttributes = {
   },
 }
 
+/*
+ * @returns Array of required attributes for a run
+ */
 const requiredAttributes = () => ['sequencing_kit_box_barcode', 'dna_control_complex_box_barcode']
 
+/*
+ * @param Object - attributes. These will be for an existing run
+ * @returns Object - A Fresh Pacbio Sequencing Run.
+ * If id is nil it will be marked as a new run
+ */
 const newRun = (attributes) => {
   return {
     ...runAttributes,
@@ -21,6 +33,12 @@ const newRun = (attributes) => {
   }
 }
 
+/*
+ * @param Object - A Pacbio Sequencing Run
+ * @returns none - It modifies the original as it needs to be reactive
+ * checks if requiredAttributes are completed. If not adds an error
+ *
+ */
 const validate = ({ run }) => {
   const errors = {}
   requiredAttributes().forEach((field) => {
@@ -29,6 +47,12 @@ const validate = ({ run }) => {
   Vue.set(run, 'errors', errors)
 }
 
+/*
+ * @param Object - A Pacbio Sequencing Run
+ * @returns boolean
+ * A run is valid if it has no errors or errors are empty
+ * A run is not valid if it has any errors
+ */
 const valid = ({ run }) => {
   return Object.keys(run.errors || {}).length === 0
 }
