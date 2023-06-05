7b081565f65c2410ef7b6c12fae81b54c8daeaa6
Seena Nair <55585488+seenanair@users.noreply.github.com>
2023-06-05 15:42:42 +0100

    debounce value change

--- src/components/ont/runs/ONTFlowcell.vue
+++ src/components/ont/runs/ONTFlowcell.vue
@@ -36,7 +36,7 @@
             :formatter="formatter"
             :classes="flowcell_barcode_field_colour"
             placeholder="Scan library barcode"
-            :debounce="200"
+            :debounce="500"
           />
         </traction-field-error>
       </fieldset>
