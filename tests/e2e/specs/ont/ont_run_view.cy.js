02520a799ccef87772619a5e09ade3c44955668d
Ben Topping <ben.topping@outlook.com>
2023-06-08 14:10:21 +0100

    Prettier'd, removed cypress waits, rearranged barcode validation order for ont flowcell pool barcode

--- tests/e2e/specs/ont/ont_run_view.cy.js
+++ tests/e2e/specs/ont/ont_run_view.cy.js
@@ -11,7 +11,7 @@ describe('ONT Run page', () => {
       fixture: 'tractionOntRuns.json',
     })
     cy.intercept('/v1/ont/pools?include=tube,libraries.tag,libraries.request', {
-      fixture: 'tractionOntPools.json'
+      fixture: 'tractionOntPools.json',
     })
     cy.intercept('flipper/api/actors/User', {
       flipper_id: 'User',
@@ -46,8 +46,6 @@ describe('ONT Run page', () => {
 
     cy.get('#flowcell-id-1').type('ABC123')
     cy.get('#pool-id-1').type('TRAC-1-2')
-    // Wait 500ms to allow debounce function to be called and validate input
-    cy.wait(500)
 
     cy.get('#create').should('be.enabled')
     cy.get('#create').click()
@@ -74,8 +72,6 @@ describe('ONT Run page', () => {
     cy.contains('Enter at valid Flowcell ID (3 letters then at least 3 numbers)')
 
     cy.get('#pool-id-1').type('Unknown')
-    // Wait 500ms to allow debounce function to be called and validate input
-    cy.wait(500)
 
     cy.contains('Enter a valid Pool Library barcode')
   })
@@ -93,15 +89,9 @@ describe('ONT Run page', () => {
 
     cy.get('#flowcell-id-1').type('ABC123')
     cy.get('#pool-id-1').type('TRAC-1-2')
-    // Wait 500ms to allow debounce function to be called and validate input
-    cy.wait(500)
-
 
     cy.get('#flowcell-id-2').type('ABC123')
     cy.get('#pool-id-2').type('TRAC-1-2')
-    // Wait 500ms to allow debounce function to be called and validate input
-    cy.wait(500)
-
 
     cy.get('#create').should('be.enabled')
     cy.get('#create').click()
