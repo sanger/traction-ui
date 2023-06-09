346daeee1478197d0e1c1caa0d2403d9b69252f7
Stephen Hulme <sh51@sanger.ac.uk>
2023-06-09 16:01:58 +0100

    Move Plates traction-section to correct component

--- src/components/svg/Plate96SVG.vue
+++ src/components/svg/Plate96SVG.vue
@@ -1,40 +1,38 @@
 <template>
-  <traction-section title="Plates" class="plate">
-    <svg :width="width" :height="height" viewBox="0 0 350 255" preserveAspectRatio="xMidYMin">
-      <g id="layer1" transform="translate(-970.03125,-1039.9375)">
-        <path
-          id="path5203"
-          d="m 983.44263,1045.0229 323.20507,0 c 3.4232,0 6.1984,2.7801 6.1984,6.2095 l 0,210.2411 c 0,3.4294 -2.7752,6.2096 -6.1984,6.2096 l -323.20507,0 c -3.42332,0 -6.19846,-2.7802 -6.19846,-6.2096 0,0 0,0 0,0 l 0,-210.2411 c 0,-3.4294 2.77514,-6.2095 6.19846,-6.2095 0,0 0,0 0,0 z"
-        />
+  <svg :width="width" :height="height" viewBox="0 0 350 255" preserveAspectRatio="xMidYMin">
+    <g id="layer1" transform="translate(-970.03125,-1039.9375)">
+      <path
+        id="path5203"
+        d="m 983.44263,1045.0229 323.20507,0 c 3.4232,0 6.1984,2.7801 6.1984,6.2095 l 0,210.2411 c 0,3.4294 -2.7752,6.2096 -6.1984,6.2096 l -323.20507,0 c -3.42332,0 -6.19846,-2.7802 -6.19846,-6.2096 0,0 0,0 0,0 l 0,-210.2411 c 0,-3.4294 2.77514,-6.2095 6.19846,-6.2095 0,0 0,0 0,0 z"
+      />
 
-        <!-- Slot for a well component -->
-        <slot></slot>
+      <!-- Slot for a well component -->
+      <slot></slot>
 
-        <text id="rows" x="995.9538" y="1068.7362" transform="scale(0.98960714,1.010502)">
-          <tspan
-            v-for="(row, key) in plateMap.rows"
-            :id="'row' + key"
-            :key="key"
-            :x="row.x"
-            :y="row.y"
-            class="plate-heading"
-          >
-            {{ key }}
-          </tspan>
-        </text>
-        <text
-          v-for="(column, key) in plateMap.columns"
-          :id="'column' + key"
+      <text id="rows" x="995.9538" y="1068.7362" transform="scale(0.98960714,1.010502)">
+        <tspan
+          v-for="(row, key) in plateMap.rows"
+          :id="'row' + key"
           :key="key"
-          :x="column.x"
-          :y="column.y"
+          :x="row.x"
+          :y="row.y"
           class="plate-heading"
         >
-          <tspan>{{ key }}</tspan>
-        </text>
-      </g>
-    </svg>
-  </traction-section>
+          {{ key }}
+        </tspan>
+      </text>
+      <text
+        v-for="(column, key) in plateMap.columns"
+        :id="'column' + key"
+        :key="key"
+        :x="column.x"
+        :y="column.y"
+        class="plate-heading"
+      >
+        <tspan>{{ key }}</tspan>
+      </text>
+    </g>
+  </svg>
 </template>
 
 <script>
