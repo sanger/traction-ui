6d543621e62f110705f6c8ad49bb95bcb2a2e556
Harriet Craven <hc6@sanger.ac.uk>
2023-06-23 14:28:49 +0100

    Rename plate to plateNumber for clarity

--- src/components/pacbio/PacbioRunPlate.vue
+++ src/components/pacbio/PacbioRunPlate.vue
@@ -1,17 +1,17 @@
 <template>
   <traction-section title="Plates" class="min-w-[500px]">
     <div class="flex flex-row w-full">
-      <div v-for="plate in labware.plateCount" :key="plate" class="w-full">
+      <div v-for="plateNumber in labware.plateCount" :key="plateNumber" class="w-full">
         <div class="text-left mx-5 mb-5">
-          <traction-label classes="text-left">Plate number: {{ plate }}</traction-label>
+          <traction-label classes="text-left">Plate number: {{ plateNumber }}</traction-label>
           <traction-label classes="text-left">Sequencing Kit Box Barcode:</traction-label>
           <traction-input
             id="sequencing-kit-box-barcode"
-            v-model="runItem.plates[plate].sequencing_kit_box_barcode"
-            :value="runItem.plates[plate].sequencing_kit_box_barcode"
+            v-model="runItem.plates[plateNumber].sequencing_kit_box_barcode"
+            :value="runItem.plates[plateNumber].sequencing_kit_box_barcode"
             placeholder="Sequencing Kit Box Barcode"
             type="text"
-            :data-attribute="`sequencing_kit_box_barcode-${plate}`"
+            :data-attribute="`sequencing_kit_box_barcode-${plateNumber}`"
           />
         </div>
         <div :class="labware.plateClasses">
@@ -19,11 +19,11 @@
             v-slot="{ position }"
             :labware-type="labware.labwareType"
             :name="labware.labwareType.name"
-            :data-attribute="`pacbio-run-plate-${plate}`"
+            :data-attribute="`pacbio-run-plate-${plateNumber}`"
           >
             <PacbioRunWell
               :position="position"
-              :plate-number="plate"
+              :plate-number="plateNumber"
               :interactive="true"
               @click="onWellClick"
             />
