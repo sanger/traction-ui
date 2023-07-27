93bf9abe32c76620b19930afcc6673535db3324b
Ben Topping <ben.topping@outlook.com>
2023-07-27 10:46:05 +0100

    Removed font smoothing due to non-standard behaviour

--- src/App.vue
+++ src/App.vue
@@ -87,8 +87,6 @@ export default {
 <style lang="scss">
 #app {
   font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
