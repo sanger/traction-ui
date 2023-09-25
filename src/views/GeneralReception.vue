e108324004ce6cb63c0287ca0b15518bf5dd801d
Stephen <519327+stevieing@users.noreply.github.com>
2023-09-25 13:37:17 +0100

    Added version and feature flag for new reception request.

--- src/views/GeneralReception.vue
+++ src/views/GeneralReception.vue
@@ -189,6 +189,7 @@ import TractionHeading from '../components/TractionHeading.vue'
 import LibraryTypeSelect from '@/components/shared/LibraryTypeSelect'
 import DataTypeSelect from '@/components/shared/DataTypeSelect'
 import { defaultRequestOptions } from '@/lib/receptions'
+import useSWRV from 'swrv'
 
 // We don't expect the modal to display without a message. If we end up in this
 // state then something has gone horribly wrong.
@@ -207,10 +208,6 @@ export default {
     receptions: {
       type: Object,
       default: () => Receptions,
-    },
-    version: {
-      type: String,
-      default: 'v1',
     },
   },
   data: () => ({
@@ -237,6 +234,12 @@ export default {
     barcodeCount: ({ barcodeArray }) => barcodeArray.length,
     presentRequestOptions: ({ requestOptions }) =>
       Object.fromEntries(Object.entries(requestOptions).filter(([, v]) => v)),
+    version: () => {
+      // we only care if the feature is enabled or not, so we can just check for the presence of the feature
+      // if the feature flag can't be returned for any reason, we'll default to v1
+      const { data } = useSWRV(`${import.meta.env.VITE_TRACTION_BASE_URL}/flipper/api/actors/User`)
+      return data?.value?.features['dpl_877_reception_request']?.enabled ? 'v2' : 'v1'
+    },
   },
   methods: {
     importStarted({ message }) {
