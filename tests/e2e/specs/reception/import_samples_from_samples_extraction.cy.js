c22d4538c215e592b8617a01508b99e08a8761a0
Stephen <519327+stevieing@users.noreply.github.com>
2023-09-26 14:29:48 +0100

    Added v2 test for samples extraction.

--- tests/e2e/specs/reception/import_samples_from_samples_extraction.cy.js
+++ tests/e2e/specs/reception/import_samples_from_samples_extraction.cy.js
@@ -10,7 +10,28 @@ describe('Import samples from Samples extraction, for Pacbio', () => {
     })
   })
 
-  it('Successfully', () => {
+  it('Successfully - v1', () => {
+    cy.visit('#/reception')
+    cy.get('[data-type="source-list"]').select('Samples Extraction')
+    cy.contains('Scan barcodes')
+    cy.get('#barcodes').type('SE108532I')
+    cy.intercept('/api/v1/assets?filter[barcode]=SE108532I', {
+      fixture: 'sampleExtractionTubesWithSample.json',
+    })
+    cy.intercept('POST', '/v1/receptions', { fixture: 'tractionPacbioRequest.json' }).as(
+      'postPayload',
+    )
+    cy.contains('Import 1 labware into PacBio from Samples Extraction')
+    cy.get('[data-action="import-labware"]').click()
+    cy.contains('Imported 1 labware(s) from Samples Extraction')
+  })
+
+  it('Successfully - v2', () => {
+
+    cy.withFlags({
+      dpl_877_reception_request: { enabled: true },
+    })
+    
     cy.visit('#/reception')
     cy.get('[data-type="source-list"]').select('Samples Extraction')
     cy.contains('Scan barcodes')
