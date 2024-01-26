83108583c91e9011ab72dbc2b62f5d6b703f4212
Seena Nair <55585488+seenanair@users.noreply.github.com>
2024-01-26 13:55:07 +0000

    Set libraries changes

--- src/stores/pacbioLibraries.js
+++ src/stores/pacbioLibraries.js
@@ -7,6 +7,7 @@ import { groupIncludedByResource, dataToObjectById } from '@/api/JsonApi.js'
  * 'TODO_LIBRARY_CHANGE': This store needs to be updated to use the new library creation
  * All the changes, that might be required, are marked with the above comment
  */
+
 export const usePacbioLibrariesStore = defineStore('pacbioLibraries', {
   state: () => ({
     tagSetChoices: [],
@@ -15,16 +16,6 @@ export const usePacbioLibrariesStore = defineStore('pacbioLibraries', {
   }),
 
   getters: {
-    /**Return all libraries */
-    libraryRequest: () => {
-      const rootStore = useRootStore()
-      return rootStore.api.traction.pacbio.libraries
-    },
-    // Return the library requests
-    requestLibraryRequest: () => {
-      const rootStore = useRootStore()
-      return rootStore.api.traction.pacbio.request_library
-    },
     // Returns a list of tag options by tagSetId
     tagChoicesForId: (state) => (tagSetId) => {
       return state.tagChoices[tagSetId] || []
@@ -34,23 +25,29 @@ export const usePacbioLibrariesStore = defineStore('pacbioLibraries', {
     /**Create a new libary */
     async createLibraryInTraction(library, tagId) {
       const rootState = useRootStore()
-      //TODO_LIBRARY_CHANGE: The body structure needs review- updated the attriutes to match the new library structure
+
       const body = {
         data: {
-          type: 'library',
+          type: 'libraries',
           attributes: {
             pacbio_request_id: library.sample.id,
             template_prep_kit_box_barcode: library.template_prep_kit_box_barcode,
             tag_id: tagId,
-            volume: library.volume,
             concentration: library.concentration,
+            volume: library.volume,
             insert_size: library.insert_size,
+            primary_aliquot_attributes: {
+              template_prep_kit_box_barcode: library.template_prep_kit_box_barcode,
+              volume: library.volume,
+              concentration: library.concentration,
+              insert_size: library.insert_size,
+            },
           },
         },
       }
       //TODO_LIBRARY_CHANGE:Needs to be changed to library
-      const request = rootState.api.traction.pacbio.pools
-      const promise = request.create({ data: body, include: 'tube' })
+      const request = rootState.api.traction.pacbio.libraries
+      const promise = request.create({ data: body, include: 'tube,primary_aliquot' })
       const { success, data: { included = [] } = {}, errors } = await handleResponse(promise)
       const { tubes: [tube = {}] = [] } = groupIncludedByResource(included)
       const { attributes: { barcode = '' } = {} } = tube
@@ -59,7 +56,8 @@ export const usePacbioLibrariesStore = defineStore('pacbioLibraries', {
 
     /**Delete all libraries with given library ids */
     async deleteLibraries(libraryIds) {
-      const promises = this.libraryRequest.destroy(libraryIds)
+      const rootStore = useRootStore()
+      const promises = rootStore.api.traction.pacbio.libraries.destroy(libraryIds)
       //TODO_LIBRARY_CHANGE: Update all components (which calls deleteLibraries) to use the new response format
       const responses = await Promise.all(promises.map((promise) => handleResponse(promise)))
       return responses
@@ -67,17 +65,17 @@ export const usePacbioLibrariesStore = defineStore('pacbioLibraries', {
 
     /**Set libraries in store with those fetched using given filter and page  */
     async setLibraries(filter, page) {
-      const request = this.libraryRequest
-      const promise = request.get({
+      const rootStore = useRootStore()
+      const promise = rootStore.api.traction.pacbio.libraries.get({
         page,
         filter,
         //TODO_LIBRARY_CHANGE: Instead of pool.tube we should be using tube relation in library
-        include: 'request,tag,pool.tube',
+        include: 'request,tag,tube',
       })
       const response = await handleResponse(promise)
 
       const { success, data: { data, included = [], meta = {} } = {}, errors = [] } = response
-      const { tubes, tags, requests, pools } = groupIncludedByResource(included)
+      const { tubes, tags, requests } = groupIncludedByResource(included)
 
       if (success) {
         /* 
@@ -85,22 +83,17 @@ export const usePacbioLibrariesStore = defineStore('pacbioLibraries', {
       for the pacbio libraries page
     */
         const libraries = data.map((library) => {
-          /** */
-          //TODO_LIBRARY_CHANGE: Keep this? The pool might or might not be present (to support legacy data - pool with libraries)
-          const pool = pools?.find((pool) => pool.id == library.relationships.pool.data?.id)
           return {
+            /** */
             id: library.id,
             ...library.attributes,
-            pool,
             tag_group_id: tags?.find((tag) => tag.id == library.relationships.tag.data?.id)
               ?.attributes.group_id,
             sample_name: requests?.find(
               (request) => request.id == library.relationships.request.data?.id,
             )?.attributes.sample_name,
-            //TODO_LIBRARY_CHANGE: Need to change to library.relationships.tube.data?.id
-            barcode: tubes?.find((tube) => tube.id == pool.relationships.tube.data?.id)?.attributes
-              .barcode,
-            //TODO_LIBRARY_CHANGE: Need to retrieve primary aliquot associated with library?
+            barcode: tubes?.find((tube) => tube.id == libraries.relationships.tube.data?.id)
+              ?.attributes.barcode,
           }
         })
         this.libraries = libraries
@@ -111,8 +104,7 @@ export const usePacbioLibrariesStore = defineStore('pacbioLibraries', {
     /**Fetch all tag sets */
     async fetchPacbioTagSets() {
       const rootStore = useRootStore()
-      const request = rootStore.api.traction.pacbio.tag_sets
-      const promise = request.get({ include: 'tags' })
+      const promise = rootStore.api.traction.pacbio.tag_sets.get({ include: 'tags' })
       const response = await handleResponse(promise)
       const { success, data: { data, included = [] } = {}, errors = [] } = response
       if (success) {
