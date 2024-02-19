0a0447affad2a482bfd1636a6f01bb187ca59963
Seena Nair <55585488+seenanair@users.noreply.github.com>
2024-02-19 17:22:57 +0000

    Changed Pacbio library form layout

--- src/components/pacbio/PacbioLibraryForm.vue
+++ src/components/pacbio/PacbioLibraryForm.vue
@@ -1,27 +1,32 @@
 <template>
   <div>
-    <traction-form id="librarForm" @keydown.enter.prevent>
-      <fieldset id="tag-set-select-input" class="py-2">
-        <label>Tag:</label>
+    <traction-form
+      id="libraryForm"
+      class="flex flex-row justify-between space-x-2 p-2 bg-gray-200"
+      @keydown.enter.prevent
+    >
+      <fieldset id="tag-set-select-input">
+        <traction-label class="ml-1">Tag set</traction-label>
         <traction-select
           id="tag-set-input"
           v-model="selectedTagSetId"
           data-type="tag-set-list"
           :options="tagSetOptions"
-          class="mb-3"
           @update:modelValue="resetSelectedTagId"
         ></traction-select>
+      </fieldset>
 
+      <fieldset id="tag-select-input">
+        <traction-label class="ml-1">Tag</traction-label>
         <traction-select
           id="tag-input"
           v-model="formLibrary.tag_id"
           :options="tagOptions"
           :disabled="!selectedTagSetId"
-          class="mb-3"
         />
       </fieldset>
-      <fieldset id="input-group-volume" class="py-2">
-        <label>Volume:</label>
+      <fieldset id="input-group-volume">
+        <traction-label class="ml-1">Volume</traction-label>
         <traction-input
           id="library-volume"
           v-model="formLibrary.volume"
@@ -33,8 +38,8 @@
         </traction-input>
       </fieldset>
 
-      <fieldset id="input-group-concentration" class="py-2">
-        <label>Concentration:</label>
+      <fieldset id="input-group-concentration">
+        <traction-label class="ml-1">Concentration</traction-label>
 
         <traction-input
           id="library-concentration"
@@ -47,8 +52,10 @@
         </traction-input>
       </fieldset>
 
-      <fieldset id="input-group-templatePrepKitBoxBarcode" class="py-2">
-        <label>Template prep kit box barcode:</label>
+      <fieldset id="input-group-templatePrepKitBoxBarcode">
+        <traction-label class="ml-1 whitespace-nowrap"
+          >Template prep kit box barcode</traction-label
+        >
         <traction-input
           id="library-templatePrepKitBoxBarcode"
           v-model="formLibrary.template_prep_kit_box_barcode"
@@ -62,8 +69,8 @@
         </traction-input>
       </fieldset>
 
-      <fieldset id="input-group-insertSize" class="py-2">
-        <label>Insert size:</label>
+      <fieldset id="input-group-insertSize">
+        <traction-label class="ml-1">Insert size</traction-label>
         <traction-input
           id="library-insertSize"
           v-model="formLibrary.insert_size"
@@ -79,6 +86,31 @@
 </template>
 
 <script setup>
+/**
+ * Following are new Vue 3 features used in this component:
+ * 
+ * script setup : is a Vue 3 function that allows you to define props, reactive variables, and computed properties in the setup function.
+ *
+ * ref:  is a Vue 3 function that allows you to create a reactive object which is a replacement for the data option in Vue 2
+ * ref() takes the argument and returns it wrapped within a ref object with a .value property:
+ * e.g : ref(0) returns { value: 0 }
+ * To access the value, you use the .value property in setup function, but in the template, you can use the variable directly.
+ * {@link} https://v3.vuejs.org/guide/reactivity-fundamentals.html#ref
+ 
+ * Composables: are a new Vue 3 feature that allows you to create reusable logic.
+ * {@link} https://vuejs.org/guide/reusability/composables
+ *
+ * computed: is a Vue 3 function that allows you to create a computed property.
+ * It is a replacement for the computed option in Vue 2.
+ * {@link} https://v3.vuejs.org/guide/reactivity-computed-watchers.html#computed-properties
+ *
+ * defineProps: is a Vue 3 function that allows you to define props in the setup function which is a replacement for the  props option in Vue 2.
+ * {@link} https://v3.vuejs.org/guide/component-props.html#prop-validation
+ *
+* defineExpose: is a Vue 3 function that allows you to expose properties to parent components.
+ * {@link} https://vuejs.org/api/sfc-script-setup.html#defineexpose
+ *  */
+
 /***
  * PacbioLibraryForm component can be used to create or edit a library.
  * @param {Object} library - The library to be  edited or created
@@ -87,34 +119,26 @@ import { computed, ref } from 'vue'
 import { usePacbioLibrariesStore } from '@/stores/pacbioLibraries'
 import useAlert from '@/composables/useAlert.js'
 
+// useAlert is a composable function that is used to create an alert.It is used to show a success or failure message.
+const { showAlert } = useAlert() 
+
+/**
+ * usePacbioLibrariesStore is a composable function that is used to access the 'pacbioLibraries' store.
+ * It is used to create a new library.
+ */
+const librariesStore = usePacbioLibrariesStore()
+
 // Define props
 const props = defineProps({
-  // The library to be edited or created
   library: {
+    // The library to be edited or created
     type: Object,
     required: true,
     default() {
-      return { tag: '', sample: {} }
+      return { tag_id: '', sample: {} }
     },
   },
 })
-
-//Create Pinia store
-const librariesStore = usePacbioLibrariesStore()
-
-/**
- * Get the tagset for the given library tag, if any
- */
-const getTagset = () => {
-  const tagSet = props.library.tag ? librariesStore.tagsetForTagId(props.library.tag) : ''
-  return tagSet ? tagSet.id : ''
-}
-
-/*
-selectedTagSetId is a reactive variable, so it will update when the library prop changes
-Initialize  selectedTagSetId with the tagset id of the library prop
-*/
-const selectedTagSetId = ref(getTagset())
 
 /*
 formLibrary is a reactive variable, so it will update when the library prop changes
@@ -129,22 +153,37 @@ so that the parent component can access the formLibrary and its properties
 defineExpose({
   formLibrary,
 })
+
 /*
-tagSetOptions is a computed variable, so it will update when the librariesStore.tagSetChoices changes
+selectedTagSetId is a reactive variable, so it will be initialised with an empty string 
+This will be updated When the component is created with a library having tag_id or when the user selects a tag set
 */
+const selectedTagSetId = ref('')
+
+
+/**
+ * @name tagSetOptions
+ * @type {<Array<Object>>}
+ * @description A computed property that returns an array of tag set options for a select input.
+ * The first option is a placeholder with a value of an empty string and a text of 'Please select a tag set'.
+ * The remaining options are the tag set choices from the 'librariesStore'.
+ */
 const tagSetOptions = computed(() => {
   const placeholder = { value: '', text: 'Please select a tag set' }
   return [placeholder, ...librariesStore.tagSetChoices]
 })
-/*
-tagOptions is a computed variable, so it will update when the librariesStore.tagChoicesForId(selectedTagSetId.value) changes
-*/
+
+/**
+ * @name tagOptions
+ * @type {<Array<Object>>}
+ * @description A computed property that returns an array of tag options for a select input.
+ * The first option is a placeholder with a value of an empty string and a text of 'Please select a tag'.
+ * The remaining options are the tag choices from the 'librariesStore' for the currently selected tag set ID.
+ */
 const tagOptions = computed(() => {
   const placeholder = { value: '', text: 'Please select a tag' }
   return [placeholder, ...librariesStore.tagChoicesForId(selectedTagSetId.value)]
 })
-const { showAlert } = useAlert()
-
 
 /**
  * Show a failure message
@@ -156,24 +195,32 @@ const showFailureMessage = (action, errors) => {
 }
 
 /**
- * Define provider method to fetch tagsets, which is called when the component is created
+ * @method provider
+ * Fetches the pacbio tag sets from the 'pacbioLibraries' store.
+ * If the fetch is successful, it sets the selected tag set ID to the tag set ID of the library.
+ * If the fetch is not successful, it shows shows a failure message.
  */
 const provider = async () => {
   try {
     const { success, errors } = await librariesStore.fetchPacbioTagSets()
-    if (!success) {
+    if (success) {
+      // If the library has a tag_id, set the selected tag set ID to the tag set ID of the library
+      const tagSet = props.library.tag_id ? librariesStore.tagsetForTagId(props.library.tag_id) : ''
+      selectedTagSetId.value = tagSet ? tagSet.id : ''
+    } else {
       showFailureMessage('find tags', errors)
     }
   } catch (error) {
     showFailureMessage('find tags', [error.message])
   }
 }
+
+provider()
 /**
- * Reset the selected tag id
+ * @method resetSelectedTagId
+ * Resets the selected tag ID to an empty string.
  */
 const resetSelectedTagId = () => {
   formLibrary.value.tag_id = ''
 }
-
-provider()
 </script>
