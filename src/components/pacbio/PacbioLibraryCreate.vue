7b5edbf2465cdaf006ee0c872196db2a5f159e2a
Seena Nair <55585488+seenanair@users.noreply.github.com>
2024-02-19 17:21:58 +0000

    Replaced modal with an in-place dialog for  library creation

--- src/components/pacbio/PacbioLibraryCreate.vue
+++ src/components/pacbio/PacbioLibraryCreate.vue
@@ -1,110 +1,138 @@
 <template>
-  <div>
-    <traction-button id="pacbioLibraryCreate" :disabled="disabled" theme="create" @click="show">
-      Create Library
-    </traction-button>
-    <traction-modal
-      id="pacbioLibraryModal"
-      ref="modalRef"
-      size="lg"
-      title="Create Library"
-      :static="isStatic"
-      :visible="showModal"
-      scrollable
-      @cancel="hideModal"
+  <div :class="`${displayCreatePanel && 'mt-6'}`">
+    <traction-button
+      v-if="!displayCreatePanel"
+      id="pacbioLibraryCreate"
+      :disabled="!selectedSample.sample_name"
+      theme="create"
+      @click="toggleDisplayCreatePanel"
     >
-      <div>
+      {{ 'Create Library' }}
+    </traction-button>
+    <div
+      v-if="displayCreatePanel"
+      class="p-4 ml-4 mb-4 mt-2 rounded-md text-left items-center border-2 border-gray-200"
+    >
+      <traction-heading level="3" show-border shadow-md class="mb-2">
+        Create Library
+      </traction-heading>
+      <div class="mb-2">
         <label>The sample selected for this library is:</label>
         <br />
         {{ selectedSample.sample_name }} ({{ selectedSample.source_identifier }})
       </div>
       <PacbioLibraryForm ref="formRef" :library="library" />
-      <template #modal-footer="{ cancel }">
-        <traction-button @click="cancel()"> Cancel </traction-button>
-
+      <div class="flex flex-row items-center justify-end space-x-2 mt-2">
+        <traction-button @click="toggleDisplayCreatePanel"> Cancel </traction-button>
         <traction-button id="create-btn" theme="create" @click="createLibrary">
           Create
         </traction-button>
-      </template>
-    </traction-modal>
+      </div>
+    </div>
   </div>
 </template>
 
+
 <script setup>
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 import { usePacbioLibrariesStore } from '@/stores/pacbioLibraries'
 import useAlert from '@/composables/useAlert.js'
-import useModalHelper from '@/composables/useModalHelper.js'
 import PacbioLibraryForm from '@/components/pacbio/PacbioLibraryForm.vue'
-// Define props
+
+/**
+ * Following are new Vue 3 features used in this component:
+ * 
+ * script setup : is a Vue 3 function that allows you to define props, reactive variables, and computed properties in the setup function.
+ *
+ * ref:  is a Vue 3 function that allows you to create a reactive object which is a replacement for the data option in Vue 2
+ * ref() takes the argument and returns it wrapped within a ref object with a .value property:
+ * e.g : ref(0) returns { value: 0 }
+ * To access the value, you use the .value property in setup function, but in the template, you can use the variable directly.
+ * {@link} https://v3.vuejs.org/guide/reactivity-fundamentals.html#ref
+ 
+ * Composables: are a new Vue 3 feature that allows you to create reusable logic.
+ * {@link} https://vuejs.org/guide/reusability/composables
+ *
+ * computed: is a Vue 3 function that allows you to create a computed property.
+ * It is a replacement for the computed option in Vue 2.
+ * {@link} https://v3.vuejs.org/guide/reactivity-computed-watchers.html#computed-properties
+ *
+ * defineProps: is a Vue 3 function that allows you to define props in the setup function which is a replacement for the  props option in Vue 2.
+ * {@link} https://v3.vuejs.org/guide/component-props.html#prop-validation
+ 
+ */
+
+/**
+ * PacbioLibraryCreate component is used to create a new library for a selected sample.
+ */
+
+ //define props
 const props = defineProps({
-  disabled: Boolean,
-  isStatic: Boolean,
-  selectedSample: {
+  selectedSample: { //The selected sample for which the library is being created.
     type: Object,
     default() {
       return {}
     },
+    required: true,
   },
 })
 
-// Define refs
-const library = ref({ tag: '', sample: {} })
-const selectedTagSetId = ref('')
-const showModal = ref(false)
-const modalRef = ref(null)
-const formRef = ref(null)
-//Composables
-const { showAlert } = useAlert()
-const { hide } = useModalHelper(modalRef.value)
+const formRef = ref(null) // Create a ref for the PacbioLibraryForm component
 
-// Define emits
-const emit = defineEmits(['alert'])
-//Emits the 'alert' event
-const emitAlert = (message) => emit('alert', message)
+/**
+ * Create a ref for the displayCreatePanel variable which is used to toggle the display of the create panel.
+ * If displayCreatePanel is true, the create panel is displayed, otherwise displays a 'Create' button.
+ */
+const displayCreatePanel = ref(false)
 
-//Create Pinia store
+const { showAlert } = useAlert() // useAlert is a composable function that is used to create an alert.It is used to show a success or failure message.
+
+
+/**
+ * usePacbioLibrariesStore is a composable function that is used to access the 'pacbioLibraries' store.
+ * It is used to create a new library.
+ */
 const librariesStore = usePacbioLibrariesStore()
 
-// Define methods
+/**
+ * library is a computed property that returns the selected sample for which the library is being created.
+ * It is used to create a new library and which passed to the PacbioLibraryForm component.
+ */
+const library = computed(() => {
+  return { tag: '', sample: { ...props.selectedSample } }
+})
 
-// Show a failure message
+/**
+ * @method createLibrary
+ * @description Creates a new library by calling the createLibraryInTraction method from the 'pacbioLibraries' store.
+ * @returns {void} Displays a success message if the library is created successfully, otherwise displays a failure message.
+ */
 const showFailureMessage = (action, errors) => {
   showAlert(`Failed to ${action} in Traction: ${errors.length > 0 ? errors[0] : ''}`, 'danger')
 }
 
-// Define provider method
-const provider = async () => {
-  try {
-    const { success, errors } = await librariesStore.fetchPacbioTagSets()
-    if (!success) {
-      showFailureMessage('find tags', errors)
-    }
-  } catch (error) {
-    showFailureMessage('find tags', [error.message])
-  }
+/**
+ * @method toggleDisplayCreatePanel
+ * @description Toggles the display of the create panel.
+ * If displayCreatePanel is true, the create panel is displayed, otherwise displays a 'Create' button.
+ */
+const toggleDisplayCreatePanel = () => {
+  displayCreatePanel.value = !displayCreatePanel.value
 }
 
+/**
+ * @method createLibrary
+ * @description Creates a new library by calling the createLibraryInTraction method from the 'pacbioLibraries' store.
+ * @returns {void} Displays a success message if the library is created successfully, otherwise displays a failure message.
+ */
 const createLibrary = async () => {
   const { success, barcode, errors } = await librariesStore.createLibraryInTraction(
-    formRef.value.formLibrary,
+    formRef?.value?.formLibrary,
   )
   if (success) {
-    hideModal()
-    emitAlert('Created library with barcode ' + barcode, 'success')
+    showAlert('Created library with barcode ' + barcode, 'success')
   } else {
     showFailureMessage('create library', errors)
   }
 }
-
-const show = () => {
-  library.value = { tag: "", sample: props.selectedSample }
-  showModal.value = true
-  selectedTagSetId.value = ''
-}
-const hideModal = () => {
-  hide()
-  showModal.value = false
-}
-provider()
 </script>
