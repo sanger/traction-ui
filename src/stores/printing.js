34a49dd6fea60a4f17d19d4c4896edf00686f3c8
Stephen <519327+stevieing@users.noreply.github.com>
2024-05-03 09:11:38 +0100

    Add createPrintJob action to printing Pinia store

--- src/stores/printing.js
+++ src/stores/printing.js
@@ -1,7 +1,51 @@
 import { defineStore } from 'pinia'
+import { handleResponse } from '@/api/v1/ResponseHelper'
+import store from '@/store'
 
 export const usePrintingStore = defineStore('printing', {
   state: () => ({
     printers: [],
+    tubeLabelTemplateName: import.meta.env.VITE_TUBE_LABEL_TEMPLATE_NAME,
   }),
+  actions: {
+    /**
+     * Creates a print job in PrintMyBarcode
+     * @param rootState the vuex rootState object. Provides access to current state
+     * @param state Provides access to local state
+     * @param {String} printerName The name of the printer to send the print job to
+     * @param {Array} labels Should be of a standard structure. See relevant components.
+     * @param {Integer} copies Number of copies of labels
+     * @returns {Object {success: Boolean, message: String}} Is the print job successful? With a success or failure message
+     **/
+    async createPrintJob({ printerName, labels, copies }) {
+      // extract the request from the rootState
+      const request = store.state.api.printMyBarcode.print_jobs
+
+      const payload = {
+        printer_name: printerName,
+        label_template_name: this.tubeLabelTemplateName,
+        labels,
+        copies,
+      }
+
+      const promise = request.create({ data: payload })
+
+      const response = await handleResponse(promise)
+
+      const { success, data, errors } = response
+
+      // we need to create a final message
+      const message = success
+        ? // if it was success just send a simple message
+          'Barcode(s) successfully printed'
+        : // print my barcode does not return the errors in the correct format
+          // so we need to extract the errors and turn them into something meaningful
+          data?.errors?.length > 0
+          ? data.errors.map((e) => e.source.pointer + ' ' + e.detail).join(', ')
+          : // otherwise we can just return the errors
+            errors
+
+      return { success, message }
+    },
+  },
 })
--- tests/unit/stores/printing.spec.js
+++ tests/unit/stores/printing.spec.js
@@ -12,4 +12,75 @@ describe('usePrintingStore', () => {
     const store = usePrintingStore()
     expect(store.printers).toEqual([])
   })
+
+  describe.skip('#createPrintJob', () => {
+    const printJobOptions = {
+      printerName: 'my_printer',
+      labels: [
+        {
+          barcode: 'TRAC-1',
+          first_line: 'line 1',
+          second_line: 'line 2',
+          third_line: 'line 3',
+          fourth_line: 'line 4',
+          label_name: 'main_label',
+        },
+      ],
+      copies: 1,
+    }
+
+    it('successful', async () => {
+      const store = usePrintingStore()
+      const create = vi.fn()
+      //const rootState = { api: { printMyBarcode: { print_jobs: { create } } } }
+      const tubeLabelTemplateName = 'tube_label_template'
+      const mockResponse = {
+        status: '201',
+      }
+
+      create.mockResolvedValue(mockResponse)
+
+      const { success, message } = await store.createPrintJob({ ...printJobOptions })
+
+      const { printerName: printer_name, ...payload } = printJobOptions
+
+      expect(create).toBeCalledWith({
+        data: { ...payload, printer_name, label_template_name: tubeLabelTemplateName },
+      })
+      expect(success).toBeTruthy()
+      expect(message).toEqual('Barcode(s) successfully printed')
+    })
+
+    it('unsuccessful', async () => {
+      const store = usePrintingStore()
+      const create = vi.fn()
+      // const rootState = { api: { printMyBarcode: { print_jobs: { create } } } }
+      const mockResponse = {
+        status: '422',
+        response: {
+          data: {
+            errors: [
+              {
+                source: {
+                  pointer: '/data/attributes/printer',
+                },
+                detail: "can't be blank",
+              },
+            ],
+          },
+        },
+      }
+
+      create.mockRejectedValue(mockResponse)
+
+      // eslint-disable-next-line no-unused-vars
+      const { _, ...rest } = printJobOptions
+
+      const { success, message } = await store.createPrintJob({ ...rest })
+
+      expect(success).toBeFalsy()
+      // looks like there is an issue with the errors again. How many times do we have to fix this.
+      expect(message).toEqual("/data/attributes/printer can't be blank")
+    })
+  })
 })
