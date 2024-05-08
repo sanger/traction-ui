ca729b36f69f95c7a7171fdc4e9603d76f90a405
Shiv <44001656+SHIV5T3R@users.noreply.github.com>
2024-05-08 15:44:16 +0100

    Prettier

--- src/views/pacbio/PacbioLibraryIndex.vue
+++ src/views/pacbio/PacbioLibraryIndex.vue
@@ -28,10 +28,7 @@
             <p class="mb-4">Are you sure you want to delete the selected libraries?</p>
             <div class="flex justify-center space-x-4">
               <button
-                @click="
-                  handleLibraryDelete(),
-                  showConfirmationModal = false
-                "
+                @click="handleLibraryDelete(), (showConfirmationModal = false)"
                 class="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600"
               >
                 Yes
