082232e28d3e42e6ef7cac0314b7eaa4ca000542
Shiv <44001656+SHIV5T3R@users.noreply.github.com>
2024-05-09 09:10:28 +0100

    Linting

--- src/views/pacbio/PacbioLibraryIndex.vue
+++ src/views/pacbio/PacbioLibraryIndex.vue
@@ -28,14 +28,14 @@
             <p class="mb-4">Are you sure you want to delete the selected libraries?</p>
             <div class="flex justify-center space-x-4">
               <button
-                @click="handleLibraryDelete(), (showConfirmationModal = false)"
                 class="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600"
+                @click="handleLibraryDelete(), (showConfirmationModal = false)"
               >
                 Yes
               </button>
               <button
-                @click="showConfirmationModal = false"
                 class="px-4 py-2 bg-gray-200 text-gray-700 rounded hover:bg-gray-300"
+                @click="showConfirmationModal = false"
               >
                 No
               </button>
