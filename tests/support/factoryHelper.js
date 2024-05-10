9d563047df90cfc6e22069e106d7d4caddfdc371
Seena Nair <55585488+seenanair@users.noreply.github.com>
2024-05-10 11:02:58 +0100

    Added documentation

--- tests/support/factoryHelper.js
+++ tests/support/factoryHelper.js
@@ -1,8 +1,18 @@
 import fs from 'fs'
 import { join } from 'path'
 
+//This is the path to the data folder where all the test json files are stored
 const dataPath = '../data'
 
+/**
+ * This function will load a file and return a factory object
+ * @param {*} name file name
+ * @param {*} axios boolean flag to indicate if axios response should be returned
+ * @returns
+ * example usage:
+ * const data = RequestFactory('Printers')
+ * This returns a factory object with the data from the Printers.json file
+ */
 const RequestFactory = (name, axios = true) => {
   const data = loadFile(name)
   if (axios) {
@@ -19,11 +29,17 @@ const RequestFactory = (name, axios = true) => {
         json: () => Promise.resolve(data),
         ok: true,
       },
+      //This is the data within the file that was loaded which is parsed to json
       content: data,
     }
   }
 }
 
+/**
+ * This function will load a file and return the content
+ * @param {*} fileName  The name of the file to load
+ * @returns The content of the file
+ */
 const loadFile = (fileName) => {
   const filePath = join(__dirname, dataPath, `${fileName}.json`)
   try {
