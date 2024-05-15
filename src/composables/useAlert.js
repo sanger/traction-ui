fb6302be805dc5dc9a9ce8707af336613a30f5e2
Shiv <44001656+SHIV5T3R@users.noreply.github.com>
2024-05-15 14:57:52 +0100

    Added page origin + timestamp to message store

--- src/composables/useAlert.js
+++ src/composables/useAlert.js
@@ -1,4 +1,6 @@
 import store from '@/store'
+import { useRoute } from 'vue-router'
+
 /**
  * A composable function to commit a mutation to the Vuex store.
  * @returns {Function} .showAlert A method that shows an alert. It commits a mutation to the Vuex store.
@@ -7,10 +9,16 @@ import store from '@/store'
  * @param {string} dataType - The data type of the alert.
  */
 export default function useAlert() {
+  const route = useRoute()
+
   const showAlert = (message, type, dataType) => {
-    //TODO: This need to be refactored to use the Pinia root store once we have converted all components to use useAlert
-    store.commit('traction/addMessage', { type, message, dataType })
+    const page = route.name
+    const time = new Date().toLocaleString()
+
+    // Commit the mutation to the Vuex store
+    store.commit('traction/addMessage', { type, message, dataType, page, time })
   }
+
   // Return the showAlert method
   return {
     showAlert,
