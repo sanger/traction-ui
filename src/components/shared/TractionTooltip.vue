d32987beea2e547f5e427d02573e112c9779c6f6
Seena Nair <55585488+seenanair@users.noreply.github.com>
2024-05-20 13:17:26 +0100

    Revert "Revert "Merge branch 'develop'""
    This reverts commit 2d8a533278781e8c6d371f364b1f2394922e6cf9.

---
+++ src/components/shared/TractionTooltip.vue
@@ -0,0 +1,33 @@
+<template>
+  <div
+    id="tooltip-div"
+    class="justify-end contents-end px-1 relative"
+    @mouseover="hover = true"
+    @mouseleave="hover = false"
+  >
+    <div
+      v-show="hover"
+      id="tooltip-text"
+      class="text-sm px-1 bg-yellow-400 text-gray-700 absolute rounded bg-opacity-75 shadow-xl left-0 top-[-25px]"
+    >
+      {{ tooltipText }}
+    </div>
+    <slot />
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  tooltipText: {
+    type: String,
+    required: true,
+  },
+})
+
+/* A reactive reference to a boolean value indicating whether the element is being hovered over.
+  This will be used to show the used volume tooltip when the user hovers over the used volume badge.
+ */
+const hover = ref(false)
+</script>
--- src/views/pacbio/PacbioPoolCreate.vue
+++ src/views/pacbio/PacbioPoolCreate.vue
@@ -38,10 +38,14 @@
           <PacbioTagSetItem />
         </div>
         <div>
-          <PacbioLabwareSelectedList :labware="scannedLabware" @closed="onClosed" />
+          <PacbioLabwareSelectedList
+            :labware="scannedLabware"
+            :highlight="aliquotSelectionHighlightLabware"
+            @closed="onClosed"
+          />
         </div>
         <div>
-          <PacbioPoolEdit />
+          <PacbioPoolEdit @aliquot-selected="handleAliquotSelection" />
         </div>
       </div>
     </div>
@@ -88,6 +92,8 @@ const searchText = ref('')
  * The search input ref
  */
 const searchRef = ref(null)
+
+const aliquotSelectionHighlightLabware = ref(null)
 
 /**
  * Fetches the pools data
@@ -152,5 +158,23 @@ const search = async (value) => {
   // Add the labware to the scanned labware array
   scannedLabware.value.push({ barcode: value, type: findPlate.success ? 'plates' : 'tubes' })
   searchText.value = ''
+}
+
+const handleAliquotSelection = (request) => {
+  if (!request) {
+    aliquotSelectionHighlightLabware.value = null
+    return
+  }
+  let labware = pacbioPoolCreateStore.selectedTubes.find((tube) =>
+    tube.requests.some((req) => req === request.id),
+  )
+  if (!labware) {
+    labware = pacbioPoolCreateStore.selectedPlates.find((plate) =>
+      pacbioPoolCreateStore
+        .wellList(plate.wells || [])
+        .some((well) => well.requests.includes(request.id)),
+    )
+  }
+  aliquotSelectionHighlightLabware.value = { labware, request }
 }
 </script>
