df6e23c553a804357713db99dc7e81eb780c4a21
Stephen <519327+stevieing@users.noreply.github.com>
2024-06-02 17:22:10 +0100

    Create PrintJobTYpe. Move label creation to computed.

--- src/components/labelPrinting/LabelPrintingForm.vue
+++ src/components/labelPrinting/LabelPrintingForm.vue
@@ -32,7 +32,7 @@
               <traction-select
                 id="suffix-selection"
                 v-model="form.suffix"
-                :options="workflowDropdownOptions"
+                :options="suffixOptions"
                 placeholder="Please select a suffix"
               ></traction-select>
             </div>
@@ -123,11 +123,11 @@ import DataFetcher from '@/components/DataFetcher.vue'
 import BarcodeIcon from '@/icons/BarcodeIcon.vue'
 import { getCurrentDate } from '@/lib/DateHelpers.js'
 import {
-  createWorkflowDropdownOptions,
-  createWorkflowOptions,
+  createSuffixDropdownOptions,
+  createSuffixItems,
   createLabelsFromBarcodes,
 } from '@/lib/LabelPrintingHelpers.js'
-import WorkflowList from '@/config/WorkflowList.json'
+import SuffixList from '@/config/SuffixList.json'
 import { nextTick } from 'vue'
 import LabelTypes from '@/config/LabelTypes.json'
 
@@ -190,16 +190,16 @@ const labelType = computed(() => {
  * Creates a computed property to get the suffix options
  * @returns {Array} suffix options
  */
-const workflowDropdownOptions = computed(() => {
-  return createWorkflowDropdownOptions(WorkflowList)
+const suffixOptions = computed(() => {
+  return createSuffixDropdownOptions(SuffixList)
 })
 
 /**
  * Creates a computed property to get the suffix items
  * @returns {Array} suffix items
  */
-const workflowOptions = computed(() => {
-  return createWorkflowOptions(WorkflowList)
+const suffixItems = computed(() => {
+  return createSuffixItems(SuffixList)
 })
 
 /**
@@ -208,7 +208,7 @@ const workflowOptions = computed(() => {
  */
 const labels = computed(() => {
   const date = getCurrentDate()
-  const workflowItem = workflowOptions.value[form.suffix]
+  const suffixItem = suffixItems.value[form.suffix]
 
   // it is possible for there to be no barcodes so we need to add a guard
   // we filter to remove an nulls
@@ -218,7 +218,7 @@ const labels = computed(() => {
   return createLabelsFromBarcodes({
     sourceBarcodeList: splitSourceBarcodeList,
     date,
-    workflowItem,
+    suffixItem,
     numberOfLabels: form.numberOfLabels,
   })
 })
