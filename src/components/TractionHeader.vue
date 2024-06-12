9504956be6c7d9fae1094a87fbd6f7567594362b
wy1 <wy1@sanger.ac.uk>
2024-06-12 16:26:25 +0100

    separate the header component

---
+++ src/components/TractionHeader.vue
@@ -0,0 +1,48 @@
+<template>
+    <div :class="headerClasses">
+      <div class="max-w-7xl mx-auto px-4 sm:px-6">
+        <div class="flex justify-between items-center py-4 md:justify-start md:space-x-10">
+          <div class="flex flex-row gap-x-2">
+            <img class="w-8 h-8" src="../images/traction-logo.svg" alt="Traction logo" />
+            <div class="text-white text-2xl">Traction</div>
+            <TractionLink name="Home" link="/dashboard" view-type="2" />
+            <TractionLink name="Label Printing" link="/label-printing" view-type="2" />
+            <TractionLink name="QC Results Upload" link="/qc-results-upload" view-type="2" />
+            <TractionLink name="Reception" link="/reception" view-type="2" />
+          </div>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+    import TractionLink from '@/components/TractionLink'
+
+    export default {
+      components: {
+         TractionLink
+      },
+      name: 'TractionHeader',
+      data(){
+        return {
+           dev: false,
+           uat:true,
+           production: false
+        };
+      },
+      computed:{
+        headerClasses(){
+            let bgColorClass = '';
+            if (this.dev){
+                bgColorClass = 'bg-purple-600';
+            }else if(this.uat){
+                bgColorClass = 'bg-green-600';
+            }else {
+                bgColorClass = 'bg-blue-600';
+            }
+            return [ 'relative from-sdb to-sdb-400', bgColorClass ];
+        }
+      },
+    }
+
+</script>
