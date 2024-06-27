74745b1544b77fa4bb538452ee42f4f9f9762281
wy1 <wy1@sanger.ac.uk>
2024-06-27 15:41:30 +0100

    add TractionHeader test

---
+++ tests/unit/components/TractionHeader.spec.js
@@ -0,0 +1,15 @@
+import TractionHeader from '@/components/TractionHeader'
+import { mount } from '@support/testHelper'
+import.meta.env.VITE_ENVIRONMENT = 'uat'
+
+describe('TractionHeader.vue', () => {
+    let wrapper
+    
+    beforeEach(() => {
+        wrapper = mount(TractionHeader)
+    })
+
+    it('has headerClasses', () => {
+        expect(wrapper.vm.headerClasses).toStrictEqual(['relative from-sdb to-sdb-400', 'bg-purple-600'])
+    })
+})
