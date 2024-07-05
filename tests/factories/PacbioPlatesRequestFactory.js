582db66b6156ead4ac0a27a15534f08b6a975758
Seena Nair <55585488+seenanair@users.noreply.github.com>
2024-07-05 12:17:09 +0100

    Update PacbioPlatesRequestFactory.js

--- tests/factories/PacbioPlatesRequestFactory.js
+++ tests/factories/PacbioPlatesRequestFactory.js
@@ -313,7 +313,20 @@ const PacbioPlatesRequestFactory = () => {
     },
   }
 
-  return BaseFactory(data)
+  const storeData = [
+    {
+      barcode: 'DN814327C',
+      created_at: '2021/06/03 06:59',
+      id: '61',
+    },
+    {
+      barcode: 'DN814567Q',
+      created_at: '2021/06/03 14:57',
+      id: '62',
+    },
+  ]
+
+  return { ...BaseFactory(data), storeData }
 }
 
 export default PacbioPlatesRequestFactory
