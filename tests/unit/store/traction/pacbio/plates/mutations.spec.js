0533ebf41a145d98f7178ad5ea6af1165657fa5c
Seena Nair <55585488+seenanair@users.noreply.github.com>
2024-07-26 13:24:02 +0100

    refactor(pacbio-plates) : axios to fetch

--- tests/unit/store/traction/pacbio/plates/mutations.spec.js
+++ tests/unit/store/traction/pacbio/plates/mutations.spec.js
@@ -1,29 +1,14 @@
 import mutations from '@/store/traction/pacbio/plates/mutations'
-import { dataToObjectById } from '@/api/JsonApi'
+import PacbioPlatesRequestFactory from '@tests/factories/PacbioPlatesRequestFactory'
 
-let plates
+const pacbioPlatesRequestFactory = PacbioPlatesRequestFactory()
 
 describe('mutations', () => {
-  beforeEach(() => {
-    plates = [
-      {
-        barcode: 'DN814327C',
-        created_at: '2021/06/03 06:59',
-        id: '61',
-      },
-      {
-        barcode: 'DN814567Q',
-        created_at: '2021/06/03 14:57',
-        id: '62',
-      },
-    ]
-  })
-
   it('"setPlates" sets "state.plates" to the given plates', () => {
     const state = {
       plates: {},
     }
-    mutations.setPlates(state, plates)
-    expect(state.plates).toEqual(dataToObjectById({ data: plates, includeRelationships: true }))
+    mutations.setPlates(state, pacbioPlatesRequestFactory.content.data)
+    expect(state.plates).toEqual(pacbioPlatesRequestFactory.storeData.plates)
   })
 })
