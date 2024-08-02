352679f33b3296d367477a689a80457af3178e44
Dasun Pubudumal <dp27@sanger.ac.uk>
2024-08-02 09:01:09 +0100

    Fixing unit tests for v2

--- src/store/traction/pacbio/requests/getters.js
+++ src/store/traction/pacbio/requests/getters.js
@@ -1,7 +1,6 @@
 const getters = {
   requests: (state) => Object.values(state.requests),
-  // requestsRequest: (state, getters, rootState) => rootState.api.v2.traction.pacbio.requests,
-  requestsRequest: (state, getters, rootState) => rootState.api.v1.traction.pacbio.requests,
+  requestsRequest: (state, getters, rootState) => rootState.api.v2.traction.pacbio.requests,
 }
 
 export default getters
