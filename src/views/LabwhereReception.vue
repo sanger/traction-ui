ba40e0bace9b9f2a6f6f78179f215d46e3afe977
Seena Nair <55585488+seenanair@users.noreply.github.com>
2024-10-01 18:13:54 +0100

    refactor(labwhere-reception): improvements
    - Simplify the structure of the LabwhereReception.vue component
    - Update the form layout and styling
    - Remove unnecessary fieldset elements
    - Update the descriptions of the form fields
    - Update the validation logic for user barcode and labware barcodes
    - Update the confirmation text based on the action (scan in or scan out)

--- src/views/LabwhereReception.vue
+++ src/views/LabwhereReception.vue
@@ -1,81 +1,66 @@
 <template>
-  <div class="w-full md:w-3/4 mx-auto">
-    <traction-form
-      classes="flex flex-col md:flex-row px-10 gap-4"
-      @submit="storeBarcodes"
-      @reset="onReset"
-    >
-      <div class="w-full md:w-3/5 p-4 gap-4 bg-gray-100 rounded-md">
-        <div class="space-y-10">
-          <fieldset>
-            <traction-heading level="4" show-border>User barcode or swipecard</traction-heading>
-            <traction-field-error data-attribute="user-code-error" :error="errors.user_code">
-              <traction-input
-                id="userCode"
-                v-model="user_code"
-                class="flex w-full"
-                @update:model-value="validateUserCode"
-              />
-            </traction-field-error>
-          </fieldset>
+  <div class="w-3/5 mx-auto bg-gray-100 border border-gray-200 bg-gray-100 rounded-md p-4">
+    <traction-form @submit="scanBarcodesToLabwhere">
+      <fieldset>
+        <traction-heading level="4" show-border description="Enter user barcode/swipecard">User barcode or swipecard</traction-heading>
+        <traction-field-error data-attribute="user-code-error" :error="errors.user_code">
+          <traction-input
+            id="userCode"
+            v-model="user_code"
+            class="flex w-full"
+            @update:model-value="validateUserCode"
+          />
+        </traction-field-error>
+      </fieldset>
 
-          <fieldset>
-            <traction-heading level="4" show-border description="Scan location barcode"
-              >Location barcode</traction-heading
-            >
-            <traction-field-error
-              data-attribute="location-barcode-error"
-              :error="errors.location_barcode"
-            >
-              <traction-input
-                id="locationBarcode"
-                v-model="location_barcode"
-                class="flex w-full"
-                @update:model-value="validateLocationBarcode"
-            /></traction-field-error>
-          </fieldset>
+      <fieldset>
+        <traction-heading level="4" show-border description="Scan location barcode (Leave blank to scan out)"
+          >Location barcode</traction-heading
+        >
+        <traction-field-error
+          data-attribute="location-barcode-error"
+          :error="errors.location_barcode"
+        >
+          <traction-input
+            id="locationBarcode"
+            v-model="location_barcode"
+            class="flex w-full"
+        /></traction-field-error>
+      </fieldset>
 
-          <fieldset>
-            <traction-heading level="4" show-border>Start position</traction-heading>
-            <traction-input id="startPosition" v-model="start_position" type="number" />
-          </fieldset>
+      <fieldset>
+        <traction-heading level="4" show-border description="Enter position (Only necessary for locations with coordinates)">Start position</traction-heading>
+        <traction-input id="startPosition" v-model="start_position" type="number" />
+      </fieldset>
 
-          <fieldset>
-            <traction-heading level="4" show-border description="Scan barcodes to save to location"
-              >Barcodes</traction-heading
-            >
-            <traction-field-error data-attribute="barcodes-error" :error="errors.labware_barcodes">
-              <textarea
-                id="labware_barcodes"
-                v-model="labware_barcodes"
-                rows="4"
-                max-rows="10"
-                name="labware_barcodes"
-                class="w-full text-base py-2 px-3 border border-gray-300 bg-white rounded-md"
-                @input="validateLabwareBarcodes"
-              />
-            </traction-field-error>
-          </fieldset>
-        </div>
+      <fieldset>
+        <traction-heading level="4" show-border description="Scan barcodes to save to location"
+          >Barcodes</traction-heading
+        >
+        <traction-field-error data-attribute="barcodes-error" :error="errors.labware_barcodes">
+          <textarea
+            id="labware_barcodes"
+            v-model="labware_barcodes"
+            rows="10"
+            max-rows="20"
+            name="labware_barcodes"
+            class="w-full text-base py-2 px-3 border border-gray-300 bg-white rounded-md"
+            @input="validateLabwareBarcodes"
+          />
+        </traction-field-error>
+      </fieldset>
+      <div class="flex flex-col w-full py-3 gap-y-3">
+        <fieldset class="border border-2 border-gray-400 p-3 bg-gray-300 rounded-md">
+          <legend class="text-md text-gray-500 font-semibold px-2">Preview</legend>
+          <traction-label data-attribute="preview-message" class="text-blue-700">
+            {{ ` ${confirmationText} ` }}
+          </traction-label>
+        </fieldset>
       </div>
-      <div class="w-full md:w-2/5 p-4 space-y-4 bg-sdb-400 rounded-md border-t-4 border-sp">
-        <traction-heading level="3" class-name="text-white italic" show-border>
-          Preview and submit
-        </traction-heading>
-
-        <traction-label class="flex text-white text-left" data-attribute="preview-message">
-          {{ ` ${confirmationText} ` }}
-        </traction-label>
-        <div>
-          <div class="space-x-4 pb-4 flex flex-row">
-            <traction-button id="submit-button" class="grow" type="submit" theme="printRed">
-              Store barcodes
-            </traction-button>
-            <traction-button id="reset-button" type="reset" theme="resetWhite">
-              Reset
-            </traction-button>
-          </div>
-        </div>
+      <div class="flex w-full py-4 gap-y-3">
+        <traction-button id="submit-button" class="h-15 py-3" type="submit" theme="printRed">
+          Scan In/Out
+        </traction-button>
       </div>
     </traction-form>
   </div>
@@ -85,26 +70,27 @@
 /**
  * LabwhereReception Component
  *
- * This component provides a form interface for users to store labware barcodes into a specified location in Labwhere.
- *
+ * This component provides a form interface for users to scan labware barcodes into a specified location or to scan barcodes out of their parent location in Labwhere.
+ * Scanning in requires a user barcode, location barcode, and labware barcodes.
+ * For scanning out, the location barcode should be blank and it requires a user barcode and labware barcodes only.
+ * 
  * The form includes fields for:
  * - User barcode or swipecard
  * - Location barcode
  * - Start position (optional)
  * - Labware barcodes
  *
- * The component validates the input fields for 'User barcode', 'Location barcode' and 'Labware barcodes'
- * and displays error messages for any invalid or missing data when the form is submitted or when the input fields are updated.
+ * The component validates the input fields for 'User barcode', and 'Labware barcodes'
+ * and displays error messages for any missing data when the form is submitted or when the input fields are updated.
  * The 'Start position' field is optional and does not require validation.
  *
- * The form also provides a reset button to clear the form fields.
- * The component also provides a preview section where users can review the barcodes to be stored before submission.
+ * The component also provides a preview section where users can review the action before submission.
  *
- * Upon successful validation, the form data is submitted to the Labwhere API to store the barcodes in the specified location.
+ * Upon successful validation, the form data is submitted to the Labwhere API to scan in/out the barcodes in/from the specified location.
  * It displays a success message if the barcodes are stored successfully, or an error message if the submission fails.
  */
 import { ref, reactive, computed } from 'vue'
-import { storeBarcodesIntoLabwhereLocation } from '@/services/labwhere/client.js'
+import { scanBarcodesInLabwhereLocation } from '@/services/labwhere/client.js'
 import useAlert from '@/composables/useAlert.js'
 import TractionForm from '@/components/shared/TractionForm.vue'
 import TractionHeading from '@/components/TractionHeading.vue'
@@ -132,16 +118,6 @@ const validateUserCode = () => {
   }
 }
 
-/**
- * Validate location barcode
- */
-const validateLocationBarcode = () => {
-  if (!location_barcode.value) {
-    errors.location_barcode = 'Location barcode is required'
-  } else {
-    delete errors.location_barcode
-  }
-}
 
 /**
  * Validate labware barcodes
@@ -150,12 +126,7 @@ const validateLabwareBarcodes = () => {
   if (!labware_barcodes.value) {
     errors.labware_barcodes = 'Labware barcodes are required'
   } else {
-    const uniqueBarcodes = new Set(barcodeArray.value)
-    if (uniqueBarcodes.size !== barcodeArray.value.length) {
-      errors.labware_barcodes = 'Labware barcodes must be unique'
-    } else {
-      delete errors.labware_barcodes
-    }
+    delete errors.labware_barcodes
   }
 }
 
@@ -167,37 +138,41 @@ const validateForm = () => {
   resetErrors() // Clear previous errors
 
   validateUserCode()
-  validateLocationBarcode()
   validateLabwareBarcodes()
 
   return Object.keys(errors).length === 0
 }
 
 /**
- * Computed property to get the array of labware barcodes.
+ * Computed property to get the array of unique labware barcodes.
  */
-const barcodeArray = computed(() => {
-  return labware_barcodes.value
-    .split('\n')
-    .map((barcode) => barcode.trim())
-    .filter((barcode) => barcode !== '')
+ const uniqueBarcodesArray = computed(() => {
+  return Array.from(
+    new Set(
+      labware_barcodes.value
+        .split('\n')
+        .map((barcode) => barcode.trim())
+        .filter((barcode) => barcode !== '')
+    )
+  )
 })
 
 /**
  * Store barcodes into labwhere location
  *
  */
-const storeBarcodes = async () => {
+const scanBarcodesToLabwhere = async () => {
   if (validateForm()) {
     try {
-      const response = await storeBarcodesIntoLabwhereLocation(
+      const response = await scanBarcodesInLabwhereLocation(
         user_code.value,
         location_barcode.value,
-        labware_barcodes.value,
+        uniqueBarcodesArray.value.join('\n'),
         start_position.value,
       )
       if (response.success) {
-        showAlert('Barcodes stored successfully', 'success')
+        const message = response.message??'Barcodes stored successfully'
+        showAlert( message, 'success')
       } else {
         showAlert(response.errors.join('\n'), 'danger')
       }
@@ -211,10 +186,18 @@ const storeBarcodes = async () => {
  * Computed property to get the confirmation text.
  */
 const confirmationText = computed(() => {
-  const barcodeCount = barcodeArray.value.length
-  return barcodeCount === 0
-    ? `No barcodes to store to location ${location_barcode.value}`
-    : `Store ${barcodeCount} barcode(s) to location ${location_barcode.value}`
+  const barcodeCount = uniqueBarcodesArray.value.length
+  let text = ''
+  if(barcodeCount ===0) {
+    text = `No barcodes to scan to location ${location_barcode.value}`
+  } else {
+    if(location_barcode.value) {
+      text = `Scan in ${barcodeCount} barcode(s) to location ${location_barcode.value}`
+    } else {
+      text = `Scan out ${barcodeCount} barcode(s) from their current locations`
+    }
+  }
+  return text
 })
 
 /**
@@ -222,16 +205,5 @@ const confirmationText = computed(() => {
  */
 const resetErrors = () => {
   Object.keys(errors).forEach((key) => delete errors[key])
-}
-
-/**
- * Reset the form fields and errors.
- */
-const onReset = () => {
-  user_code.value = ''
-  location_barcode.value = ''
-  labware_barcodes.value = ''
-  start_position.value = null
-  resetErrors()
 }
 </script>
