9fb09157466e52e2d6c1d24bd3890f20ec164ead
Stephen <519327+stevieing@users.noreply.github.com>
2024-10-28 11:22:42 +0000

    Add 'allowNone' prop to DataTypeSelect component

--- src/components/shared/DataTypeSelect.vue
+++ src/components/shared/DataTypeSelect.vue
@@ -29,6 +29,10 @@ export default {
       type: String,
       default: undefined,
     },
+    allowNone: {
+      type: Boolean,
+      default: true,
+    },
   },
   emits: ['update:modelValue'],
   setup() {
