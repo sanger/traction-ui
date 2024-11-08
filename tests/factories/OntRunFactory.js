78f173d599dd56cb98f40ffd032b0423f3a0bcba
Seena Nair <55585488+seenanair@users.noreply.github.com>
2024-11-08 12:27:42 +0000

    refactor(ont-run-factory): merged ontRunsFactory and ontRunFactory

--- tests/factories/OntRunFactory.js
+++ tests/factories/OntRunFactory.js
@@ -1,37 +1,161 @@
 import BaseFactory from './BaseFactory.js'
+import { dataToObjectById, find } from '../../src/api/JsonApi.js'
+
 /**
- * Factory for creating ONT run data.
+ * Creates store data from the provided data.
+ *
+ * @param {Object} data - The data to be processed.
+ * @returns {Object} An object containing the runs, runsArray, validRun, newRun, and findRun.
+ */
+const createStoreData = (data) => {
+  const runs = dataToObjectById({
+    data: data.data,
+    includeRelationships: true,
+  })
+  return {
+    runs,
+    runsArray: Object.values(runs),
+    validRun: {
+      id: 1,
+      instrument_name: 'GXB02004',
+      state: 'pending',
+      flowcell_attributes: [{ tube_barcode: 'TRAC-A-1' }],
+    },
+    newRun: {
+      id: 'new',
+      instrument_name: null,
+      state: null,
+      flowcell_attributes: [],
+    },
+    findRun: {
+      flowcell_attributes: [],
+      id: '1',
+      state: 'pending',
+      instrument_name: 'GXB02004',
+    },
+  }
+}
+
+/**
+ *
+ * @param {Object} data
+ * @param {String | null} findBy - 'libraries' | 'pools'
+ * @returns {Object} - { ...BaseFactory, storeData }
+ * if the findBy is libraries or pools find a single record by libraries or pools
+ * otherwise return the data as is.
+ * This is a bit messy but it will do for now.
+ */
+const getData = (data, findBy) => {
+  const index =
+    findBy === 'flowcells'
+      ? data.data.findIndex((item) => item.relationships.flowcells?.data)
+      : null
+
+  if (index !== null) {
+    // we need to includeAll as the requests for pools are in the libraries and I think
+    // pulled out as used_by in the aliquots
+    const foundData = find({ data, start: index, count: 1, get: true, includeAll: true })
+    return { ...BaseFactory(foundData), storeData: createStoreData(foundData) }
+  } else {
+    return { ...BaseFactory(data), storeData: createStoreData(data) }
+  }
+}
+
+/**
+ * Factory for creating ONT runs data.
  *
  * @returns {Object} An object containing the base factory data, find data, and store data.
  */
-const OntRunFactory = () => {
+
+const OntRunFactory = ({ findBy = null } = {}) => {
   const data = {
-    data: {
-      id: '2',
-      type: 'runs',
-      links: {
-        self: 'http://127.0.0.1:3100/v1/ont/runs/2',
-      },
-      attributes: {
-        experiment_name: 'ONTRUN-2',
-        state: 'started',
-        created_at: '2023-01-11T15:51:13.691Z',
-        ont_instrument_id: 2,
-      },
-      relationships: {
-        flowcells: {
-          links: {
-            self: 'http://127.0.0.1:3100/v1/ont/runs/2/relationships/flowcells',
-            related: 'http://127.0.0.1:3100/v1/ont/runs/2/flowcells',
-          },
-          data: {
-            type: 'flowcells',
-            id: '3',
+    data: [
+      {
+        id: '1',
+        type: 'runs',
+        links: {
+          self: 'http://127.0.0.1:3100/v1/ont/runs/1',
+        },
+        attributes: {
+          experiment_name: 'ONTRUN-1',
+          state: 'pending',
+          created_at: '2023-01-11T15:45:25.034Z',
+          ont_instrument_id: 1,
+        },
+        relationships: {
+          instrument: {
+            links: {
+              self: 'http://127.0.0.1:3100/v1/ont/runs/1/relationships/instrument',
+              related: 'http://127.0.0.1:3100/v1/ont/runs/1/instrument',
+            },
+            data: {
+              type: 'instruments',
+              id: '1',
+            },
           },
         },
       },
-    },
+      {
+        id: '2',
+        type: 'runs',
+        links: {
+          self: 'http://127.0.0.1:3100/v1/ont/runs/2',
+        },
+        attributes: {
+          experiment_name: 'ONTRUN-2',
+          state: 'started',
+          created_at: '2023-01-11T15:51:13.691Z',
+          ont_instrument_id: 2,
+        },
+        relationships: {
+          instrument: {
+            links: {
+              self: 'http://127.0.0.1:3100/v1/ont/runs/2/relationships/instrument',
+              related: 'http://127.0.0.1:3100/v1/ont/runs/2/instrument',
+            },
+            data: {
+              type: 'instruments',
+              id: '1',
+            },
+          },
+          flowcells: {
+            links: {
+              self: 'http://127.0.0.1:3100/v1/ont/runs/2/relationships/flowcells',
+              related: 'http://127.0.0.1:3100/v1/ont/runs/2/flowcells',
+            },
+            data: {
+              type: 'flowcells',
+              id: '3',
+            },
+          },
+        },
+      },
+    ],
     included: [
+      {
+        id: '1',
+        type: 'instruments',
+        links: {
+          self: 'http://127.0.0.1:3100/v1/ont/instruments/1',
+        },
+        attributes: {
+          name: 'GXB02004',
+          instrument_type: 'GridION',
+          max_number_of_flowcells: 5,
+        },
+      },
+      {
+        id: '2',
+        type: 'instruments',
+        links: {
+          self: 'http://127.0.0.1:3100/v1/ont/instruments/2',
+        },
+        attributes: {
+          name: 'PC24B148',
+          instrument_type: 'PromethION',
+          max_number_of_flowcells: 24,
+        },
+      },
       {
         id: '3',
         type: 'flowcells',
@@ -42,11 +166,12 @@ const OntRunFactory = () => {
         },
       },
     ],
+    meta: {
+      page_count: 1,
+    },
   }
 
-  return {
-    ...BaseFactory(data),
-  }
+  return getData(data, findBy)
 }
 
 export default OntRunFactory
