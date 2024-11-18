a9f45a210cff0f9c3bcff1839739fc5b348b595b
sabrine33 <sa33@sanger.ac.uk>
2024-11-18 11:26:34 +0000

    Update the PR according to the code review recommendations.

--- tests/unit/views/GeneralReception.spec.js
+++ tests/unit/views/GeneralReception.spec.js
@@ -63,7 +63,7 @@ describe('GeneralReception', () => {
       const { wrapperObj: wrapper } = buildWrapper()
       const workflowSelect = wrapper.find('#workflowSelect')
       await workflowSelect.setValue('lw-shelf-1-30451')
-      expect(wrapper.find('[data-testid=additional-details]').text()).toContain(
+      expect(wrapper.find('[data-testid=workflow-location-text]').text()).toContain(
         'The imported labware will be scanned into LRT007 – Shelf 1',
       )
     })
