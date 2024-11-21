5f21c193db0057082f34f3993eb94c910bc22a5f
Ben Topping <ben.topping@outlook.com>
2024-11-21 20:46:20 +0000

    style: restyled dashboard and pipeline tabs

--- src/views/PipelineView.vue
+++ src/views/PipelineView.vue
@@ -1,9 +1,8 @@
 <template>
   <div class="flex flex-col justify-start">
     <div class="flex flex-row border-b-2 border-gray-200">
-      <span class="text-2xl pr-4 text-gray-400 text-light">{{ pipelineInfo.title }}</span>
       <traction-menu>
-        <div v-for="(pipelineRoute, index) in pipelineInfo.routes" :key="index" class="flex">
+        <div v-for="(pipelineRoute, index) in pipelineInfo.routes" :key="index">
           <router-link
             v-if="!isActive(pipelineRoute)"
             :class="routeClass(pipelineRoute)"
@@ -13,6 +12,7 @@
           <span v-else :class="routeClass(pipelineRoute)">{{ humanise(pipelineRoute) }} </span>
         </div>
       </traction-menu>
+      <span class="text-2xl pl-4 text-gray-400 text-light">{{ pipelineInfo.title }}</span>
     </div>
     <router-view class="mt-2" />
   </div>
@@ -47,10 +47,10 @@ export default {
       return this.$route.path === this.path(pipelineRoute)
     },
     routeClass(route) {
-      return `text-black bg-gray-200 hover:bg-sdb-300 hover:text-white flex
-        pl-2 pr-2 p-0 m-0 rounded-t-md
+      return `text-black hover:bg-sdb-300 hover:text-white flex
+        py-2 px-4 m-0 rounded-t-md w-24 justify-center
         transition-colors duration-100 ease-in-out cursor-pointer
-        ${this.isActive(route) ? 'text-white bg-sdb-300' : 'text-black bg-gray-200 hover:bg-sdb-300'}`
+        ${this.isActive(route) ? 'text-white bg-sdb-300' : 'text-black bg-gray-200'}`
     },
     humanise,
   },
--- src/views/TractionDashboard.vue
+++ src/views/TractionDashboard.vue
@@ -1,41 +1,62 @@
 <template>
   <div>
-    <div :class="`grid grid-cols-${pipelines.length} px-4 gap-x-8`">
-      <LabelledCard
-        v-for="(pipeline, index) in pipelines"
-        :key="index"
-        :title="pipeline.title"
-        :description="pipeline.description"
-      >
-        <!-- Links ordered by user workflow, specified in pipelines config -->
-        <TractionLink
-          v-for="(route, inner_index) in pipeline.routes"
-          :key="inner_index"
-          :name="humanise(route)"
-          :link="`/${pipeline.name}/${route}`"
-        />
-      </LabelledCard>
+    <div class="flex flex-col px-4 gap-y-4">
+      <div class="flex flex-col text-left pb-4">
+        <h1
+          class="text-4xl bg-gradient-to-r from-sp-400 to-sdb-400 inline-block text-transparent bg-clip-text w-40"
+        >
+          Traction
+        </h1>
+        <span class="text-lg text-gray font-light"
+          >Long read LIMS for library prep and sequencing setup</span
+        >
+      </div>
+      <div class="flex flex-col text-left border border-gray-200 rounded-md p-4">
+        <h2 class="text-2xl">Pipelines</h2>
+        <span class="text-md text-gray font-light"
+          >A set of quick links for each sequencing pipeline</span
+        >
+        <div :class="`grid grid-cols-${pipelines.length} gap-x-8`">
+          <LabelledCard
+            v-for="(pipeline, index) in pipelines"
+            :key="index"
+            :title="pipeline.title"
+            :description="pipeline.description"
+          >
+            <!-- Links ordered by user workflow, specified in pipelines config -->
+            <TractionLink
+              v-for="(route, inner_index) in pipeline.routes"
+              :key="inner_index"
+              :name="humanise(route)"
+              :link="`/${pipeline.name}/${route}`"
+            />
+          </LabelledCard>
+        </div>
+      </div>
+
+      <div class="flex flex-col text-left border border-gray-200 rounded-md p-4">
+        <h2 class="text-2xl">Tools</h2>
+        <span class="text-md text-gray font-light">A set of quick links for Traction tools</span>
+        <div class="flex flex-row flex-wrap gap-y-8 gap-x-3 py-8 px-4 text-center">
+          <TractionLink
+            v-for="route in toolRoutes"
+            :key="route"
+            :name="humanise(route)"
+            :link="`/${route}`"
+          />
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
-<script>
+<script setup>
+import { computed } from 'vue'
 import PipelinesConfig from '@/config/PipelinesConfig'
 import { humanise } from '@/lib/stringHumanisation'
 import LabelledCard from '@/components/LabelledCard'
 import TractionLink from '@/components/TractionLink'
 
-export default {
-  name: 'TractionDashboard',
-  components: {
-    LabelledCard,
-    TractionLink,
-  },
-  computed: {
-    pipelines: () => PipelinesConfig.filter(({ active }) => active),
-  },
-  methods: {
-    humanise,
-  },
-}
+const toolRoutes = ['label-printing', 'qc-results-upload', 'reception', 'labwhere-reception']
+const pipelines = computed(() => PipelinesConfig.filter(({ active }) => active))
 </script>
