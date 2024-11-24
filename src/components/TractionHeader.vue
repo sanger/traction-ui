3fe768026428391b9cde2e271f2b23ff4f204375
Ben Topping <ben.topping@outlook.com>
2024-11-24 21:10:48 +0000

    style: new spinner and related component refresh, positional cleanup to dashboard

--- src/components/TractionHeader.vue
+++ src/components/TractionHeader.vue
@@ -2,9 +2,9 @@
   <div :class="headerClasses">
     <div class="max-w-7xl mx-auto px-4 sm:px-6">
       <div class="flex justify-between items-center py-4 md:justify-start md:space-x-10">
-        <div class="flex flex-row gap-x-2">
-          <router-link to="/" class="flex gap-x-2">
-            <img class="w-8 h-8" src="../images/traction-logo.svg" alt="Traction logo" />
+        <div class="flex flex-row gap-x-2 items-center">
+          <router-link to="/" class="flex-none gap-x-2">
+            <img class="w-10 h-10" src="../images/traction-logo.svg" alt="Traction logo" />
           </router-link>
           <TractionLink name="Label Printing" link="/label-printing" view-type="2" />
           <TractionLink name="QC Results Upload" link="/qc-results-upload" view-type="2" />
