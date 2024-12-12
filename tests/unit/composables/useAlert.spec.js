d9026d6978df88fe1a3db058e5e73c90a605b85c
Stephen <519327+stevieing@users.noreply.github.com>
2024-12-12 09:15:04 +0000

    Mock vue-router in useAlert tests to prevent injection warnings and reset mock before each test

--- tests/unit/composables/useAlert.spec.js
+++ tests/unit/composables/useAlert.spec.js
@@ -1,13 +1,25 @@
 import useAlert from '@/composables/useAlert.js'
 import store from '@/store'
+import { useRouter } from 'vue-router'
+
+// we need to mock the vue-router to avoid the following warning:
+// [Vue warn]: inject() can only be used inside setup() or functional components.
+vi.mock('vue-router')
 
 describe('#useAlert', () => {
+  useRouter.mockReturnValue({
+    push: vi.fn(),
+  })
+
+  beforeEach(() => {
+    useRouter().push.mockReset()
+  })
+
   it('commits addMessage on show Alert call', () => {
     const mockCommit = vi.fn().mockResolvedValue({})
     store.commit = mockCommit
     const { showAlert } = useAlert()
     showAlert('show this message', 'success')
-    // TODO: [Vue warn]: inject() can only be used inside setup() or functional components.
     expect(mockCommit).toHaveBeenCalledWith('traction/addMessage', {
       type: 'success',
       message: 'show this message',
