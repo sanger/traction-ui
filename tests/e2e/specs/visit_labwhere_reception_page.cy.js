9546b611223da0325744ab3fac2b1eaa263a4b90
Seena Nair <55585488+seenanair@users.noreply.github.com>
2024-12-16 17:51:39 +0000

    Update visit_labwhere_reception_page.cy.js

--- tests/e2e/specs/visit_labwhere_reception_page.cy.js
+++ tests/e2e/specs/visit_labwhere_reception_page.cy.js
@@ -1,3 +1,6 @@
+
+import PacbioLibraryFactory from '../../factories/PacbioLibraryFactory.js'
+
 describe('Labware Reception page', () => {
   beforeEach(() => {
     cy.visit('#/labwhere-reception')
@@ -33,6 +36,39 @@ describe('Labware Reception page', () => {
     cy.wait('@storeBarcodes').its('response.statusCode').should('eq', 201)
     cy.contains('barcode1, barcode2 successfully stored in location123')
   })
+
+  it('exhausts samples when scanning to a destroyed location ', () => {
+    cy.get('#userCode').type('user123')
+    cy.get('#locationBarcode').type('lw-destroyed-217')
+    cy.get('#labware_barcodes').type('barcode1\nbarcode2')
+    const pacbioLibraryFactory = PacbioLibraryFactory()
+    
+    cy.wrap(pacbioLibraryFactory).as('pacbioLibraryFactory')
+      cy.get('@pacbioLibraryFactory').then((pacbioLibraryFactory) => {
+      cy.intercept('/v1/pacbio/libraries?filter[barcode]=barcode1,barcode2&include=request,tag,tube', {
+        statusCode: 200,
+        body: pacbioLibraryFactory.content,
+      })
+      cy.intercept('PATCH', '/v1/pacbio/libraries/722', {
+        statusCode: 200,
+        body: {
+          data: {},
+        },
+      })
+    })
+    cy.intercept('POST', '/api/scans', {
+      statusCode: 201,
+      body: {
+        message: 'barcode1, barcode2 successfully stored in location123',
+      },
+    }).as('storeBarcodes')
+
+    cy.get('#submit-button').click()
+
+    cy.wait('@storeBarcodes').its('response.statusCode').should('eq', 201)
+    cy.contains('barcode1, barcode2 successfully stored in location123 and sample volumes have been exhausted for 1 library')
+  })
+
 
   it('displays error when POST is unsuccessful', () => {
     cy.get('#userCode').type('user123')
