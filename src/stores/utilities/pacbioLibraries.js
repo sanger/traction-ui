995d5cc62487a3c00bffbc4aec22724b5bd54afb
Seena Nair <55585488+seenanair@users.noreply.github.com>
2025-01-03 14:36:33 +0000

    refactor: simplify library update logic and remove unused fetchLibraries function

--- src/stores/utilities/pacbioLibraries.js
+++ src/stores/utilities/pacbioLibraries.js
@@ -1,53 +1,4 @@
-import useRootStore from '@/stores'
-import { handleResponse } from '@/api/ResponseHelper.js'
-import { groupIncludedByResource, dataToObjectById } from '@/api/JsonApi.js'
-
-/**
- *
- * @param {Integer | String} id - id of the library
- * @param {Integer | String} pacbio_request_id - id of the pacbio request
- * @param {String} template_prep_kit_box_barcode - barcode of the template prep kit box
- * @param {Integer | String} tag_id - id of the tag
- * @param {Float} concentration - concentration of the library
- * @param {Float} volume - volume of the library
- * @param {Float} insert_size - insert size of the library
- * @returns {Object} - payload for creating a library
- * if it is an update id is added otherwise pacbio_request_id is added
- *
- */
-const libraryPayload = ({
-  id,
-  pacbio_request_id,
-  template_prep_kit_box_barcode,
-  tag_id,
-  concentration,
-  volume,
-  insert_size,
-}) => {
-  const requiredAttributes = {
-    template_prep_kit_box_barcode,
-    tag_id,
-    concentration,
-    volume,
-    insert_size,
-  }
-
-  const payload = {
-    data: {
-      type: 'libraries',
-      attributes: {
-        ...requiredAttributes,
-        primary_aliquot_attributes: {
-          ...requiredAttributes,
-        },
-      },
-    },
-  }
-
-  id ? (payload.data.id = id) : (payload.data.attributes.pacbio_request_id = pacbio_request_id)
-
-  return payload
-}
+import { updatePacbioLibraryResource } from '@/services/traction/PacbioLibrary.js'
 
 /**
  * @function validateFields
@@ -73,46 +24,6 @@ const validateLibraryFields = (library) => {
     success: errors.length === 0,
     errors: errors.length ? 'Missing required field(s)' : '',
   }
-}
-
-/**
- * Fetches all libraries.
- *
- * @param {Object} fetchOptions - The options to fetch libraries with.
- * The options include page, filter, and include.
- * e.g { page: { "size": "24", "number": "1"}, filter: { source_identifier: 'sample1' }, include: 'request,tag,tube' }
- */
-async function fetchLibraries(fetchOptions = {}) {
-  const includes = new Set(fetchOptions.include ? fetchOptions.include.split(',') : [])
-  const requiredIncludes = ['request', 'tag', 'tube']
-  requiredIncludes.forEach((item) => includes.add(item))
-
-  const fetchOptionsDefaultInclude = {
-    ...fetchOptions,
-    include: Array.from(includes).join(','),
-  }
-  const rootStore = useRootStore()
-  const pacbioLibraries = rootStore.api.traction.pacbio.libraries
-  const promise = pacbioLibraries.get(fetchOptionsDefaultInclude)
-  const response = await handleResponse(promise)
-
-  const { success, body: { data, included = [], meta = {} } = {}, errors = [] } = response
-  let libraries = {},
-    tubes = {},
-    tags = {},
-    requests = {}
-  if (success && data && data.length > 0) {
-    const {
-      tubes: included_tubes,
-      tags: included_tags,
-      requests: included_requests,
-    } = groupIncludedByResource(included)
-    libraries = dataToObjectById({ data, includeRelationships: true })
-    tubes = dataToObjectById({ data: included_tubes })
-    tags = dataToObjectById({ data: included_tags })
-    requests = dataToObjectById({ data: included_requests })
-  }
-  return { success, data, errors, meta, libraries, tubes, tags, requests }
 }
 
 /**
@@ -150,7 +61,7 @@ const formatAndTransformLibraries = (libraries, tubes, tags, requests) =>
  */
 async function exhaustLibrayVolume(library) {
   library.volume = library.used_volume
-  const { success, errors } = await updateLibrary(library)
+  const { success, errors } = await validateAndUpdateLibrary(library)
   return { success, errors }
 }
 
@@ -160,24 +71,14 @@ async function exhaustLibrayVolume(library) {
  * @param {Object} libraryFields - The fields of the library to update.
  * @returns {Promise<Object>} - An object containing the success status and any errors.
  */
-async function updateLibrary(libraryFields) {
+async function validateAndUpdateLibrary(libraryFields) {
   //Validate the libraryFields to ensure that all required fields are present
   const valid = validateLibraryFields(libraryFields)
   if (!valid.success) {
     return valid
   }
-  const rootStore = useRootStore()
-  const request = rootStore.api.traction.pacbio.libraries
-  const payload = libraryPayload(libraryFields)
-  const promise = request.update(payload)
-  const { success, errors } = await handleResponse(promise)
+  const { success, errors } = await updatePacbioLibraryResource(libraryFields)
   return { success, errors }
 }
 
-export {
-  libraryPayload,
-  fetchLibraries,
-  updateLibrary,
-  exhaustLibrayVolume,
-  formatAndTransformLibraries,
-}
+export { validateAndUpdateLibrary, exhaustLibrayVolume, formatAndTransformLibraries }
