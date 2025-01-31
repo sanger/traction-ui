e95fc0ba51d585874d4651cf94b852122bdde42e
Stephen <519327+stevieing@users.noreply.github.com>
2025-01-31 15:59:57 +0000

    feat(ontPoolCreate): enhance store with pooling data structure and related getters

--- src/stores/ontPoolCreate.js
+++ src/stores/ontPoolCreate.js
@@ -5,6 +5,16 @@ import { defineStore } from 'pinia'
 // import { useOntRootStore } from '@/stores/ontRoot.js'
 
 /**
+ * Used for combining objects based on id
+ * TODO: Move to helper
+ */
+const mergeRepresentations = (parent, child, keyFunction = (id) => id) => {
+  return Object.values(parent).map((parentRecord) => {
+    return { ...child[keyFunction(parentRecord.id)], ...parentRecord }
+  })
+}
+
+/**
  * Defines a store for managing Ont pool creation.
  *
  * @exports useOntPoolCreateStore
@@ -15,6 +25,39 @@ export const useOntPoolCreateStore = defineStore('ontPoolCreate', {
       // The main source of request information. Requests are indexed by id.
       // Populated by the requests included in the request for plates.
       requests: {},
+      // The main plate store. Represents the authoritative source of plate
+      // information. Plates are indexed by id.
+      plates: {},
+      // The main tube store. Represents the authoritative source of tube
+      // information. Plates are indexed by id.
+      tubes: {},
+      /**
+       * The main source of well information. Wells are indexed by id.
+       * Populated by the wells included in the request for plates.
+       * @example {"id":"1","type":"wells","position":"A1","requests":["1"]}
+       */
+      wells: {},
+    },
+    selected: {
+      // Object reflecting the id of the selected tag set and a selected
+      // attribute.
+      // eg { id: '1' }
+      tagSet: {},
+      // Object representing all selected plates. Selected plates are indexed by
+      // id. Each plate is represented by an object with an id and a selected
+      // attribute { id: 'plate_id', selected: true }
+      plates: {},
+      // Object representing all selected tubes. Selected tubes are indexed by
+      // id. Each tube is represented by an object with an id and a selected
+      // attribute { id: 'tube_id', selected: true }
+      tubes: {},
+    },
+    // Handles the store data for the pooling page
+    pooling: {
+      // Pool: The current pool being edited or created
+      pool: {},
+      // Tube: The tube for the current pool
+      tube: {},
     },
   }),
   getters: {
@@ -23,6 +66,51 @@ export const useOntPoolCreateStore = defineStore('ontPoolCreate', {
      * @param {Object} state The Vuex state object
      */
     requests: (state) => Object.values(state.resources.requests),
+
+    /**
+     * Returns the pool
+     * @param {Object} state The Vuex state object
+     */
+    poolItem: (state) => state.pooling.pool || {},
+
+    /**
+     * Returns the tube
+     * @param {Object} state The Vuex state object
+     */
+    tubeItem: (state) => state.pooling.tube || {},
+
+    /**
+     * Returns a library
+     * @param {Object} state The Vuex state object
+     */
+    libraryItem: (state) => (id) => state.pooling.libraries[id],
+
+    /**
+     * Returns a list of selected plates
+     * @param {Object} state The Vuex state object
+     */
+    selectedPlates: ({ selected, resources }) =>
+      mergeRepresentations(selected.plates, resources.plates),
+
+    /**
+     * Returns a list of selected tubes
+     * @param {Object} state The Vuex state object
+     */
+    selectedTubes: ({ selected, resources }) =>
+      mergeRepresentations(selected.tubes, resources.tubes),
+
+    /**
+     * Returns a list of all fetched wells
+     * @param {Object} state The Vuex state object
+     */
+    wellList: (state) => (ids) => {
+      const wells = state.resources.wells
+      if (ids) {
+        return ids.map((id) => wells[id])
+      } else {
+        return wells.values
+      }
+    },
   },
   actions: {},
 })
--- tests/unit/stores/ontPoolCreate.spec.js
+++ tests/unit/stores/ontPoolCreate.spec.js
@@ -31,9 +31,123 @@ describe('useOntPoolCreateStore', () => {
       store = useOntPoolCreateStore()
     })
 
-    it('return the requests', () => {
-      store.resources.requests = ontRequestFactory.storeData
-      expect(store.requests).toEqual(Object.values(ontRequestFactory.storeData))
+    describe('requests', () => {
+      it('return the requests', () => {
+        store.resources.requests = ontRequestFactory.storeData
+        expect(store.requests).toEqual(Object.values(ontRequestFactory.storeData))
+      })
+    })
+
+    describe('poolItem', () => {
+      const pool = {
+        id: 1,
+        volume: '1',
+        kit_barcode: 'ABC1',
+        concentration: '1',
+        insert_size: '100',
+      }
+
+      it('returns the correct data', () => {
+        store.pooling.pool = pool
+        expect(store.poolItem).toEqual(pool)
+      })
+
+      it('when the pool does not exist', () => {
+        store.pooling.pool = undefined
+        expect(store.poolItem).toEqual({})
+      })
+    })
+
+    describe('tubeItem', () => {
+      const tube = {
+        id: 1,
+        barcode: 'GEN-1668092750-4',
+      }
+      it('returns the tube data', () => {
+        store.pooling.tube = tube
+        expect(store.tubeItem).toEqual(tube)
+      })
+
+      it('when the tube does not exist', () => {
+        store.pooling.tube = undefined
+        expect(store.tubeItem).toEqual({})
+      })
+    })
+
+    describe('libraryItem', () => {
+      // When selecting a request with append the id with an underscore. This ensures
+      // keys are maintained in insertion order, not numeric order. This allow our requests
+      // to maintain the order in which they were selected
+      const libraries = {
+        3: {
+          ont_request_id: '3',
+          tag_id: null,
+          volume: null,
+          concentration: null,
+          insert_size: null,
+        },
+        4: {
+          ont_request_id: '4',
+          tag_id: null,
+          volume: null,
+          concentration: null,
+          insert_size: null,
+        },
+      }
+
+      it('returns a library', () => {
+        store.pooling.libraries = libraries
+        expect(store.libraryItem('3')).toEqual(libraries['3'])
+      })
+    })
+
+    describe('selectedPlates', () => {
+      it('returns a list of the selected plates', () => {
+        const plates = {
+          1: {
+            barcode: 'DN1',
+            id: '1',
+            type: 'plates',
+            wells: {},
+          },
+        }
+        store.selected.plates = plates
+        expect(store.selectedPlates).toEqual(Object.values(plates))
+      })
+    })
+
+    describe('selectedTubes', () => {
+      it('returns the list of the selected tubes', () => {
+        const tubes = {
+          1: {
+            barcode: 'DN1',
+            id: '1',
+            type: 'tube',
+          },
+        }
+        store.selected.tubes = tubes
+        expect(store.selectedTubes).toEqual(Object.values(tubes))
+      })
+    })
+
+    describe('wellList', () => {
+      const wells = {
+        1: { id: '1', position: 'A1' },
+        2: { id: '2', position: 'B1' },
+        3: { id: '3', position: 'C1' },
+        4: { id: '4', position: 'D1' },
+        5: { id: '5', position: 'E1' },
+      }
+      it('returns a list of all fetched wells', () => {
+        store.resources.wells = wells
+        expect(store.wellList()).toEqual(wells.values)
+      })
+
+      it('when ids are included', () => {
+        store.resources.wells = wells
+        const ids = ['1', '2', '3']
+        expect(store.wellList(ids).length).toEqual(ids.length)
+      })
     })
   })
 })
