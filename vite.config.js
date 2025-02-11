73512ee265b6a02914a6a2341483f07ffb633a62
Stephen <519327+stevieing@users.noreply.github.com>
2025-02-11 11:41:58 +0000

    fix: update Tailwind CSS integration and dependencies

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,14 @@ import vue from '@vitejs/plugin-vue'
 // Help us keep track of what we import, and the size effect it has on the bundle
 import { visualizer } from 'rollup-plugin-visualizer'
 import { fileURLToPath, URL } from 'url'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     visualizer(), // The docs recommend this one goes last
+    tailwindcss(),
   ],
   resolve: {
     alias: {
