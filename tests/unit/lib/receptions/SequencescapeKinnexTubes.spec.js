8843dd88b73ffc88b131a2da1f3595ebf0a4fcaa
Seena Nair <55585488+seenanair@users.noreply.github.com>
2025-06-09 16:43:56 +0100

    update(SequencescapeKinnexTubes): Refactor attributes structure to include compound sample tubes and enhance sample details

--- tests/unit/lib/receptions/SequencescapeKinnexTubes.spec.js
+++ tests/unit/lib/receptions/SequencescapeKinnexTubes.spec.js
@@ -40,7 +40,7 @@ describe('SequencescapeKinnexTubes', () => {
       expect(foundBarcodes).toEqual(new Set(['NT67O']))
       expect(attributes).toEqual({
         source: 'traction-ui.sequencescape',
-        tubes_attributes: [
+        compound_sample_tubes_attributes: [
           {
             barcode: 'NT67O',
             request: {
@@ -48,14 +48,35 @@ describe('SequencescapeKinnexTubes', () => {
               library_type: 'Example',
               cost_code: 'aCostCodeExample',
             },
-            sample: {
-              name: 'Supplier A',
-              external_id: 'f3e18688-4142-11f0-ac8e-e2df7c04b5f2',
-              species: 'human',
-              retention_instruction: null,
-              date_of_sample_collection: '2023-01-01',
-              donor_id: 'Donor 1',
-            },
+            samples: [
+              {
+                date_of_sample_collection: '2023-01-01',
+                donor_id: 'Donor 1',
+                external_id: 'f3e18688-4142-11f0-ac8e-e2df7c04b5f2',
+                name: 'sample_1_SQPD-9015_A4',
+                retention_instruction: null,
+                species: 'human',
+                supplier_name: 'Supplier A',
+              },
+              {
+                date_of_sample_collection: '2023-01-02',
+                donor_id: 'Donor 1',
+                external_id: 'f3e372c2-4142-11f0-ac8e-e2df7c04b5f2',
+                name: 'sample_2_SQPD-9015_A4',
+                retention_instruction: null,
+                species: 'human',
+                supplier_name: 'Supplier A',
+              },
+              {
+                date_of_sample_collection: '2023-01-03',
+                donor_id: 'Donor 1',
+                external_id: 'f3e4f250-4142-11f0-ac8e-e2df7c04b5f2',
+                name: 'sample_3_SQPD-9015_A4',
+                retention_instruction: null,
+                species: 'human',
+                supplier_name: 'Supplier A',
+              },
+            ],
           },
         ],
       })
