715d10be47808c32d180628b64ee2346494b7b9e
Stephen <519327+stevieing@users.noreply.github.com>
2025-06-20 15:50:56 +0100

    Refactor AnnotationItem tests to use mountWithStore for better state management

--- tests/unit/components/AnnotationItem.spec.js
+++ tests/unit/components/AnnotationItem.spec.js
@@ -1,5 +1,7 @@
-import { mount } from '@support/testHelper.js'
+import { mountWithStore } from '@support/testHelper.js'
 import AnnotationItem from '@/components/AnnotationItem.vue'
+import { usePacbioRunCreateStore } from '@/stores/pacbioRunCreate.js'
+import AnnotationTypeFactory from '@tests/factories/AnnotationTypeFactory.js'
 
 const annotation = {
   id: '1',
@@ -10,26 +12,43 @@ const annotation = {
   annotatable_type: 'Pacbio::Run',
 }
 
+const annotationTypeFactory = AnnotationTypeFactory()
+
+const mountComponent = (props = {}) => {
+  const { wrapper } = mountWithStore(AnnotationItem, {
+    initialState: {
+      pscbioRunCreate: {
+        resources: {
+          annotationTypes: annotationTypeFactory.storeData,
+        },
+      },
+    },
+    props,
+    createStore: () => usePacbioRunCreateStore(),
+  })
+  return wrapper
+}
+
 describe('AnnotationItem.vue', () => {
   it('renders annotation item with correct data', () => {
-    const wrapper = mount(AnnotationItem, {
-      props: { annotation },
-    })
+    const wrapper = mountComponent({ annotation })
     expect(wrapper.find('[data-attribute="comment"]').element.value).toEqual('annotation 1')
     expect(wrapper.find('[data-attribute="user"]').element.value).toEqual('lulu')
     expect(wrapper.find('[data-attribute="created-at"]').text()).toEqual('2023-10-01T12:00:00Z')
   })
 
   it('is a new record when annotation is empty', () => {
-    const wrapper = mount(AnnotationItem)
+    const wrapper = mountComponent()
     expect(wrapper.vm.isNewRecord).toBeTruthy()
   })
 
   it('disables input when the annotation is not new', () => {
-    const wrapper = mount(AnnotationItem, {
-      props: { ...annotation, id: 'new' },
-    })
+    const wrapper = mountComponent({ annotation: { ...annotation, id: 'new' } })
     expect(wrapper.vm.isNewRecord).toBeTruthy()
     expect(wrapper.find('input').attributes('disabled')).toBeDefined()
   })
+
+  // describe('annotation type', () => {
+
+  // })
 })
