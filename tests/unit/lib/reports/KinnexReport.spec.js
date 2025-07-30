c387dd5d02c09f96cf207c937a395f513853f255
Ben Topping <ben.topping@outlook.com>
2025-07-30 10:45:19 +0100

    tests(kinnex-report): refactors expected sample assertions, use testHelper response helpers

--- tests/unit/lib/reports/KinnexReport.spec.js
+++ tests/unit/lib/reports/KinnexReport.spec.js
@@ -4,7 +4,12 @@ import {
   fetchFunction,
 } from '@/lib/reports/KinnexReport.js'
 import useRootStore from '@/stores'
-import { createPinia, setActivePinia } from '@support/testHelper.js'
+import {
+  createPinia,
+  setActivePinia,
+  successfulResponse,
+  failedResponse,
+} from '@support/testHelper.js'
 import PacbioRequestFactory from '@tests/factories/PacbioRequestFactory.js'
 const pacbioRequestFactory = PacbioRequestFactory({ includeRelationships: true })
 
@@ -17,6 +22,31 @@ vi.mock('@/composables/useAlert', () => ({
 
 const request = pacbioRequestFactory.content.data[0]
 const sample = pacbioRequestFactory.content.included[7]
+
+const formattedSample = (sample, request, study, facultySponsor) => {
+  const s = {
+    request_id: request.id,
+    barcode: request.attributes.barcode,
+    cost_code: request.attributes.cost_code,
+    library_type: request.attributes.library_type,
+    external_study_id: request.attributes.external_study_id,
+    date_of_sample_collection: sample.attributes.date_of_sample_collection,
+    supplier_name: sample.attributes.supplier_name,
+    donor_id: sample.attributes.donor_id,
+    species: sample.attributes.species,
+    sanger_sample_id: sample.attributes.sanger_sample_id,
+  }
+  if (study && facultySponsor) {
+    return {
+      ...s,
+      study_number: study.id,
+      uuid: study.attributes.uuid,
+      study_name: study.attributes.name,
+      submitting_faculty: facultySponsor.attributes.name,
+    }
+  }
+  return s
+}
 
 // Mocked data from Sequencescape
 const ssStudy = {
@@ -58,39 +88,34 @@ const ssFacultySponsor = {
 }
 
 describe('KinnexReport', () => {
-  let rootStore, get, ssGet
+  let rootStore, tractionRequestGet, sequencescapeStudyGet
 
   beforeEach(() => {
     const pinia = createPinia()
     setActivePinia(pinia)
     // Mock the root store and its API
     rootStore = useRootStore()
-    get = vi.fn()
-    rootStore.api.traction.pacbio.requests.get = get
+    tractionRequestGet = vi.fn()
+    rootStore.api.traction.pacbio.requests.get = tractionRequestGet
 
-    ssGet = vi.fn()
-    rootStore.api.sequencescape.studies.get = ssGet
+    sequencescapeStudyGet = vi.fn()
+    rootStore.api.sequencescape.studies.get = sequencescapeStudyGet
   })
 
   describe('fetchTractionSamples', () => {
     it('returns empty array and shows alert if both responses fail', async () => {
-      get.mockRejectedValue('Internal Server Error')
+      tractionRequestGet.mockResolvedValue(failedResponse())
 
       const { data, errors } = await fetchTractionSamples('input', [])
       expect(data).toEqual([])
       expect(errors).toEqual({
-        message: 'Error fetching samples from Traction: Internal Server Error',
+        message: 'Error fetching samples from Traction: error1 There was an error.',
         type: 'danger',
       })
     })
 
     it('returns empty array and shows alert if no data found', async () => {
-      get.mockResolvedValue({
-        status: 200,
-        statusText: 'OK',
-        ok: true,
-        json: () => Promise.resolve({ data: [], included: [] }),
-      })
+      tractionRequestGet.mockResolvedValue(successfulResponse({ statusCode: 200, data: [] }))
 
       const { data, errors } = await fetchTractionSamples('input', [])
       expect(data).toEqual([])
@@ -102,7 +127,7 @@ describe('KinnexReport', () => {
 
     it('removes duplicates and returns formatted samples', async () => {
       // Duplicates because the same sample is found in both requests
-      get.mockResolvedValue({
+      tractionRequestGet.mockResolvedValue({
         status: 200,
         statusText: 'OK',
         ok: true,
@@ -112,64 +137,37 @@ describe('KinnexReport', () => {
       const { data, errors } = await fetchTractionSamples('input', [])
       expect(errors).toEqual({})
       expect(data).toHaveLength(1)
-      expect(data[0]).toEqual({
-        request_id: request.id,
-        barcode: request.attributes.barcode,
-        cost_code: request.attributes.cost_code,
-        library_type: request.attributes.library_type,
-        external_study_id: request.attributes.external_study_id,
-        date_of_sample_collection: sample.attributes.date_of_sample_collection,
-        supplier_name: sample.attributes.supplier_name,
-        donor_id: sample.attributes.donor_id,
-        species: sample.attributes.species,
-        sanger_sample_id: sample.attributes.sanger_sample_id,
-      })
+      expect(data[0]).toEqual(formattedSample(sample, request))
     })
 
     it('successful if only one of the requests returns data', async () => {
-      get.mockResolvedValueOnce({
+      tractionRequestGet.mockResolvedValueOnce({
         status: 200,
         statusText: 'OK',
         ok: true,
         json: () => Promise.resolve({ data: [request], included: [sample] }),
       })
       // Second call is for source_identifier fetch
-      get.mockResolvedValueOnce({
-        status: 200,
-        statusText: 'OK',
-        ok: true,
-        json: () => Promise.resolve({ data: [], included: [] }),
-      })
+      tractionRequestGet.mockResolvedValueOnce(successfulResponse({ statusCode: 200, data: [] }))
 
       const { data, errors } = await fetchTractionSamples('input', [])
       expect(errors).toEqual({})
       expect(data).toHaveLength(1)
-      expect(data[0]).toEqual({
-        request_id: request.id,
-        barcode: request.attributes.barcode,
-        cost_code: request.attributes.cost_code,
-        library_type: request.attributes.library_type,
-        external_study_id: request.attributes.external_study_id,
-        date_of_sample_collection: sample.attributes.date_of_sample_collection,
-        supplier_name: sample.attributes.supplier_name,
-        donor_id: sample.attributes.donor_id,
-        species: sample.attributes.species,
-        sanger_sample_id: sample.attributes.sanger_sample_id,
-      })
+      expect(data[0]).toEqual(formattedSample(sample, request))
     })
 
     it('fetches multiple samples from Traction API across filter types', async () => {
       const request2 = pacbioRequestFactory.content.data[1]
       const sample2 = pacbioRequestFactory.content.included[8]
 
-      get.mockResolvedValueOnce({
+      tractionRequestGet.mockResolvedValueOnce({
         status: 200,
         statusText: 'OK',
         ok: true,
         json: () => Promise.resolve({ data: [request], included: [sample] }),
       })
       // Second call is for source_identifier fetch
-      get.mockResolvedValueOnce({
+      tractionRequestGet.mockResolvedValueOnce({
         status: 200,
         statusText: 'OK',
         ok: true,
@@ -177,37 +175,12 @@ describe('KinnexReport', () => {
       })
       const { data, errors } = await fetchTractionSamples('sample1,sample2', [])
       expect(errors).toEqual({})
-      expect(data).toEqual([
-        {
-          request_id: request.id,
-          barcode: request.attributes.barcode,
-          cost_code: request.attributes.cost_code,
-          library_type: request.attributes.library_type,
-          external_study_id: request.attributes.external_study_id,
-          date_of_sample_collection: sample.attributes.date_of_sample_collection,
-          supplier_name: sample.attributes.supplier_name,
-          donor_id: sample.attributes.donor_id,
-          species: sample.attributes.species,
-          sanger_sample_id: sample.attributes.sanger_sample_id,
-        },
-        {
-          request_id: request2.id,
-          barcode: request2.attributes.barcode,
-          cost_code: request2.attributes.cost_code,
-          library_type: request2.attributes.library_type,
-          external_study_id: request2.attributes.external_study_id,
-          date_of_sample_collection: sample2.attributes.date_of_sample_collection,
-          supplier_name: sample2.attributes.supplier_name,
-          donor_id: sample2.attributes.donor_id,
-          species: sample2.attributes.species,
-          sanger_sample_id: sample2.attributes.sanger_sample_id,
-        },
-      ])
-      expect(get).toHaveBeenCalledWith({
+      expect(data).toEqual([formattedSample(sample, request), formattedSample(sample2, request2)])
+      expect(tractionRequestGet).toHaveBeenCalledWith({
         filter: { sample_name: 'sample1,sample2' },
         include: 'sample',
       })
-      expect(get).toHaveBeenCalledWith({
+      expect(tractionRequestGet).toHaveBeenCalledWith({
         filter: { source_identifier: 'sample1,sample2' },
         include: 'sample',
       })
@@ -216,22 +189,17 @@ describe('KinnexReport', () => {
 
   describe('fetchSequencescapeStudies', () => {
     it('returns empty array and shows alert if response fails', async () => {
-      ssGet.mockRejectedValue('Internal Server Error')
+      sequencescapeStudyGet.mockResolvedValue(failedResponse())
       const { data, errors } = await fetchSequencescapeStudies([{ external_study_id: 'ext' }])
       expect(data).toEqual([])
       expect(errors).toEqual({
-        message: 'Error fetching studies from Sequencescape: Internal Server Error',
+        message: 'Error fetching studies from Sequencescape: error1 There was an error.',
         type: 'danger',
       })
     })
 
     it('returns empty array and shows alert if no data found', async () => {
-      ssGet.mockResolvedValue({
-        status: 200,
-        statusText: 'OK',
-        ok: true,
-        json: () => Promise.resolve({ data: [], included: [] }),
-      })
+      sequencescapeStudyGet.mockResolvedValue(successfulResponse({ statusCode: 200 }))
       const { data, errors } = await fetchSequencescapeStudies([{ external_study_id: 'ext' }])
       expect(data).toEqual([])
       expect(errors).toEqual({
@@ -241,7 +209,7 @@ describe('KinnexReport', () => {
     })
 
     it('returns formatted studies from Sequencescape', async () => {
-      ssGet.mockResolvedValue({
+      sequencescapeStudyGet.mockResolvedValue({
         status: 200,
         statusText: 'OK',
         ok: true,
@@ -269,20 +237,20 @@ describe('KinnexReport', () => {
       const request2 = pacbioRequestFactory.content.data[1]
       const sample2 = pacbioRequestFactory.content.included[8]
 
-      get.mockResolvedValueOnce({
+      tractionRequestGet.mockResolvedValueOnce({
         status: 200,
         statusText: 'OK',
         ok: true,
         json: () => Promise.resolve({ data: [request], included: [sample] }),
       })
       // Second call is for source_identifier fetch
-      get.mockResolvedValueOnce({
+      tractionRequestGet.mockResolvedValueOnce({
         status: 200,
         statusText: 'OK',
         ok: true,
         json: () => Promise.resolve({ data: [request2], included: [sample2] }),
       })
-      ssGet.mockResolvedValue({
+      sequencescapeStudyGet.mockResolvedValue({
         status: 200,
         statusText: 'OK',
         ok: true,
@@ -296,49 +264,14 @@ describe('KinnexReport', () => {
       const result = await fetchFunction('sample1,sample2', [])
 
       expect(result.data).toEqual([
-        {
-          request_id: request.id,
-          barcode: request.attributes.barcode,
-          cost_code: request.attributes.cost_code,
-          library_type: request.attributes.library_type,
-          external_study_id: request.attributes.external_study_id,
-          date_of_sample_collection: sample.attributes.date_of_sample_collection,
-          supplier_name: sample.attributes.supplier_name,
-          donor_id: sample.attributes.donor_id,
-          species: sample.attributes.species,
-          sanger_sample_id: sample.attributes.sanger_sample_id,
-          study_number: ssStudy.id,
-          uuid: ssStudy.attributes.uuid,
-          study_name: ssStudy.attributes.name,
-          submitting_faculty: ssFacultySponsor.attributes.name,
-        },
-        {
-          request_id: request2.id,
-          barcode: request2.attributes.barcode,
-          cost_code: request2.attributes.cost_code,
-          library_type: request2.attributes.library_type,
-          external_study_id: request2.attributes.external_study_id,
-          date_of_sample_collection: sample2.attributes.date_of_sample_collection,
-          supplier_name: sample2.attributes.supplier_name,
-          donor_id: sample2.attributes.donor_id,
-          species: sample2.attributes.species,
-          sanger_sample_id: sample2.attributes.sanger_sample_id,
-          study_number: ssStudy.id,
-          uuid: ssStudy.attributes.uuid,
-          study_name: ssStudy.attributes.name,
-          submitting_faculty: ssFacultySponsor.attributes.name,
-        },
+        formattedSample(sample, request, ssStudy, ssFacultySponsor),
+        formattedSample(sample2, request2, ssStudy, ssFacultySponsor),
       ])
       expect(result.errors).toEqual({})
     })
 
     it('returns errors if traction returns no samples', async () => {
-      get.mockResolvedValue({
-        status: 200,
-        statusText: 'OK',
-        ok: true,
-        json: () => Promise.resolve({ data: [], included: [] }),
-      })
+      tractionRequestGet.mockResolvedValue(successfulResponse({ statusCode: 200, data: [] }))
 
       const { data, errors } = await fetchFunction('input', [])
       expect(data).toEqual([])
@@ -349,22 +282,13 @@ describe('KinnexReport', () => {
     })
 
     it('returns errors if traction returns but sequencescape does not', async () => {
-      get.mockResolvedValue({
+      tractionRequestGet.mockResolvedValue({
         status: 200,
         statusText: 'OK',
         ok: true,
         json: () => Promise.resolve({ data: [request], included: [sample] }),
       })
-      ssGet.mockResolvedValue({
-        status: 200,
-        statusText: 'OK',
-        ok: true,
-        json: () =>
-          Promise.resolve({
-            data: [],
-            included: [],
-          }),
-      })
+      sequencescapeStudyGet.mockResolvedValue(successfulResponse({ statusCode: 200 }))
 
       const { data, errors } = await fetchFunction('input', [])
       expect(data).toEqual([])
