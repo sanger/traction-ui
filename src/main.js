728ad8b70249e0257d059a15d19723c87a45ba0a
Seena Nair <55585488+seenanair@users.noreply.github.com>
2025-08-07 11:44:07 +0100

    refactor: remove Vuex store integration from main.js

--- src/main.js
+++ src/main.js
@@ -3,14 +3,12 @@ import './styles/index.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
 import globalAlert from '@/mixins/globalAlert'
 import { registerGlobal } from '@/components/shared'
 import { createPinia } from 'pinia'
 
 const app = createApp(App)
 registerGlobal(app)
-app.use(store)
 app.use(router)
 app.mixin(globalAlert)
 app.use(createPinia())
