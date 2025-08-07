dec23b68ed8385b78863079dc95ad3d78af361c9
Seena Nair <55585488+seenanair@users.noreply.github.com>
2025-08-07 11:13:20 +0100

    refactor: enhance setup.js with detailed comments and Pinia instance setup

--- tests/support/setup.js
+++ tests/support/setup.js
@@ -2,9 +2,19 @@
 
 import { vi, afterEach } from 'vitest'
 
+/*createPinia is for creating a Pinia instance to test pinia stores.
+setActivePinia sets the newly created Pinia instance as the active Pinia instance.
+This is necessary to allow useStore to pick up the any other pinia instance other than default global instance
+More documentation available on https://pinia.vuejs.org/cookbook/testing.html*/
+import { setActivePinia, createPinia } from 'pinia'
+
 // https://vitest.dev/api/vi.html#vi-mock
 vi.mock('swrv')
 vi.mock('axios')
+
+beforeEach(() => {
+  setActivePinia(createPinia())
+})
 
 afterEach(() => {
   vi.clearAllMocks()
