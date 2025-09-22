f27796d86a19c6a573f3fc719da1ac246df3a445
Stephen <519327+stevieing@users.noreply.github.com>
2025-09-22 09:46:56 +0100

    feat: remove unused FeatureFlagTest component and enable feature flag in ont_samples_view tests

--- src/components/shared/FeatureFlagTest.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div>The refactored feature flag functionality works!</div>
-</template>
-
-<script setup>
-// No script logic needed for this basic message
-</script>
--- tests/e2e/specs/ont/ont_samples_view.cy.js
+++ tests/e2e/specs/ont/ont_samples_view.cy.js
@@ -2,6 +2,9 @@ import OntRequestFactory from '../../../factories/OntRequestFactory.js'
 
 describe('Ont samples view', () => {
   beforeEach(() => {
+    cy.withFlags({
+      feature_flag_test: { enabled: true },
+    })
     cy.wrap(OntRequestFactory()).as('ontRequestFactory')
     cy.get('@ontRequestFactory').then((ontRequestFactory) => {
       cy.intercept('/v1/ont/requests?page[size]=25&page[number]=1', {
