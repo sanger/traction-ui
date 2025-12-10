0b94231d7386c1b7e881f92f80855ab4474bc0ab
Ben Topping <ben.topping@outlook.com>
2025-12-10 15:49:33 +0000

    tests: correct duplicate selected barcodes

--- tests/unit/views/pacbio/PacbioPoolIndex.spec.js
+++ tests/unit/views/pacbio/PacbioPoolIndex.spec.js
@@ -95,7 +95,7 @@ describe('PacbioPoolIndex.vue', () => {
         pools.state.selected = [
           { id: 1, barcode: 'TRAC-1', source_identifier: 'SQSC-1' },
           { id: 2, barcode: 'TRAC-2', source_identifier: 'SQSC-2' },
-          { id: 3, barcode: 'TRAC-2', source_identifier: 'SQSC-2' },
+          { id: 3, barcode: 'TRAC-3', source_identifier: 'SQSC-3' },
         ]
       })
 
@@ -114,7 +114,7 @@ describe('PacbioPoolIndex.vue', () => {
           [
             { id: 1, barcode: 'TRAC-1', source_identifier: 'SQSC-1' },
             { id: 2, barcode: 'TRAC-2', source_identifier: 'SQSC-2' },
-            { id: 3, barcode: 'TRAC-2', source_identifier: 'SQSC-2' },
+            { id: 3, barcode: 'TRAC-3', source_identifier: 'SQSC-3' },
           ],
           'Pacbio - Pool',
         )
