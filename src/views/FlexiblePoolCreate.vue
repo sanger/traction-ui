cd1b3f7c4f9846653a59c1e6926d887823c04cb3
yoldas <ay6@sanger.ac.uk>
2026-02-05 02:33:59 +0000

    Update tooltip with CSV column descriptions

--- src/views/FlexiblePoolCreate.vue
+++ src/views/FlexiblePoolCreate.vue
@@ -59,26 +59,32 @@
                             <li>
                               All columns must contain values: <br />
                               <div class="px-4 font-bold">
-                                Pool Number, Sample Name, Tag, Template Prep Kit Box Barcode,
-                                Volume, Concentration, Insert Size
+                                Pool Number,Source Identifier,Tag Set,Tag,Template Prep Kit Box
+                                Barcode,Volume (uL),Concentration (ng/uL),Insert Size
                               </div>
                             </li>
                             <li>
                               <strong>Pool Number:</strong> Samples with the same pool number will
                               be grouped into the same pool.
                             </li>
-                            <li><strong>Sample Name:</strong></li>
-                            <ul class="list-disc list-inside pl-2">
-                              <li>
-                                The 'Sample Name' field must correspond to a sample name in
-                                Traction.
-                              </li>
-                            </ul>
+                            <li>
+                              <strong>Source Identifier:</strong> Library source, Tube barcode, or
+                              combination of Plate barcode and Well position.
+                            </li>
+                            <li><strong>Tag Set:</strong> The tag set name of tags to be used.</li>
                             <li><strong>Tag:</strong> The tag to apply to the sample.</li>
                             <li>
                               <strong>Template Prep Kit Box Barcode:</strong> The barcode of the
                               template prep kit.
                             </li>
+                            <li>
+                              <strong>Volume (uL):</strong> The volume of the sample in microliters.
+                            </li>
+                            <li>
+                              <strong>Concentration (ng/uL):</strong> The concentration of the
+                              sample in nanograms per microliter.
+                            </li>
+                            <li><strong>Insert Size:</strong> The insert size for the sample.</li>
                           </ul>
                         </div>
                       </template>
