5b43ef47b910c00ae0d2525c08ca3cbefc17ca0c
Dasun Pubudumal <pubudumald@gmail.com>
2026-03-09 15:00:40 +0000

    fix(FlexiblePoolCreate): update watch to use reactive id value for route changes

--- src/views/FlexiblePoolCreate.vue
+++ src/views/FlexiblePoolCreate.vue
@@ -186,7 +186,7 @@ const busy = ref(false)
 // Actions
 // Watch for changes to the route id and call provider
 watch(
-  () => route.params.id,
+  () => id.value,
   async (newId, oldId) => {
     if (newId !== oldId) {
       await provider()
