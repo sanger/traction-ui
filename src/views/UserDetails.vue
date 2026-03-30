98cb76c6dfcf87d113a0f01ed21066536c6aecf4
Stephen Hulme <sh51@sanger.ac.uk>
2026-03-30 17:02:32 +0100

    fix: standardise html

--- src/views/UserDetails.vue
+++ src/views/UserDetails.vue
@@ -1,22 +1,23 @@
 <template>
-  <h2 class="text-2xl">Welcome {{ name }}!</h2>
-  <br />
-  <p>
-    🎉 Congratulations! 🎉 You're now successfully logged in to your account. We're thrilled to have
-    you as part of our community. Your journey starts here, and we're here to support you every step
-    of the way. Below are your account details. 🔍 🌟🔐
-  </p>
-  <br />
   <div>
-    <h2 class="text-2xl">User Information</h2>
-    <br />
+    <div class="w-3/4 mx-auto bg-gray-100 border border-gray-200 bg-gray-100 rounded-md p-4 mb-4">
+      <traction-heading level="2" show-border>Welcome {{ name }}</traction-heading>
+      <p>
+        You're now successfully logged in to your account. We're thrilled to have you as part of our
+        community. Your journey starts here, and we're here to support you every step of the way.
+        Below are your account details.
+      </p>
+    </div>
 
-    <traction-table
-      id="user-claims-table"
-      :items="userClaims"
-      :fields="claimFields"
-      empty-text="No user claims available"
-    />
+    <div class="w-full mx-auto bg-gray-100 border border-gray-200 bg-gray-100 rounded-md p-4">
+      <traction-heading level="3" show-border class="mt-4">Your Account Details</traction-heading>
+      <traction-table
+        id="user-claims-table"
+        :items="userClaims"
+        :fields="claimFields"
+        empty-text="No user claims available"
+      />
+    </div>
   </div>
 </template>
 <script setup>
